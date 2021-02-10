85c9c335849e3ec966a875a184f108f06eae9bc1
c7yrus <c7yrus@gmail.com>
2021-02-11 09:08:17 +1100

    initial for setting up the address component

---
+++ src/app/DTT/address/google_maps/index.js
@@ -0,0 +1,64 @@
+import { useEffect, useRef, createRef } from 'react'
+import { apiConfig } from 'config/get-api-config'
+import { equals } from 'ramda'
+const GoogleMaps = ({ searchAddress, addressRef, setAllOptions, allOptions }) => {
+  const googleMapRef = createRef()
+  const googleMap = useRef(null)
+  let map
+  const initMap = () => {
+    map = new window.google.maps.Map(googleMapRef.current, {
+      center: { lat: -33.8688, lng: 151.2195 },
+      zoom: 13,
+      mapTypeId: 'roadmap',
+    })
+    const input = document.getElementById('address-input')
+    const options = {
+      fields: ['formatted_address', 'geometry', 'name'],
+      origin: map.getCenter(),
+      strictBounds: false,
+    }
+    const autocomplete = new window.google.maps.places.Autocomplete(input, options)
+    const infowindow = new window.google.maps.InfoWindow()
+    const infowindowContent = document.getElementById('infowindow-content')
+    infowindow.setContent(infowindowContent)
+    autocomplete.addListener('place_changed', () => {
+      infowindow.close()
+      const place = autocomplete.getPlace()
+      if (!place.geometry) {
+        window.alert("No details available for input: '" + place.name + "'")
+        return
+      }
+      // If the place has a geometry, then present it on a map.
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport)
+      } else {
+        map.setCenter(place.geometry.location)
+        map.setZoom(17)
+      }
+    })
+    const displaySuggestions = (predictions, status) => {
+      if (status !== window.google.maps.places.PlacesServiceStatus.OK) {
+        alert(status)
+        return
+      }
+      if (!equals(allOptions, predictions)) {
+        setAllOptions(predictions)
+      }
+    }
+    const initService = () => {
+      const service = new window.google.maps.places.AutocompleteService()
+      service.getQueryPredictions({ input: addressRef.current.value }, displaySuggestions)
+    }
+    initService()
+  }
+  useEffect(() => {
+    const googleMapScript = document.createElement('script')
+    googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${apiConfig.ENV_GOOGLE_MAPS_APIKEY}&libraries=places`
+    window.document.body.appendChild(googleMapScript)
+    googleMapScript.addEventListener('load', () => {
+      googleMap.current = initMap()
+    })
+  })
+  return <div id="google-map" ref={googleMapRef} />
+}
+export default GoogleMaps
--- src/app/DTT/address/index.js
+++ src/app/DTT/address/index.js
@@ -1,11 +1,50 @@
+import { useState, useRef, useEffect } from 'react'
 import { Read } from 'app/DTT/text'
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
-
-const Write = ({ onSendAnswer, data }) => <GooglePlacesAutocomplete />
-
+import GoogleMaps from './google_maps'
+import { Input } from '@chakra-ui/react'
+import { isEmpty } from 'ramda'
+const Write = ({ onSendAnswer, data }) => {
+  const [searchAddress, setSearchAddress] = useState('')
+  const [allOptions, setAllOptions] = useState([])
+  const [showSuggestiongs, setShowSuggestions] = useState(true)
+  useEffect(() => {
+    isEmpty(searchAddress) && setAllOptions([])
+  }, [searchAddress])
+  const addressRef = useRef(null)
+  return (
+    <div>
+      <div onClick={() => setShowSuggestions(true)}>
+        <Input
+          id="address-input"
+          ref={addressRef}
+          onChange={e => {
+            setSearchAddress(e.target.value)
+          }}
+          value={searchAddress}
+        />
+      </div>
+      {showSuggestiongs &&
+        allOptions.map(({ description }) => (
+          <div
+            onClick={() => {
+              setSearchAddress(description)
+              setShowSuggestions(false)
+            }}
+          >
+            <option>{description}</option>
+          </div>
+        ))}
+      <GoogleMaps
+        addressRef={addressRef}
+        searchAddress={searchAddress}
+        allOptions={allOptions}
+        setAllOptions={setAllOptions}
+      />
+    </div>
+  )
+}
 const Address = {
   Write,
   Read,
 }
-
 export default Address
