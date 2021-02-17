6c934583880585d3e765dbd6369f0ff1b4b451ab
slevens <sjlevens@gmail.com>
2021-02-17 13:57:15 +1100

    make usable in main

--- src/index.js
+++ src/index.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import ReactDOM from 'react-dom'
+// import reportWebVitals from './reportWebVitals'
+import getApiConfig from 'config/get-api-config'
+import { initLog } from 'utils/log'
+import { ReactKeycloakProvider } from '@react-keycloak/web'
+import { Center, CircularProgress } from '@chakra-ui/react'
+import App from 'app'
+const Error = lazy(() => import('error'))
 
-import App from './Sandbox'
+const initialiseApp = async () => {
+  try {
+    initLog()
+    const { keycloak } = await getApiConfig()
 
-const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement)
+    ReactDOM.render(
+      <React.StrictMode>
+        <ReactKeycloakProvider
+          authClient={keycloak}
+          LoadingComponent={
+            <Center>
+              <CircularProgress isIndeterminate />
+            </Center>
+          }
+        >
+          <App />
+        </ReactKeycloakProvider>
+      </React.StrictMode>,
+      document.getElementById('root'),
+    )
+  } catch (err) {
+    ReactDOM.render(
+      <React.StrictMode>
+        <Suspense fallback={<div />}>
+          <Error />
+        </Suspense>
+      </React.StrictMode>,
+      document.getElementById('root'),
+    )
+  }
+}
+
+initialiseApp()
+// reportWebVitals()
--- src/utils/developer/Sandbox.js
+++ src/utils/developer/Sandbox.js
@@ -1,4 +1,6 @@
+/* eslint-disable */
 import React from 'react'
+
 import {
   ChakraProvider,
   Box,
