9f0ee0d3e9642da577ff071cc8b85f015e7a9bb8
Barad Ghimire <baradghimire@gmail.com>
2021-02-17 13:44:11 +1100

    Initialise design sandbox

--- src/index.js
+++ src/index.js
@@ -1,44 +1,7 @@
-import React, { lazy, Suspense } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-// import reportWebVitals from './reportWebVitals'
-import getApiConfig from 'config/get-api-config'
-import { initLog } from 'utils/log'
-import { ReactKeycloakProvider } from '@react-keycloak/web'
-import { Center, CircularProgress } from '@chakra-ui/react'
-import App from 'app'
-const Error = lazy(() => import('error'))
 
-const initialiseApp = async () => {
-  try {
-    initLog()
-    const { keycloak } = await getApiConfig()
+import App from './Sandbox'
 
-    ReactDOM.render(
-      <React.StrictMode>
-        <ReactKeycloakProvider
-          authClient={keycloak}
-          LoadingComponent={
-            <Center>
-              <CircularProgress isIndeterminate />
-            </Center>
-          }
-        >
-          <App />
-        </ReactKeycloakProvider>
-      </React.StrictMode>,
-      document.getElementById('root'),
-    )
-  } catch (err) {
-    ReactDOM.render(
-      <React.StrictMode>
-        <Suspense fallback={<div />}>
-          <Error />
-        </Suspense>
-      </React.StrictMode>,
-      document.getElementById('root'),
-    )
-  }
-}
-
-initialiseApp()
-// reportWebVitals()
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
