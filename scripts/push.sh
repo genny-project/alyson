dcce0a52716d5e34f4490b876511d7601ddcf9e3
slevens <sjlevens@gmail.com>
2021-02-17 15:16:07 +1100

    Update push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-
 if [ -z "${1}" ]; then
    version="latest"
 else
    version="${1}"
 fi
-
-
-docker push gennyproject/alyson:"${version}"
-docker tag  gennyproject/alyson:"${version}" gennyproject/alyson:latest
-docker push gennyproject/alyson:latest
-
+docker push gennyproject/alyson-v9:"${version}"
+docker tag  gennyproject/alyson-v9:"${version}" gennyproject/alyson-v9:latest
+docker push gennyproject/alyson-v9:latest
