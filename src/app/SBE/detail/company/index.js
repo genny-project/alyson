24bf5735bbf52364dd895e419b252f621d12a034
slevens <sjlevens@gmail.com>
2021-02-18 13:13:34 +1100

    switch over templates

--- src/app/SBE/detail/company/index.js
+++ src/app/SBE/detail/company/index.js
@@ -1,11 +1,10 @@
 import { useSelector } from 'react-redux'
 import { selectCode, selectRows } from 'redux/db/selectors'
 import { faUser, faBriefcase, faGraduationCap } from '@fortawesome/free-solid-svg-icons'
-import { Divider } from '@chakra-ui/react'
+import { Box, Divider } from '@chakra-ui/react'
 
 import Header from './templates/header'
 import DetailSection from './templates/detail-section'
-import styles from './templates/styles'
 import getActions from 'app/SBE/utils/get-actions'
 
 const contactDetails = {
@@ -45,7 +44,7 @@ const Cv = ({ sbeCode }) => {
   const headerAttribute = 'PRI_NAME'
 
   return (
-    <div style={styles.container}>
+    <Box>
       <Header
         code={code}
         sbeCode={sbeCode}
@@ -59,7 +58,7 @@ const Cv = ({ sbeCode }) => {
       <DetailSection code={code} details={industryDetails} />
       <Divider />
       <DetailSection code={code} details={companyDescription} />
-    </div>
+    </Box>
   )
 }
 
--- src/app/SBE/detail/internship/templates/header.js
+++ src/app/SBE/detail/internship/templates/header.js
@@ -1,30 +1,29 @@
-import { HStack, Text } from '@chakra-ui/react'
+import { Stack, Flex, Spacer, Box } from '@chakra-ui/react'
 import { map } from 'ramda'
 
 import Action from 'app/BE/action'
 import Attribute from 'app/BE/attribute'
-import styles from './styles'
 
 const Header = ({ code, sbeCode, imageSrc, headerAttribute, actions }) => {
   return (
-    <div style={styles.headerContainer}>
-      <div style={styles.headerSectionInfoContainer}>
-        <div style={styles.headerSectionProfile}>
-          <Attribute code={code} attribute={imageSrc} variant={'profile_image'} />
-        </div>
-        <div style={styles.headerSectionName}>
-          <Text fontSize="4xl">
-            <Attribute code={code} attribute={headerAttribute} />
-          </Text>
-        </div>
-      </div>
-      <div style={styles.headerSectionActionContainer}>
-        <HStack>
+    <Flex p="2" alignItems="start">
+      <Attribute code={code} attribute={imageSrc} variant={'profile_image'} />
+      <Box p="3">
+        <Attribute
+          code={code}
+          attribute={headerAttribute}
+          config={{ fontSize: '3xl', fontWeight: 'semibold' }}
+        />
+      </Box>
+
+      <Spacer />
+      <Box mt="6">
+        <Stack direction={['row', 'column']}>
           {actions &&
             map(action => <Action parentCode={sbeCode} code={action} targetCode={code} />)(actions)}
-        </HStack>
-      </div>
-    </div>
+        </Stack>
+      </Box>
+    </Flex>
   )
 }
 
