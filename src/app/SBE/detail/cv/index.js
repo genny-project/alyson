2112fb1cb1e201e2c7227304a6fb36eb748102ac
Barad Ghimire <baradghimire@gmail.com>
2021-02-19 12:53:18 +1100

    Design changes (#15)
    * implementing menu
    
    * refactoring with barad
    
    * Action menu fix
    
    * Phone icon
    
    * changes as per review
    
    * review changes
    
    * refactor
    
    Co-authored-by: c7yrus <c7yrus@gmail.com>

--- src/app/SBE/detail/cv/index.js
+++ src/app/SBE/detail/cv/index.js
@@ -1,57 +1,45 @@
 import { useSelector } from 'react-redux'
 import { selectCode, selectRows } from 'redux/db/selectors'
-import { faUser, faBriefcase, faGraduationCap } from '@fortawesome/free-solid-svg-icons'
 import { Box, Divider } from '@chakra-ui/react'
 
 import Header from './templates/header'
 import DetailSection from './templates/detail-section'
 import getActions from 'app/SBE/utils/get-actions'
-
-const contactDetails = {
-  sectionIcon: faUser,
-  title: 'Contact Details',
-  attributes: ['PRI_PREFERRED_NAME', 'PRI_MOBILE', 'PRI_EMAIL', 'PRI_ADDRESS_FULL'],
-}
-
-const internshipDetail = {
-  sectionIcon: faBriefcase,
-  title: 'Internship Details',
-  attributes: ['PRI_START_DATE', 'PRI_ASSOC_DURATION', 'PRI_TRANSPORT'],
-}
-
-const careerObj = {
-  sectionIcon: faGraduationCap,
-  title: 'Career Objective',
-  attributes: ['PRI_CAREER_OBJ'],
-}
+import sectionAttributes from './utils/section-attributes'
 
 const Cv = ({ sbeCode }) => {
   const sbe = useSelector(selectCode(sbeCode))
+
   const rows = useSelector(selectRows(sbeCode))
 
   if (!sbe || !rows.length) return null
 
-  const code = rows[0]
+  const beCode = rows[0]
   const actions = getActions(sbe)
 
-  const imageAttribute = 'PRI_IMAGE_URL'
-  const headerAttribute = 'PRI_NAME'
+  const {
+    contactDetails,
+    internshipDetail,
+    careerObj,
+    imageAttribute,
+    headerAttribute,
+  } = sectionAttributes
 
   return (
-    <Box>
+    <Box mx={10}>
       <Header
-        code={code}
+        beCode={beCode}
         sbeCode={sbeCode}
         imageSrc={imageAttribute}
         headerAttribute={headerAttribute}
         actions={actions}
       />
       <Divider />
-      <DetailSection code={code} details={contactDetails} />
+      <DetailSection beCode={beCode} details={contactDetails} />
       <Divider />
-      <DetailSection code={code} details={internshipDetail} />
+      <DetailSection beCode={beCode} details={internshipDetail} />
       <Divider />
-      <DetailSection code={code} details={careerObj} />
+      <DetailSection beCode={beCode} details={careerObj} />
     </Box>
   )
 }
---
+++ src/app/SBE/detail/cv/utils/section-attributes.js
@@ -0,0 +1,25 @@
+import { faUser, faBriefcase, faGraduationCap } from '@fortawesome/free-solid-svg-icons'
+
+const sectionAttributes = {
+  imageAttribute: 'PRI_IMAGE_URL',
+  headerAttribute: 'PRI_NAME',
+  contactDetails: {
+    sectionIcon: faUser,
+    title: 'Contact Details',
+    attributes: ['PRI_PREFERRED_NAME', 'PRI_MOBILE', 'PRI_EMAIL', 'PRI_ADDRESS_FULL'],
+  },
+
+  internshipDetail: {
+    sectionIcon: faBriefcase,
+    title: 'Internship Details',
+    attributes: ['PRI_START_DATE', 'PRI_ASSOC_DURATION', 'PRI_TRANSPORT'],
+  },
+
+  careerObj: {
+    sectionIcon: faGraduationCap,
+    title: 'Career Objective',
+    attributes: ['PRI_CAREER_OBJ'],
+  },
+}
+
+export default sectionAttributes
