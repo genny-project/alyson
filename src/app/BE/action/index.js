4b895a75127974105a8a93db04f03b6bf9cd3353
c7yrus <c7yrus@gmail.com>
2021-02-23 09:46:43 +1100

    removing duplicated import

--- src/app/BE/action/index.js
+++ src/app/BE/action/index.js
@@ -1,7 +1,6 @@
 import { useSelector } from 'react-redux'
 import { selectCode } from 'redux/db/selectors'
-import { Button } from '@chakra-ui/react'
-import { MenuItem } from '@chakra-ui/react'
+import { Button, MenuItem } from '@chakra-ui/react'
 import { onSendMessage } from 'vertx'
 
 const Action = ({ parentCode, code, targetCode, size = 'xs', mobile }) => {
