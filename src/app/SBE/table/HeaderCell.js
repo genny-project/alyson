604ed8cd1333ca41a841f9faf793b106893ec819
Barad Ghimire <baradghimire@gmail.com>
2021-02-23 14:38:22 +1100

    Design updates (#22)
    * Remove top border, replace Quidditch icon ;)
    
    * Update darkmode toggle hotkey and icons
    
    * Testing gradients in sandbox
    
    * Update location icon
    
    * Replace caretdown with angledown
    looks slightly nicer IMO
    
    * wip - add up/down arrow to header cell sort
    
    * wip - experimental split of theme
    
    * wip - dev mode concept
    
    * changes
    
    Co-authored-by: slevens <sjlevens@gmail.com>

--- src/app/SBE/table/HeaderCell.js
+++ src/app/SBE/table/HeaderCell.js
@@ -1,8 +1,10 @@
 import { replace } from 'ramda'
-import { Td, Text } from '@chakra-ui/react'
+import { HStack, IconButton, Td, Text } from '@chakra-ui/react'
 import { useSelector } from 'react-redux'
 import { selectCode } from 'redux/db/selectors'
 import { onSendAnswer } from 'vertx'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons'
 
 const Cell = ({ attribute, parentCode }) => {
   const data = useSelector(selectCode(parentCode, attribute))
@@ -10,23 +12,23 @@ const Cell = ({ attribute, parentCode }) => {
 
   return (
     <Td>
-      <Text
-        color={sort && sort.weight === 0 ? 'teal' : ''}
-        cursor={sort ? 'pointer' : null}
-        onClick={
-          sort
-            ? () =>
-                onSendAnswer({
-                  attributeCode: sort.attributeCode,
-                  targetCode: sort.baseEntityCode,
-                  value: sort.value === 'ASC' ? 'DESC' : 'ASC',
-                })
-            : null
-        }
-        fontWeight="semibold"
-      >
-        {data?.attributeName} {sort ? (sort.value === 'ASC' ? '▲' : '▼') : ''}
-      </Text>
+      <HStack spacing="4" color={sort?.weight === 0 ? 'teal' : ''}>
+        <Text fontWeight="semibold">{data?.attributeName}</Text>
+        {sort && (
+          <IconButton
+            variant="ghost"
+            cursor={'pointer'}
+            onClick={() =>
+              onSendAnswer({
+                attributeCode: sort.attributeCode,
+                targetCode: sort.baseEntityCode,
+                value: sort.value === 'ASC' ? 'DESC' : 'ASC',
+              })
+            }
+            icon={<FontAwesomeIcon icon={sort.value === 'ASC' ? faAngleUp : faAngleDown} />}
+          />
+        )}
+      </HStack>
     </Td>
   )
 }
