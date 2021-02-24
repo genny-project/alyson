85fc720b0c2465965e1dd62a9e83b71b7e3e7f8e
slevens <sjlevens@gmail.com>
2021-02-24 14:19:15 +1100

    setting up map

--- src/app/BE/action/index.js
+++ src/app/BE/action/index.js
@@ -3,7 +3,7 @@ import { selectCode } from 'redux/db/selectors'
 import { Button, MenuItem } from '@chakra-ui/react'
 import { onSendMessage } from 'vertx'
 
-const Action = ({ parentCode, code, targetCode, size = 'xs', mobile }) => {
+const Action = ({ parentCode, code, targetCode, size = 'xs', mobile, colorScheme }) => {
   const data = useSelector(selectCode(parentCode, code))
 
   if (!data) return null
@@ -15,18 +15,14 @@ const Action = ({ parentCode, code, targetCode, size = 'xs', mobile }) => {
       targetCode,
     })
 
-  return (
-    <>
-      {mobile ? (
-        <MenuItem onClick={handleClick} test-id={code}>
-          {data.attributeName}
-        </MenuItem>
-      ) : (
-        <Button onClick={handleClick} size={size} test-id={code}>
-          {data.attributeName}
-        </Button>
-      )}
-    </>
+  return mobile ? (
+    <MenuItem onClick={handleClick} test-id={code}>
+      {data.attributeName}
+    </MenuItem>
+  ) : (
+    <Button colorScheme={colorScheme} onClick={handleClick} size={size} test-id={code}>
+      {data.attributeName}
+    </Button>
   )
 }
 
--- src/app/SBE/utils/get-actions.js
+++ src/app/SBE/utils/get-actions.js
@@ -1,6 +1,6 @@
 import { filter, includes } from 'ramda'
 
-const getActions = filter(includes('ACT_'))
+const getActions = filter(v => includes('ACT_', v) && !includes('SCH_', v))
 export const getTableActions = filter(includes('SCH_ACT_'))
 
 export default getActions
