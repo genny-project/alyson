02d263d18e77ae8eb4d70f1f92a75f011a4a8100
c7yrus <c7yrus@gmail.com>
2021-02-24 15:49:32 +1100

    error handling and file checking

--- src/app/DTT/upload/dropzone.js
+++ src/app/DTT/upload/dropzone.js
@@ -1,15 +1,28 @@
 import { useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import { Box, Flex, Text, Input, Button, Center, Image, useToast } from '@chakra-ui/react'
-import { isEmpty, map } from 'ramda'
+import { isEmpty, map, pathOr, compose, includes, split } from 'ramda'
 
-const DropZone = ({ handleSave, closeDropzone, maxSize = '5000000' }) => {
+const DropZone = ({ handleSave, closeDropzone, maxSize = '5000000', maxFiles = 1 }) => {
   const [files, setFiles] = useState([])
   const toast = useToast()
+  const checkIfImage = compose(includes('image'), split('/'))
+
+  const showErrorMessage = file => {
+    const errorMessage = pathOr('', ['errors', 0, 'message'])(file)
+    return toast({
+      position: 'bottom',
+      title: 'An error occurred.',
+      description: errorMessage,
+      status: 'error',
+      duration: 3000,
+    })
+  }
 
   const { getRootProps, getInputProps } = useDropzone({
-    accept: 'image/*',
+    accept: 'image/*, application/pdf',
     maxSize: maxSize,
+    maxFiles: maxFiles,
     onDrop: (acceptedFiles, rejectedFiles) => {
       setFiles(
         acceptedFiles.map(file =>
@@ -17,29 +30,34 @@ const DropZone = ({ handleSave, closeDropzone, maxSize = '5000000' }) => {
             preview: URL.createObjectURL(file),
           }),
         ),
-        rejectedFiles.map(file =>
-          toast({
-            position: 'bottom',
-            title: 'An error occurred.',
-            description: `There was an error, try again!`,
-            status: 'error',
-            duration: 3000,
-          }),
-        ),
+        !isEmpty(rejectedFiles) && showErrorMessage(rejectedFiles[0]),
       )
     },
   })
 
-  const preview = map(file => (
-    <Box w="200px" h="200px" key={file.name}>
-      <Image src={file.preview} alt="drag and drop" boxSize="200px" objectFit="cover" />
-    </Box>
-  ))(files)
+  const preview = map(({ name, preview, type }) => {
+    if (checkIfImage(type)) {
+      return (
+        <Box w="200px" h="200px" key={name} r="5">
+          <Image src={preview} alt="drag and drop" boxSize="200px" objectFit="cover" />
+        </Box>
+      )
+    }
+
+    return (
+      <Flex key={name} p="4" background="teal.50">
+        <Text color="black">{`Uploaded File:`}</Text>
+        <Text color="black" ml="2" fontWeight="bold">
+          {name}
+        </Text>
+      </Flex>
+    )
+  })(files)
 
   return (
     <Flex w="100%" p={4} direction="column" bg="white">
       <Text color="black">{`Upload File`}</Text>
-      <Box {...getRootProps()} p="10" my="4" border="1px black dashed">
+      <Box {...getRootProps()} p="10" my="4" cursor="pointer" border="1px black dashed">
         <Center>
           <Text color="black">{`Drag 'n' drop some files here, or click to select files`}</Text>
         </Center>
@@ -65,4 +83,5 @@ const DropZone = ({ handleSave, closeDropzone, maxSize = '5000000' }) => {
     </Flex>
   )
 }
+
 export default DropZone
