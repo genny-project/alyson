6f46cef95b5dc74a97c7ab3df7e86cecc734ec35
Barad Ghimire <baradghimire@gmail.com>
2021-02-28 18:04:13 +1100

    1998

--- src/error/index.js
+++ src/error/index.js
@@ -1,14 +1,32 @@
-import { Center, Heading, VStack } from '@chakra-ui/react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons'
+import '98.css'
 
 const Error = () => (
-  <Center h="90vh">
-    <VStack spacing="15">
-      <FontAwesomeIcon size="4x" icon={faExclamationCircle} color="red" />
-      <Heading>Sorry our server is not responding, try again later!</Heading>
-    </VStack>
-  </Center>
+  <div
+    style={{
+      height: '90vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <div style={{ width: 300 }} className="window">
+      <div className="title-bar">
+        <div className="title-bar-text">oh-no.exe</div>
+        <div className="title-bar-controls">
+          <button aria-label="Minimize" />
+          <button aria-label="Maximize" />
+          <button aria-label="Close" />
+        </div>
+      </div>
+      <div className="window-body">
+        <p style={{ textAlign: 'center' }}>Sorry, our servers are not responding right now.</p>
+        <p style={{ textAlign: 'center' }}>Please try again later!</p>
+        <div className="field-row" style={{ justifyContent: 'center' }}>
+          <button onClick={() => window.location.reload()}>Retry</button>
+        </div>
+      </div>
+    </div>
+  </div>
 )
 
 export default Error
