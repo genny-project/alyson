186d4b36bf1727a1cafbc1b6a2a326e30358d82a
c7yrus <c7yrus@gmail.com>
2021-03-02 16:08:30 +1100

    adding some more tests

--- src/utils/helpers/set-year-for-date.js
+++ src/utils/helpers/set-year-for-date.js
@@ -3,13 +3,23 @@ import { compose, split, prop, join, update, adjust, subtract, length, __ } from
 const setYearForDateDefault = compose(
   join('-'),
   adjust(0, subtract(__, 100)),
+  update(1, '01'),
+  update(2, '01'),
   split('-'),
   prop(0),
   split('T'),
 )(new Date().toISOString())
 
 const setYearForDateUsingValue = value =>
-  compose(join('-'), update(0, value), split('-'), prop(0), split('T'))(new Date().toISOString())
+  compose(
+    join('-'),
+    update(0, value),
+    update(1, '01'),
+    update(2, '01'),
+    split('-'),
+    prop(0),
+    split('T'),
+  )(new Date().toISOString())
 
 const setYearForDate = value => {
   if (value && length(value) === 4) {
