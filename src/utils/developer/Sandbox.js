281c1880932d3a8721e12acefa41ad262b08ae98
🤖 <baradghimire@gmail.com>
2021-03-18 14:14:48 +1100

    wip - mini contact card

--- src/utils/developer/Sandbox.js
+++ src/utils/developer/Sandbox.js
@@ -3,6 +3,7 @@ import {
   Avatar,
   Box,
   Center,
+  IconButton,
   Image,
   Flex,
   Text,
@@ -10,27 +11,21 @@ import {
   Button,
   useColorModeValue,
 } from '@chakra-ui/react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhoneAlt, faEnvelope, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
 
 const Sandbox = () => {
   return (
     <Center py={6}>
       <Box
-        maxW={'270px'}
+        maxW={'300px'}
         w={'full'}
         bg={useColorModeValue('white', 'gray.800')}
         boxShadow={'2xl'}
         rounded={'md'}
         overflow={'hidden'}
       >
-        <Image
-          h={'120px'}
-          w={'full'}
-          src={
-            'https://images.unsplash.com/photo-1612865547334-09cb8cb455da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
-          }
-          objectFit={'cover'}
-        />
-        <Flex justify={'center'} mt={-12}>
+        <Flex justify={'center'} mt={8}>
           <Avatar
             size={'xl'}
             src={
@@ -42,40 +37,68 @@ const Sandbox = () => {
             }}
           />
         </Flex>
-        <Box p={6}>
-          <Stack spacing={0} align={'center'} mb={5}>
-            <Heading fontSize={'2xl'} fontWeight={500} fontFamily={'body'}>
-              John Doe
+        <Box p={4}>
+          <Stack spacing={0} align={'center'} mb={4}>
+            <Heading fontSize={'2xl'} fontWeight={600} fontFamily={'body'}>
+              John
             </Heading>
-            <Text color={'gray.500'}>Frontend Developer</Text>
+            <Text color={'gray.500'}>Jonathan Lorem Doe</Text>
           </Stack>
           <Stack direction={'row'} justify={'center'} spacing={6}>
             <Stack spacing={0} align={'center'}>
-              <Text fontWeight={600}>23k</Text>
+              <Text fontWeight={500}>Developer</Text>
               <Text fontSize={'sm'} color={'gray.500'}>
-                Followers
+                Role
               </Text>
             </Stack>
             <Stack spacing={0} align={'center'}>
-              <Text fontWeight={600}>23k</Text>
+              <Text fontWeight={500}> Outcome.Life</Text>
               <Text fontSize={'sm'} color={'gray.500'}>
-                Followers
+                Company
               </Text>
             </Stack>
           </Stack>
-          <Button
-            w={'full'}
-            mt={8}
-            bg={useColorModeValue('#151f21', 'gray.900')}
-            color={'white'}
-            rounded={'md'}
-            _hover={{
-              transform: 'translateY(-2px)',
-              boxShadow: 'lg',
-            }}
-          >
-            Follow
-          </Button>
+          <Stack direction={'row'} justify={'center'} spacing={6} m={8}>
+            <Stack spacing={0} align={'center'}>
+              <IconButton
+                transition="all 0.2s"
+                bgGradient={'linear(to-br, teal.400,blue.500)'}
+                color={'white'}
+                rounded={'md'}
+                _hover={{
+                  transform: 'translateY(-2px)',
+                  boxShadow: 'lg',
+                }}
+                icon={<FontAwesomeIcon icon={faPhoneAlt} />}
+              />
+            </Stack>
+            <Stack spacing={0} align={'center'}>
+              <IconButton
+                transition="all 0.2s"
+                bgGradient={'linear(to-br, teal.400,blue.500)'}
+                color={'white'}
+                rounded={'md'}
+                _hover={{
+                  transform: 'translateY(-2px)',
+                  boxShadow: 'lg',
+                }}
+                icon={<FontAwesomeIcon icon={faEnvelope} />}
+              />
+            </Stack>
+            <Stack spacing={0} align={'center'}>
+              <IconButton
+                transition="all 0.2s"
+                bgGradient={'linear(to-br, teal.400,blue.500)'}
+                color={'white'}
+                rounded={'md'}
+                _hover={{
+                  transform: 'translateY(-2px)',
+                  boxShadow: 'lg',
+                }}
+                icon={<FontAwesomeIcon icon={faMapMarkerAlt} />}
+              />
+            </Stack>
+          </Stack>
         </Box>
       </Box>
     </Center>
