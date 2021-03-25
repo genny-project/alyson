9a6438bde836f0a6680013d3329c166f9aabf3e1
slevens <sjlevens@gmail.com>
2021-03-25 15:00:18 +1100

    sig canvas messery

--- src/app/DTT/signature/index.js
+++ src/app/DTT/signature/index.js
@@ -16,7 +16,6 @@ const Write = ({ questionCode, data, onSendAnswer }) => {
       ctx.clearRect(0, 0, canvas.width, canvas.height)
       ctx.font = '40px Homemade Apple'
       ctx.fillText(text, canvas.width / 6, canvas.height / 1.8)
-      onSendAnswer(canvas.toDataURL())
     }
   }, [onSendAnswer, text])
 
@@ -30,27 +29,34 @@ const Write = ({ questionCode, data, onSendAnswer }) => {
     onSendAnswer(null)
   }
 
+  useEffect(() => {
+    if (data?.value) {
+      const sigCanvas = signatureRef.current.getCanvas()
+      const img = new window.Image()
+      img.addEventListener('load', function () {
+        sigCanvas.getContext('2d').drawImage(img, 0, 0)
+      })
+      img.setAttribute('src', data.value)
+    }
+  }, [data.value])
+
   return (
     <VStack>
       <Text>Start typing your name, or draw on the pad</Text>
-      <Input w="500px" value={text} onChange={e => setText(e.target.value)} />
-
-      <Box
-        hidden={!text}
-        style={{
-          boxShadow: '20px 20px 60px #bebebe, -20px -20px 60px #ffffff',
-          borderRadius: '50px',
+      <Input
+        onBlur={() => {
+          const canvas = canvasRef.current
+          onSendAnswer(canvas.toDataURL())
         }}
-      >
+        w="500px"
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+
+      <Box hidden={!text} boxShadow="md">
         <canvas ref={canvasRef} width="500" height="200" />
       </Box>
-      <Box
-        hidden={!!text}
-        style={{
-          boxShadow: '20px 20px 60px #bebebe, -20px -20px 60px #ffffff',
-          borderRadius: '50px',
-        }}
-      >
+      <Box hidden={!!text} boxShadow="md">
         <IconButton
           position="absolute"
           variant="unstyled"
