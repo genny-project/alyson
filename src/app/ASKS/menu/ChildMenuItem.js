0b7a97a94d6cd2827122cc00c53c5fa024d4c8fc
slevens <sjlevens@gmail.com>
2021-03-29 14:06:12 +1100

    mobile nav

--- src/app/ASKS/menu/ChildMenuItem.js
+++ src/app/ASKS/menu/ChildMenuItem.js
@@ -3,14 +3,17 @@ import { selectCode } from 'redux/db/selectors'
 import sendAskClick from 'app/ASKS/utils/send-ask-click'
 import { MenuItem } from '@chakra-ui/react'
 
-const ChildMenuItem = ({ questionCode, childCode, rootCode, targetCode }) => {
+const ChildMenuItem = ({ onClose, questionCode, childCode, rootCode, targetCode }) => {
   const data = useSelector(selectCode(questionCode, childCode))
 
   if (!data) return null
 
   const { name } = data
 
-  const onClick = () => sendAskClick(questionCode, childCode, rootCode, targetCode)
+  const onClick = () => {
+    sendAskClick(questionCode, childCode, rootCode, targetCode)
+    typeof onClose === 'function' && onClose()
+  }
 
   return (
     <MenuItem test-id={childCode} onClick={onClick}>
