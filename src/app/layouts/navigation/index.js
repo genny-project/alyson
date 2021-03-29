2663652562905884bb7dd9a468ed7c486545d07f
slevens <sjlevens@gmail.com>
2021-03-29 13:56:36 +1100

    nav mobile

--- src/app/layouts/navigation/index.js
+++ src/app/layouts/navigation/index.js
@@ -1,20 +1,17 @@
 import { useColorModeValue } from '@chakra-ui/react'
-import { apiConfig } from 'config/get-api-config'
-import MobileNav from './mobile'
+import { useIsMobile } from 'utils/hooks'
 
-const getLogo = (realm, lightMode) =>
-  realm === 'internmatch'
-    ? lightMode
-      ? 'https://internmatch-uploads.s3-ap-southeast-2.amazonaws.com/internmatch_logo_light.png'
-      : 'https://internmatch-uploads.s3-ap-southeast-2.amazonaws.com/internmatch_logo_dark.png'
-    : 'https://raw.githubusercontent.com/OutcomeLife/prj_mentormatch/7.17.0/docs/mentormatch_logo.png'
+import MobileNav from './mobile'
+import DesktopNav from './desktop'
+
+const getLogo = lightMode =>
+  lightMode ? '/internmatch_logo_light.png' : '/internmatch_logo_dark.png'
 
 const Navigation = () => {
-  const { realm } = apiConfig
+  const logoSrc = useColorModeValue(getLogo(true), getLogo())
+  const isMobile = useIsMobile()
 
-  const logoSrc = useColorModeValue(getLogo(realm, true), getLogo(realm))
-
-  return <MobileNav logoSrc={logoSrc} />
+  return isMobile ? <MobileNav logoSrc={logoSrc} /> : <DesktopNav logoSrc={logoSrc} />
 }
 
 export default Navigation
