b806720ec326ea42c1e20516239fbdbb322bafc8
slevens <sjlevens@gmail.com>
2021-03-31 14:21:52 +1100

    notes

--- src/app/NOTE/tab_top/index.js
+++ src/app/NOTE/tab_top/index.js
@@ -1,11 +1,19 @@
 import { Text, VStack } from '@chakra-ui/layout'
 import { Tab } from '@chakra-ui/tabs'
 import ImageType from 'app/DTT/upload/Image'
+import { onSendMessage } from 'vertx'
 
-const TabTop = ({ tab: { image, title: name }, title }) => {
+const TabTop = ({ tab: { image, title: name, code }, title, rootCode }) => {
+  const handleClick = () =>
+    onSendMessage({
+      code: 'ACT_PRI_EVENT_ACCESS_NOTES',
+      targetCode: code,
+      rootCode,
+    })
+
   return (
     <Tab>
-      <VStack align="start" w="12rem">
+      <VStack onClick={handleClick} align="start" w="12rem">
         <ImageType.Read data={{ value: image }} />
         <Text isTruncated>{name}</Text>
         <Text textStyle="body3">{title}</Text>
