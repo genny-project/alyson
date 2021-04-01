0627d0918a5350d0eb9b32e0c784805afbbf0f11
slevens <sjlevens@gmail.com>
2021-04-01 11:41:21 +1100

    Update index.js

--- src/keycloak/loading/index.js
+++ src/keycloak/loading/index.js
@@ -1,14 +1,10 @@
-import { Box } from '@chakra-ui/layout'
-import { Progress } from '@chakra-ui/progress'
-import LoadingText from './loading-text'
+import { Center } from '@chakra-ui/layout'
+import { CircularProgress } from '@chakra-ui/progress'
 
 const Loading = () => (
-  <div>
-    <Progress w="full" isIndeterminate />
-    <Box position="fixed" top="50%" left="calc(50% - 13rem)">
-      <LoadingText />
-    </Box>
-  </div>
+  <Center h="100vh">
+    <CircularProgress isIndeterminate trackColor="teal.400" color="blue.500" />
+  </Center>
 )
 
 export default Loading
