cfd14407fd197ff19f2343aa4fb24242cca10cd2
slevens <sjlevens@gmail.com>
2021-04-06 17:22:59 +1000

    tidy

--- src/app/layouts/process/index.js
+++ src/app/layouts/process/index.js
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux'
 import { selectProcess } from 'redux/app/selectors'
 import Lane from 'app/SBE/lane'
-import { HStack } from '@chakra-ui/react'
+import { Box, HStack } from '@chakra-ui/react'
 import Search from 'app/SBE/search/Search'
 
 const Process = ({ dashboard }) => {
@@ -10,7 +10,11 @@ const Process = ({ dashboard }) => {
   if (!processCodes) return null
   return (
     <div>
-      {!dashboard && <Search sbeCode={JSON.stringify(processCodes)} />}
+      {!dashboard && (
+        <Box ml="1rem">
+          <Search sbeCode={JSON.stringify(processCodes)} />
+        </Box>
+      )}
       <HStack align="flex-start" p="5" spacing="6">
         {processCodes.map(sbeCode => (
           <Lane key={sbeCode} sbeCode={sbeCode} dashboard={dashboard} />
