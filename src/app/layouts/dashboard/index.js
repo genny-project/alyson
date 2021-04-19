1bfd9390b62be41fd513512b5cf0a394e6bcfaa9
slevens <sjlevens@gmail.com>
2021-04-19 17:02:42 +1000

    Update index.js

--- src/app/layouts/dashboard/index.js
+++ src/app/layouts/dashboard/index.js
@@ -1,25 +1,16 @@
 import { useSelector } from 'react-redux'
 import { selectDashboard, selectDashboardCounts } from 'redux/app/selectors'
 import DisplaySbe from 'app/SBE'
-import { Center, HStack, Stack, Spacer, Image, useColorModeValue } from '@chakra-ui/react'
-import { apiConfig } from 'config/get-api-config'
+import { Center, HStack, Stack, Spacer } from '@chakra-ui/react'
 
 const Dashboard = () => {
   const dashboardSbes = useSelector(selectDashboard)
   const dashboardCounts = useSelector(selectDashboardCounts)
-  const { realm } = apiConfig
-  const logoSrc = useColorModeValue(
-    'https://internmatch-uploads.s3-ap-southeast-2.amazonaws.com/internmatch_logo_light.png',
-    'https://internmatch-uploads.s3-ap-southeast-2.amazonaws.com/internmatch_logo_dark.png',
-  )
 
   if (!dashboardSbes) return <div />
   return (
     <Center m="4">
       <Stack direction="column" spacing="10" h="84vh">
-        {apiConfig && realm === 'mentormatch' && (
-          <Image src={logoSrc} style={{ cursor: 'pointer' }} htmlWidth="250px" />
-        )}
         <HStack spacing={4}>
           {dashboardCounts &&
             dashboardCounts.map(sbeCode => <DisplaySbe key={sbeCode} sbeCode={sbeCode} />)}
