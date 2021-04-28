0d24ee42e8e3eb32862cf4b7851213de97f5792d
slevens <sjlevens@gmail.com>
2021-04-28 10:32:08 +1000

    Update AgentDetail.js

--- src/app/BE/card/templates/AgentDetail.js
+++ src/app/BE/card/templates/AgentDetail.js
@@ -1,13 +1,15 @@
 import { Flex, HStack, Spacer, Text } from '@chakra-ui/layout'
 import ImageType from 'app/DTT/upload/Image'
+import { always, equals } from 'ramda'
 import { useSelector } from 'react-redux'
 import { selectAttributes, selectCode } from 'redux/db/selectors'
+import sameLength from 'redux/utils/same-length'
 import getUserType from 'utils/helpers/get-user-type'
 
 const AgentDetail = ({ code, parentCode }) => {
-  const userCode = useSelector(selectCode('USER'))
+  const userCode = useSelector(selectCode('USER'), equals)
 
-  const userType = getUserType(useSelector(selectCode(userCode)))
+  const userType = getUserType(useSelector(selectCode(userCode), sameLength))
 
   const [agentName, agentImage, agentCode] = useSelector(
     selectAttributes(code, [
@@ -15,6 +17,7 @@ const AgentDetail = ({ code, parentCode }) => {
       'PRI_LNK_AGENT__PRI_IMAGE_URL',
       'LNK_AGENT',
     ]),
+    always(true),
   )
 
   const agentPerCode = agentCode?.value
