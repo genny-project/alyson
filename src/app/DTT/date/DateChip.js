b7bcff6ecb32cfe82f847baa7f5433617603de34
🤖 <baradghimire@gmail.com>
2021-04-30 09:41:56 +1000

    Experimental - Replace existing DateChip with new Chip component

--- src/app/DTT/date/DateChip.js
+++ src/app/DTT/date/DateChip.js
@@ -1,15 +1,10 @@
-import { Badge, HStack, Text } from '@chakra-ui/layout'
-import { faTimes } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import timeBasedOnTimeZone from 'utils/helpers/timezone_magic/time-based-on-timezone'
+import Chip from 'app/layouts/components/chip'
 
 const DateChip = ({ onClick, date, includeTime, onlyYear }) => (
-  <Badge colorScheme="purple" cursor="pointer" onClick={onClick}>
-    <HStack padding="1">
-      <FontAwesomeIcon icon={faTimes} />
-      <Text>{timeBasedOnTimeZone(date, { includeTime, onlyYear })}</Text>
-    </HStack>
-  </Badge>
+  <Chip variant="secondary" onClick={onClick}>
+    {timeBasedOnTimeZone(date, { includeTime, onlyYear })}
+  </Chip>
 )
 
 export default DateChip
