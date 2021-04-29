3d8f714439f4c12c80d0110080dc0e0caf91eee0
🤖 <baradghimire@gmail.com>
2021-04-29 11:03:39 +1000

    Quick and dirty example in Sandbox

--- src/utils/developer/Sandbox.js
+++ src/utils/developer/Sandbox.js
@@ -1,11 +1,16 @@
 import { Center } from '@chakra-ui/react'
 import Button from '../../app/layouts/components/button'
+import Card from '../../app/layouts/components/card'
 import ThemeToggler from '../../app/layouts/navigation/ColorToggler'
 
 const Sandbox = () => {
   return (
     <Center m={2}>
       <ThemeToggler></ThemeToggler>
+      <Card m={2} w={20} h={20} variant="card0"></Card>
+      <Card m={2} w={20} h={20} variant="card1"></Card>
+      <Card m={2} w={20} h={20} variant="card2"></Card>
+      <Card m={2} w={20} h={20} variant="card3"></Card>
       <Button m={2} variant="special">
         Special
       </Button>
