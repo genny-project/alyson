e197c1d666cd50181a6785d338fc528761943c10
c7yrus <c7yrus@gmail.com>
2021-05-03 16:09:58 +1000

    styling done

--- src/app/ASKS/form/desktop_view/template/Body.js
+++ src/app/ASKS/form/desktop_view/template/Body.js
@@ -5,7 +5,7 @@ import Card from 'app/layouts/components/card'
 const FormBody = ({ groups, onFinish, questionCode }) => {
   return groups.map(({ label, questions }, idx) => (
     <Card key={label} w="full">
-      <VStack spacing="8" align="start">
+      <VStack align="start" spacing={8}>
         {groups.hasOwnProperty(idx) && <Text textStyle="head1">{groups[idx]?.label}</Text>}
         {questions.map(code => (
           <Ask onFinish={onFinish} key={code} parentCode={questionCode} questionCode={code} />
