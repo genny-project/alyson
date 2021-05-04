c47c6a23ff97b98ef35318a378810337b5f8c291
c7yrus <c7yrus@gmail.com>
2021-05-04 15:11:41 +1000

    comfirm action implementation

--- src/app/BE/context/Action.js
+++ src/app/BE/context/Action.js
@@ -1,24 +1,44 @@
+import { MenuItem } from '@chakra-ui/react'
+import { includes } from 'ramda'
+
 import { useSelector } from 'react-redux'
 import { selectCode } from 'redux/db/selectors'
-import { MenuItem } from '@chakra-ui/react'
 import { onSendMessage } from 'vertx'
 
-const Action = ({ parentCode, code, targetCode }) => {
+const Action = ({
+  parentCode,
+  code,
+  targetCode,
+  config = [
+    'ACT_PRI_EVENT_UNARCHIVE_INTERN',
+    'ACT_PRI_EVENT_VIEW_APPLICATIONS',
+    'ACT_PRI_EVENT_VIEW',
+  ],
+}) => {
   const data = useSelector(selectCode(parentCode, code))
-
   if (!data) return null
 
-  const handleClick = () =>
-    onSendMessage({
-      parentCode,
-      code,
-      targetCode,
-    })
+  const handleClick = (code, data) => {
+    includes(code)(config)
+      ? window.confirm(`Are you sure you want to ${data.attributeName}?`) &&
+        onSendMessage({
+          parentCode,
+          code,
+          targetCode,
+        })
+      : onSendMessage({
+          parentCode,
+          code,
+          targetCode,
+        })
+  }
 
   return (
-    <MenuItem test-id={code} onClick={handleClick}>
-      {data.attributeName}
-    </MenuItem>
+    <div>
+      <MenuItem test-id={code} onClick={() => handleClick(code, data)}>
+        {data.attributeName}
+      </MenuItem>
+    </div>
   )
 }
 
