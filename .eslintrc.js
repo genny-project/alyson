ee13acc03b93c29f6732e1a07af2526209dd1bbb
🤖 <baradghimire@gmail.com>
2021-05-06 23:33:31 +1000

    Update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  extends: ['react-app', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
