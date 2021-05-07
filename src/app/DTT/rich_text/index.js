5845670ebb780a98084eb0faf4864b38852555c5
slevens <sjlevens@gmail.com>
2021-05-07 11:06:48 +1000

    html edit

--- src/app/DTT/rich_text/index.js
+++ src/app/DTT/rich_text/index.js
@@ -16,7 +16,7 @@ import { EditorState, convertFromHTML, ContentState } from 'draft-js'
 import { Editor } from 'react-draft-wysiwyg'
 import { stateToHTML } from 'draft-js-export-html'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit, faExpand } from '@fortawesome/free-solid-svg-icons'
+import { faExpand } from '@fortawesome/free-solid-svg-icons'
 import '../../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
 import DOMPurify from 'dompurify'
 import safelyParseJson from 'utils/helpers/safely-parse-json'
@@ -30,7 +30,6 @@ const Write = ({ questionCode, data, onSendAnswer, description, html }) => {
   )
 
   const [dataValue, setDataValue] = useState(data?.value)
-  const [edit, setEdit] = useState(!data?.value)
   const [editor, setEditor] = useState(
     !data?.value ? EditorState.createEmpty() : EditorState.createWithContent(state),
   )
@@ -38,10 +37,9 @@ const Write = ({ questionCode, data, onSendAnswer, description, html }) => {
   useEffect(() => {
     if (data?.value !== dataValue) {
       setDataValue(data?.value)
-      setEdit(false)
       setEditor(EditorState.createWithContent(state))
     }
-  }, [data?.value, dataValue, edit, state])
+  }, [data?.value, dataValue, state])
 
   const curLength = (stateToHTML(editor.getCurrentContent()) || '')
     .replace(/(<([^>]+)>)/gi, '')
@@ -51,70 +49,50 @@ const Write = ({ questionCode, data, onSendAnswer, description, html }) => {
     if (minCharacterCount || maxCharacterCount) {
       if (minCharacterCount < curLength && curLength < maxCharacterCount) {
         onSendAnswer(stateToHTML(editor.getCurrentContent()))
-        setEdit(false)
       }
     } else {
       onSendAnswer(stateToHTML(editor.getCurrentContent()))
-      setEdit(false)
     }
   }
 
-  return edit ? (
-    <Box
-      test-id={questionCode}
-      w="100%"
-      border="1px solid #E2E8F0"
-      borderRadius="0.375rem"
-      p="1rem"
-    >
-      {minCharacterCount ? (
-        <HStack>
-          <Text>{`Please use between`}</Text>
-          <Text color={curLength < minCharacterCount ? 'red' : 'green'}>{minCharacterCount}</Text>
-          <Text>{`and`}</Text>
-          <Text color={curLength > maxCharacterCount ? 'red' : 'green'}>{maxCharacterCount}</Text>
-          <Text>{`characters`}</Text>
-        </HStack>
-      ) : null}
-      {minCharacterCount ? (
-        <Text>
-          {minCharacterCount > curLength
-            ? `Keep typing please`
-            : curLength > maxCharacterCount
-            ? `Too much text`
-            : `That's perfect, thanks!`}
-        </Text>
-      ) : null}
+  return <Box
+  test-id={questionCode}
+  w="100%"
+  border="1px solid #E2E8F0"
+  borderRadius="0.375rem"
+  p="1rem"
+>
+  {minCharacterCount ? (
+    <HStack>
+      <Text>{`Please use between`}</Text>
+      <Text color={curLength < minCharacterCount ? 'red' : 'green'}>{minCharacterCount}</Text>
+      <Text>{`and`}</Text>
+      <Text color={curLength > maxCharacterCount ? 'red' : 'green'}>{maxCharacterCount}</Text>
+      <Text>{`characters`}</Text>
+    </HStack>
+  ) : null}
+  {minCharacterCount ? (
+    <Text>
+      {minCharacterCount > curLength
+        ? `Keep typing please`
+        : curLength > maxCharacterCount
+        ? `Too much text`
+        : `That's perfect, thanks!`}
+    </Text>
+  ) : null}
 
-      <Editor
-        toolbar={{
-          options: ['fontSize', 'fontFamily', 'list', 'textAlign'],
-        }}
-        editorState={editor}
-        onEditorStateChange={setEditor}
-        placeholder={description}
-        onBlur={handleSave}
-        spellCheck={true}
-        lang="en"
-      />
-    </Box>
-  ) : (
-    <Box w="2xl" border="1px solid #E2E8F0" borderRadius="0.375rem" p="1rem">
-      <div
-        test-id={questionCode + '-saved'}
-        style={{ padding: '1rem' }}
-        dangerouslySetInnerHTML={{ __html: data.value }}
-      />
-      <Button
-        test-id={questionCode + '-edit'}
-        m="2"
-        leftIcon={<FontAwesomeIcon icon={faEdit} />}
-        onClick={() => setEdit(true)}
-      >
-        Edit
-      </Button>
-    </Box>
-  )
+  <Editor
+    toolbar={{
+      options: ['list', 'textAlign'],
+    }}
+    editorState={editor}
+    onEditorStateChange={setEditor}
+    placeholder={description}
+    onBlur={handleSave}
+    spellCheck={true}
+    lang="en"
+  />
+</Box>
 }
 const Read = ({ data, mini }) => {
   if (!data?.value) return null
