6c0839d6fafe8b54018e4e50dcae4466a8e262d8
slevens <sjlevens@gmail.com>
2021-05-12 13:45:38 +1000

    Update Submit.js

--- src/app/DTT/button/Submit.js
+++ src/app/DTT/button/Submit.js
@@ -1,28 +1,19 @@
-import { Box, Button, Text, VStack } from '@chakra-ui/react'
+import { Box, Button, Tag, Text, VStack, Wrap, WrapItem } from '@chakra-ui/react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { onSendMessage } from 'vertx'
 import { useState } from 'react'
-import {
-  // useDispatch,
-  useSelector,
-} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { selectAttributes, selectCode } from 'redux/db/selectors'
-import {
-  // compose,
-  filter,
-  identity,
-  map,
-  prop,
-} from 'ramda'
-// import { highlightQuestion } from 'redux/app'
+import { compose, equals, filter, identity, includes, map, prop } from 'ramda'
+import { highlightQuestion } from 'redux/app'
 
 const Submit = ({ askData, onFinish, parentCode }) => {
   const { questionCode, targetCode, name, disabled } = askData
 
-  // const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-  // const onHighlightQuestion = compose(dispatch, highlightQuestion)
+  const onHighlightQuestion = compose(dispatch, highlightQuestion)
 
   const questions = useSelector(selectCode(parentCode))
   const questionDatas = useSelector(selectAttributes(parentCode, questions))
@@ -32,9 +23,15 @@ const Submit = ({ askData, onFinish, parentCode }) => {
     identity,
     useSelector(selectAttributes(targetCode, mandatoryAttributes)),
   )
-  const mandatoryAttributesNoValue = filter(attr => !attr.value, attributeData)
+  const mandatoryAttributesNoValue = compose(
+    map(prop('attributeCode')),
+    filter(attr => !attr.value),
+  )(attributeData)
 
-  console.log(mandatoryAttributesNoValue)
+  const mandatoryQuestionsNoValue = filter(
+    q => q.questionCode !== 'QUE_SUBMIT' && !includes(mandatoryAttributesNoValue, q.attributeCode),
+  )(mandatoryQuestions)
+
   const [loading, setLoading] = useState(false)
 
   const onClick = () => {
@@ -55,28 +52,28 @@ const Submit = ({ askData, onFinish, parentCode }) => {
     <Box>
       <VStack pb="1rem" align="start" visibility={disabled ? 'visible' : 'hidden'}>
         <Text textStyle="body.error">Please complete all questions marked as mandatory with *</Text>
-        {/* <Text>These questions still need to be answered, click to scroll.</Text>
+        <Text>These questions still need to be answered, click to scroll.</Text>
         <Wrap align="start">
-          {mandatoryAttributesNoValue.map(attr => (
-            <WrapItem key={attr.attributeCode}>
+          {mandatoryQuestionsNoValue.map(question => (
+            <WrapItem key={question.attributeCode}>
               <Tag
                 opacity="0.7"
                 _hover={{ opacity: '1' }}
                 colorScheme="red"
                 cursor="pointer"
                 onClick={() => {
-                  const el = document.getElementById(attr.attributeCode)
+                  const el = document.getElementById(question.attributeCode)
                   if (!el) return
-                  onHighlightQuestion(attr.attributeCode)
+                  onHighlightQuestion(question.attributeCode)
                   el.scrollIntoView({ behavior: 'smooth', block: 'center' })
                 }}
                 transition="all 0.2s"
               >
-                {attr.attributeName}
+                {question.name}
               </Tag>
             </WrapItem>
           ))}
-        </Wrap> */}
+        </Wrap>
       </VStack>
       <Button
         isLoading={loading}
