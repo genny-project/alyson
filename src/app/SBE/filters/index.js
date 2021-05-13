c95cb700aff6444d73c6e1817f400cd17ef75ff7
slevens <sjlevens@gmail.com>
2021-05-13 11:25:17 +1000

    buggies

--- src/app/SBE/filters/index.js
+++ src/app/SBE/filters/index.js
@@ -14,12 +14,20 @@ import Ask from 'app/ASKS/ask'
 import ExistingFilters from './existing_filters'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFilter } from '@fortawesome/free-solid-svg-icons'
+import { useRef } from 'react'
 
 const Filters = ({ sbeCode }) => {
+  const triggerRef = useRef()
   const filterGrp = `QUE_FILTER_GRP_${sbeCode}`
   const [addFilters, existingFilters] = useSelector(
     selectAttributes(filterGrp, ['QUE_ADD_FILTER_GRP', 'QUE_EXISTING_FILTERS_GRP']),
   )
+
+  const onToggle = () => {
+    if (triggerRef.current) {
+      triggerRef.current.click()
+    }
+  }
 
   if (!addFilters) return null
 
@@ -28,6 +36,7 @@ const Filters = ({ sbeCode }) => {
       <Popover isLazy>
         <PopoverTrigger test-id={'filters'}>
           <Button
+            ref={triggerRef}
             leftIcon={<FontAwesomeIcon icon={faFilter} />}
             colorScheme="primary"
             variant="outline"
@@ -44,6 +53,7 @@ const Filters = ({ sbeCode }) => {
                   passedTargetCode={sbeCode}
                   key={childAskObject.attributeCode}
                   passedAskData={childAskObject}
+                  onFinish={onToggle}
                 />
               ))}
             </VStack>
