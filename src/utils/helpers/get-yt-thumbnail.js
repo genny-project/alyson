763a01b9ccee4ffdef1bcba1b202d3f04524af1d
slevens <sjlevens@gmail.com>
2021-05-17 10:35:27 +1000

    Revert "Merge branch 'internmatch' into mentormatch"
    This reverts commit 69f33adbe5487e66e75aa466c4171491105cb47e, reversing
    changes made to 6ff88c0df3b374ff1ed024ef0ab13704d7b5e3a2.

--- src/utils/helpers/get-yt-thumbnail.js
+++
@@ -1,7 +0,0 @@
-const getYtThumbnail = (url, size) => {
-  const matchRegex = url.match('[\\?&]v=([^&#]*)')
-  const videoCode = matchRegex === null ? url : matchRegex[1]
-  return 'http://img.youtube.com/vi/' + videoCode + '/' + size + '.jpg'
-}
-
-export default getYtThumbnail
