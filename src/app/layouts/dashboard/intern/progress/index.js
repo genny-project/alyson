4b3175fe1d22a76dfc5de6374453ac7df896f46b
slevens <sjlevens@gmail.com>
2021-05-18 10:58:00 +1000

    intern dashboard

--- src/app/layouts/dashboard/intern/progress/index.js
+++ src/app/layouts/dashboard/intern/progress/index.js
@@ -1,13 +1,62 @@
+import { HStack, Text, VStack } from '@chakra-ui/layout'
+import Attribute from 'app/BE/attribute'
+import Button from 'app/layouts/components/button'
+import Card from 'app/layouts/components/card'
+import Process from 'app/layouts/process'
+import { find, head, includes, replace } from 'ramda'
 import { useSelector } from 'react-redux'
-// import { selectDashboard } from 'redux/app/selectors'
-import { selectCode } from 'redux/db/selectors'
+import { selectCode, selectKeys } from 'redux/db/selectors'
+import { onSendMessage } from 'vertx'
 
-const Progress = ({ userCode }) => {
-  const progress = useSelector(selectCode(userCode, 'PRI_PROGRESS'))
+const Progress = () => {
+  const keys = useSelector(selectKeys)
+  const progressSbe = replace(
+    '@rows',
+    '',
+    find(includes('SBE_INPROGRESS_APPLICATIONS_'), keys) || '',
+  )
+  const appBe = head(useSelector(selectCode(progressSbe, 'rows')) || [])
 
-  // const dashboardSbes = useSelector(selectDashboard)
+  if (!appBe) return <Process dashboard />
 
-  if (!progress) return null
+  const onLogbook = () =>
+    onSendMessage({
+      parentCode: progressSbe,
+      targetCode: appBe,
+      code: 'ACT_PRI_EVENT_JOURNAL_VIEW',
+    })
+  return (
+    <Card>
+      <VStack>
+        <Text textStyle="head.3">In Progress Internship</Text>
+        <HStack>
+          <Text textStyle="body.1">
+            <Attribute code={appBe} attribute="PRI_TITLE" />
+          </Text>
+          <Text>at</Text>
+          <Text textStyle="body.1">
+            <Attribute code={appBe} attribute="PRI_ASSOC_HC" />
+          </Text>
+        </HStack>
+
+        <HStack spacing="1">
+          <Text textStyle="body.3">Until</Text>
+          <Text>
+            <Attribute config={{ textStyle: 'body.3' }} code={appBe} attribute="PRI_END_DATE" />
+          </Text>
+        </HStack>
+
+        <Card variant="card1">
+          <VStack>
+            <Button size="lg" variant="special" onClick={onLogbook}>
+              Go to Your Logbook
+            </Button>
+            <Attribute code={appBe} attribute="PRI_PROGRESS" />
+          </VStack>
+        </Card>
+      </VStack>
+    </Card>
+  )
 }
 
 export default Progress
--- src/redux/db/utils/update-keys.ts
+++ src/redux/db/utils/update-keys.ts
@@ -1,5 +1,7 @@
-export const removeKey: (items: Array<string>, code: string) => Array<string> = (items, code) =>
-  items.filter((item: string) => item !== code)
+export const removeKey: (items: Array<string>, code: string) => Array<string> = (
+  items = [],
+  code,
+) => items.filter((item: string) => item !== code)
 export const addKey: (items: Array<string>, code: string) => Array<string> = (items = [], code) => [
   code,
   ...items,
