402f0d053f85dc62350cdd1083b568e3ebc77767
c7yrus <c7yrus@gmail.com>
2021-05-26 15:09:10 +1000

    mentee dashboard done

--- src/app/layouts/dashboard/index.js
+++ src/app/layouts/dashboard/index.js
@@ -1,14 +1,13 @@
 import { useSelector } from 'react-redux'
+import { Center, HStack, Stack } from '@chakra-ui/react'
+
 import { selectDashboard, selectDashboardCounts } from 'redux/app/selectors'
 import DisplaySbe from 'app/SBE'
-import { Flex, HStack, Stack } from '@chakra-ui/react'
 import Process from '../process'
 import { selectCode } from 'redux/db/selectors'
 import getUserType from 'utils/helpers/get-user-type'
 import MenteeDashboard from './mentee'
 import AgentDashboard from './agent'
-import Timeline from './timeline/timeline'
-import Meetings from './timeline/meetings'
 
 const Dashboard = () => {
   const dashboardSbes = useSelector(selectDashboard)
@@ -16,16 +15,30 @@ const Dashboard = () => {
   const userCode = useSelector(selectCode('USER'))
   const userType = getUserType(useSelector(selectCode(userCode)))
 
-  // if (userType === 'MENTEE') return <MenteeDashboard />
+  if (userType === 'MENTEE') return <MenteeDashboard />
   if (userType === 'AGENT' || userType === 'ADMIN') return <AgentDashboard />
 
-  // if (!dashboardSbes) return <div />
+  if (!dashboardSbes) return <div />
 
   return (
-    <Flex paddingX="10">
-      <Timeline />
-      <Meetings />
-    </Flex>
+    <Center>
+      <Stack direction="column" spacing="10" h="84vh">
+        <Center>
+          <HStack spacing={4}>
+            {dashboardCounts &&
+              dashboardCounts.map(sbeCode => <DisplaySbe key={sbeCode} sbeCode={sbeCode} />)}
+          </HStack>
+        </Center>
+        <Center>
+          <HStack spacing={4}>
+            {dashboardSbes.map(sbeCode => (
+              <DisplaySbe key={sbeCode} sbeCode={sbeCode} />
+            ))}
+          </HStack>
+        </Center>
+        <Process dashboard />
+      </Stack>
+    </Center>
   )
 }
 
