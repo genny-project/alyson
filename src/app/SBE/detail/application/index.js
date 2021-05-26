33c2932f4ee2b938ee0149bfdeadad1d2591b114
slevens <sjlevens@gmail.com>
2021-05-26 10:02:23 +1000

    .

--- src/app/SBE/detail/application/index.js
+++ src/app/SBE/detail/application/index.js
@@ -1,129 +1,70 @@
-import { useSelector } from 'react-redux'
-import { selectCode, selectRows } from 'redux/db/selectors'
-import { Box, Divider, HStack, Link, Text, VStack } from '@chakra-ui/react'
-import useApi from 'api'
+import DetailLayout from '../layout'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBriefcase, faInfo, faPaperclip, faUserCheck } from '@fortawesome/free-solid-svg-icons'
 
-import getActions from 'app/SBE/utils/get-actions'
-import Attribute from 'app/BE/attribute'
-import Software from 'app/layouts/components/software'
-import { topHeight } from 'app/SBE/detail/helpers/set-top-height'
-import DetailHeader from 'app/layouts/components/header'
-import ProfilePicture from 'app/layouts/components/profile_picture'
-import Actions from 'app/layouts/components/actions'
+const appDetails = {
+  icon: <FontAwesomeIcon size="lg" icon={faPaperclip} />,
+  header: 'Application Details',
+  attributes: [
+    {
+      attr: 'PRI_INTERN_NAME',
+      label: 'Intern Applying',
+    },
+    { attr: 'PRI_ASSOC_HC', label: 'Host Company' },
+  ],
+}
+
+const supervisor = {
+  icon: <FontAwesomeIcon size="lg" icon={faUserCheck} />,
+  header: 'Supervisor',
+  attributes: [
+    {
+      attr: 'PRI_SUPER_NAME',
+      label: 'Supervisor',
+    },
+    { attr: 'PRI_SUPER_JOB_TITLE', label: 'Role' },
+    { attr: 'PRI_SUPER_JOB_EMAIL', label: 'Email' },
+    { attr: 'PRI_SUPER_JOB_MOBILE', label: 'Mobile' },
+  ],
+}
+
+const internshipDetail = {
+  icon: <FontAwesomeIcon size="lg" icon={faBriefcase} />,
+  header: 'Internship Details',
+  attributes: [
+    { attr: 'PRI_STATUS' },
+    { attr: 'PRI_ASSOC_INDUSTRY', label: 'Industry' },
+    { attr: 'PRI_WORKSITE', label: 'Onsite/Remote' },
+    { attr: 'PRI_INTERNSHIP_START_DATE', label: 'Start Date' },
+    { attr: 'PRI_WHICH_DAYS_STRIPPED', label: 'Days of Week' },
+    { attr: 'PRI_DRESS_CODE', label: 'Dress Code' },
+    { attr: 'PRI_ASSOC_NUM_INTERNS', label: 'Available Spots' },
+  ],
+}
+
+const resp = {
+  icon: <FontAwesomeIcon size="lg" icon={faInfo} />,
+  header: 'Roles and Responsibilities',
+  attributes: [{ attr: 'PRI_ROLES_AND_RESPONSIBILITIES' }],
+}
+const baseOutcomes = {
+  icon: <FontAwesomeIcon size="lg" icon={faInfo} />,
+  header: 'Learning Outcomes',
+  attributes: [{ attr: 'PRI_BASE_LEARNING_OUTCOMES' }],
+}
+const specOutcomes = {
+  icon: <FontAwesomeIcon size="lg" icon={faInfo} />,
+  header: 'Specific Learning Outcomes',
+  attributes: [{ attr: 'PRI_SPECIFIC_LEARNING_OUTCOMES' }],
+}
+
+const details = [
+  [appDetails, internshipDetail, resp],
+  [supervisor, baseOutcomes, specOutcomes],
+]
 
 const Application = ({ sbeCode, targetCode }) => {
-  const sbe = useSelector(selectCode(sbeCode))
-  const rows = useSelector(selectRows(sbeCode))
-
-  const beCode = targetCode ? targetCode : rows?.length ? rows[0] : null
-
-  const addressData = useSelector(selectCode(beCode, 'PRI_ADDRESS_FULL'))
-  const address = addressData?.value
-  const image = useSelector(selectCode(beCode, 'PRI_IMAGE_URL'))
-  const { getImageSrc } = useApi()
-  const src = getImageSrc(image?.value)
-  const url = useSelector(selectCode(beCode, 'PRI_COMPANY_WEBSITE_URL'))
-  const name = useSelector(selectCode(beCode, 'PRI_NAME'))
-  const software = useSelector(selectCode(beCode, 'PRI_SOFTWARE'))
-  const internshipSupervisor = useSelector(selectCode(beCode, 'PRI_SUPER_NAME'))
-  const actions = getActions(sbe)
-
-  if (!sbe) return null
-
-  if (!beCode) return null
-
-  return (
-    <Box
-      w="70vw"
-      h="100vh"
-      style={{
-        borderTopLeftRadius: '0.5rem',
-        borderTopRightRadius: '0.5rem',
-      }}
-    >
-      <DetailHeader address={address} />
-      <ProfilePicture src={src} />
-      <VStack pt="5rem" overflow="scroll" h={`calc(100vh - ${topHeight})`}>
-        <Link href={url?.value}>
-          <Text fontSize="3xl" fontWeight="semibold" flexWrap="nowrap">
-            {name?.value}
-          </Text>
-        </Link>
-        <Attribute code={beCode} attribute={'PRI_ASSOC_INDUSTRY'} />
-        <Attribute code={beCode} attribute={'PRI_STATUS'} />
-        <Actions actions={actions} sbeCode={sbeCode} beCode={beCode} />
-
-        <HStack align="start" pt="1rem">
-          <VStack hidden={!internshipSupervisor?.value} align="start">
-            <Text textStyle="body.1">Internship Supervisor</Text>
-            <Attribute code={beCode} attribute={'PRI_SUPER_NAME'} />
-            <Attribute code={beCode} attribute={'PRI_SUPER_JOB_TITLE'} />
-            <Attribute code={beCode} attribute={'PRI_SUPER_EMAIL'} />
-            <Attribute code={beCode} attribute={'PRI_SUPER_MOBILE'} />
-          </VStack>
-          <VStack align="start">
-            <HStack>
-              <Text>Internship Hosted By</Text>
-              <Attribute
-                config={{ textStyle: 'body.1' }}
-                code={beCode}
-                attribute={'PRI_ASSOC_HC'}
-              />
-            </HStack>
-            <HStack>
-              <Text>Intern Applying</Text>
-              <Attribute
-                config={{ textStyle: 'body.1' }}
-                code={beCode}
-                attribute={'PRI_INTERN_NAME'}
-              />
-            </HStack>
-            <HStack>
-              <Text>Start Date</Text>
-              <Attribute
-                config={{ textStyle: 'body.1' }}
-                code={beCode}
-                attribute={'PRI_START_DATE'}
-              />
-            </HStack>
-            <HStack>
-              <Text>Days</Text>
-              <Attribute
-                config={{ textStyle: 'body.1' }}
-                code={beCode}
-                attribute={'PRI_WHICH_DAYS_STRIPPED'}
-              />
-            </HStack>
-            <HStack>
-              <Text>Software to be Used</Text>
-              <Software value={software?.value || ''} />
-            </HStack>
-          </VStack>
-        </HStack>
-        <Divider w="90%" pt="5" />
-        <VStack w="full" align="start" p="5" pl="10">
-          <VStack align="start">
-            <Text textStyle="body.1">Roles and Responsibilities</Text>
-            <Box p="5">
-              <Attribute code={beCode} attribute={'PRI_ROLES_AND_RESPONSIBILITIES'} />
-            </Box>
-          </VStack>
-          <VStack align="start">
-            <Text textStyle="body.1">Base Learning Outcomes</Text>
-            <Box p="5">
-              <Attribute code={beCode} attribute={'PRI_BASE_LEARNING_OUTCOMES'} />
-            </Box>
-          </VStack>
-          <VStack align="start">
-            <Text textStyle="body.1">Specific Learning Outcomes</Text>
-            <Box p="5">
-              <Attribute code={beCode} attribute={'PRI_SPECIFIC_LEARNING_OUTCOMES'} />
-            </Box>
-          </VStack>
-        </VStack>
-      </VStack>
-    </Box>
-  )
+  return <DetailLayout sbeCode={sbeCode} targetCode={targetCode} details={details} />
 }
 
 export default Application
--- src/app/SBE/detail/internship/index.js
+++ src/app/SBE/detail/internship/index.js
@@ -1,117 +1,51 @@
-import { useSelector } from 'react-redux'
-import { selectCode, selectRows } from 'redux/db/selectors'
-import { Box, HStack, VStack } from '@chakra-ui/react'
-import useApi from 'api'
-import { replace } from 'ramda'
-import { faBriefcase } from '@fortawesome/free-solid-svg-icons'
+import { faBriefcase, faCompactDisc, faInfo } from '@fortawesome/free-solid-svg-icons'
 
-import getActions from 'app/SBE/utils/get-actions'
-import { topHeight } from 'app/SBE/detail/helpers/set-top-height'
-import DetailHeader from 'app/layouts/components/header'
-import ProfilePicture from 'app/layouts/components/profile_picture'
-import DetailSubHeader from 'app/layouts/components/subheader'
-import RightHandDetails from './templates/RightHandDetails'
-import LeftHandDetails from './templates/LeftHandDetails'
-import { useIsMobile } from 'utils/hooks'
-import InternshipMobileView from './mobile-view'
+import DetailLayout from '../layout'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const internshipDetail = {
-  sectionIcon: faBriefcase,
-  title: 'Internship Details',
+  icon: <FontAwesomeIcon size="lg" icon={faBriefcase} />,
+  header: 'Internship Details',
   attributes: [
-    'PRI_WORKSITE',
-    'PRI_INTERNSHIP_START_DATE',
-    'PRI_WHICH_DAYS_STRIPPED',
-    'PRI_DRESS_CODE',
-    'PRI_ASSOC_NUM_INTERNS',
+    { attr: 'PRI_STATUS' },
+    { attr: 'PRI_ASSOC_INDUSTRY', label: 'Industry' },
+    { attr: 'PRI_WORKSITE', label: 'Onsite/Remote' },
+    { attr: 'PRI_INTERNSHIP_START_DATE', label: 'Start Date' },
+    { attr: 'PRI_WHICH_DAYS_STRIPPED', label: 'Days of Week' },
+    { attr: 'PRI_DRESS_CODE', label: 'Dress Code' },
+    { attr: 'PRI_ASSOC_NUM_INTERNS', label: 'Available Spots' },
   ],
 }
 
-const responsibilitiesAndOutcomes = [
-  'PRI_ROLES_AND_RESPONSIBILITIES',
-  'PRI_BASE_LEARNING_OUTCOMES',
-  'PRI_SPECIFIC_LEARNING_OUTCOMES',
+const resp = {
+  icon: <FontAwesomeIcon size="lg" icon={faInfo} />,
+  header: 'Roles and Responsibilities',
+  attributes: [{ attr: 'PRI_ROLES_AND_RESPONSIBILITIES' }],
+}
+const baseOutcomes = {
+  icon: <FontAwesomeIcon size="lg" icon={faInfo} />,
+  header: 'Learning Outcomes',
+  attributes: [{ attr: 'PRI_BASE_LEARNING_OUTCOMES' }],
+}
+const specOutcomes = {
+  icon: <FontAwesomeIcon size="lg" icon={faInfo} />,
+  header: 'Specific Learning Outcomes',
+  attributes: [{ attr: 'PRI_SPECIFIC_LEARNING_OUTCOMES' }],
+}
+
+const software = {
+  icon: <FontAwesomeIcon size="lg" icon={faCompactDisc} />,
+  header: 'Software',
+  attributes: [{ attr: 'PRI_SOFTWARE' }],
+}
+
+const details = [
+  [internshipDetail, resp],
+  [software, baseOutcomes, specOutcomes],
 ]
 
-const subHeaderAttributes = ['PRI_ASSOC_INDUSTRY', 'PRI_STATUS']
-
 const Internship = ({ sbeCode, targetCode }) => {
-  const sbe = useSelector(selectCode(sbeCode))
-  const rows = useSelector(selectRows(sbeCode))
-
-  const beCode = targetCode ? targetCode : rows?.length ? rows[0] : null
-
-  const addressData = useSelector(selectCode(beCode, 'PRI_ADDRESS_FULL'))
-  const address = addressData?.value
-  const image = useSelector(selectCode(beCode, 'PRI_IMAGE_URL'))
-  const { getImageSrc } = useApi()
-  const src = getImageSrc(image?.value)
-  const url = useSelector(selectCode(beCode, 'PRI_COMPANY_WEBSITE_URL'))
-  const name = useSelector(selectCode(beCode, 'PRI_NAME'))
-  const software = useSelector(selectCode(beCode, 'PRI_SOFTWARE'))
-  const actions = getActions(sbe)
-  const videoData = useSelector(selectCode('PRI_VIDEO_URL'))
-
-  const linkedSupervisor = replace('SBE_INTERNSHIP_', 'SBE_LINKED_INTERN_SUPERVISOR_', sbeCode)
-  const linkedHostCpy = replace('SBE_INTERNSHIP_', 'SBE_LINKED_HOST_CPY_', sbeCode)
-
-  const isMobile = useIsMobile()
-
-  if (!sbe) return null
-
-  if (!beCode) return null
-
-  return isMobile ? (
-    <InternshipMobileView
-      beCode={beCode}
-      actions={actions}
-      src={src}
-      sbeCode={sbeCode}
-      url={url}
-      name={name}
-      subHeaderAttributes={subHeaderAttributes}
-      otherAttributes={responsibilitiesAndOutcomes}
-      linkedHostCpy={linkedHostCpy}
-      linkedSupervisor={linkedSupervisor}
-      software={software}
-    />
-  ) : (
-    <Box
-      w="70vw"
-      h="100vh"
-      style={{
-        borderTopLeftRadius: '0.5rem',
-        borderTopRightRadius: '0.5rem',
-      }}
-    >
-      <DetailHeader address={address} beCode={beCode} videoData={videoData} />
-      <ProfilePicture src={src} />
-
-      <VStack pt="5rem" overflow="scroll" h={`calc(100vh - ${topHeight})`}>
-        <DetailSubHeader
-          url={url}
-          name={name}
-          beCode={beCode}
-          sbeCode={sbeCode}
-          actions={actions}
-          subHeaderAttributes={subHeaderAttributes}
-        />
-        <HStack w="65vw" align="start" pt="5" spacing="5">
-          <LeftHandDetails
-            beCode={beCode}
-            internshipDetail={internshipDetail}
-            linkedSupervisor={linkedSupervisor}
-            software={software}
-          />
-          <RightHandDetails
-            code={beCode}
-            sbeCode={linkedHostCpy}
-            attributes={responsibilitiesAndOutcomes}
-          />
-        </HStack>
-      </VStack>
-    </Box>
-  )
+  return <DetailLayout sbeCode={sbeCode} targetCode={targetCode} details={details} />
 }
 
 export default Internship
