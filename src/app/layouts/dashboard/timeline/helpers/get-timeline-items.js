6bfbe12d70c0ac0af814ae67ee51739006ac349f
c7yrus <c7yrus@gmail.com>
2021-05-27 14:45:17 +1000

    found a bug so fixed it

--- src/app/layouts/dashboard/timeline/helpers/get-timeline-items.js
+++ src/app/layouts/dashboard/timeline/helpers/get-timeline-items.js
@@ -9,8 +9,8 @@ const useGetMenteeTimelineItems = () => {
   } = useGetMenteeInformation()
   const description =
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse venenatis placerat arcu, tempor rutrum tortor porta quis. Donec aliquam urna ac varius ultrices. Morbi vel dapibus nunc, dictum pretium justo. Nulla non blandit leo. Proin non imperdiet ex. Etiam cursus dignissim sem, nec interdum massa pellentesque eu. Proin condimentum mauris at diam porttitor, a rhoncus nisi semper. Sed sed tincidunt felis, at bibendum sapien. Etiam odio libero, pretium ac condimentum ac, congue ac mi. Aenean efficitur malesuada arcu mattis tempus.'
-  const isTrainingCompleted = trainingStatus === 'COMPLETED'
   const isProfileCompleted = profileStatus === 'COMPLETED'
+  const isTrainingCompleted = trainingStatus === 'COMPLETED'
   const isMentorSelected = selectMentorStatus === 'COMPLETED'
   const isFirstMeetingCompleted = firstMeetingStatus === 'COMPLETED'
   const items = [
@@ -27,7 +27,7 @@ const useGetMenteeTimelineItems = () => {
       description: 'Access different training modules under this section.',
       buttonText: 'Go to Training',
       completed: trainingStatus,
-      isDisabled: !isTrainingCompleted,
+      isDisabled: !isProfileCompleted,
       code: 'ACT_PRI_EVENT_START_MENTEE_TRAINING',
     },
     {
@@ -35,7 +35,7 @@ const useGetMenteeTimelineItems = () => {
       description: 'Choose the Mentor that suits you the most!',
       buttonText: 'Go to Mentor Selection',
       completed: 'NOT COMPLETED',
-      isDisabled: !isProfileCompleted,
+      isDisabled: !isTrainingCompleted,
       code: 'ACT_PRI_EVENT_SELECT_MENTOR',
     },
     {
