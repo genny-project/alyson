8168a17d2ea85c33e3eb4f971cafba91e4f42da6
slevens <sjlevens@gmail.com>
2021-05-28 12:57:17 +1000

    bring back service worker

--- public/worker.js
+++ public/worker.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-restricted-globals */
 let CACHE_NAME = 'internmatch'
 let urlsToCache = ['/']
@@ -5,41 +6,41 @@ let urlsToCache = ['/']
 // Install a service worker
 self.addEventListener('install', event => {
   // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('Opened cache')
-      return cache.addAll(urlsToCache)
-    }),
-  )
+  // event.waitUntil(
+  //   caches.open(CACHE_NAME).then(function (cache) {
+  //     console.log('Opened cache')
+  //     return cache.addAll(urlsToCache)
+  //   }),
+  // )
 })
 
 // Cache and return requests
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response
-      }
-      return fetch(event.request)
-    }),
-  )
+  // event.respondWith(
+  //   caches.match(event.request).then(function (response) {
+  //     // Cache hit - return response
+  //     if (response) {
+  //       return response
+  //     }
+  //     return fetch(event.request)
+  //   }),
+  // )
 })
 
 // Update a service worker
 self.addEventListener('activate', event => {
   let cacheWhitelist = ['internmatch']
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName)
-          }
-        }),
-      )
-    }),
-  )
+  // event.waitUntil(
+  //   caches.keys().then(cacheNames => {
+  //     return Promise.all(
+  //       cacheNames.map(cacheName => {
+  //         if (cacheWhitelist.indexOf(cacheName) === -1) {
+  //           return caches.delete(cacheName)
+  //         }
+  //       }),
+  //     )
+  //   }),
+  // )
 })
 
 self.addEventListener('error', e => {
