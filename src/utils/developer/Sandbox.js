cf7fe673d7f9b2f3c497aeee271399b13637d1cf
🤖 <baradghimire@gmail.com>
2021-06-02 15:14:47 +1000

    test darkmode

--- src/utils/developer/Sandbox.js
+++ src/utils/developer/Sandbox.js
@@ -1,9 +1,11 @@
 import { Button, IconButton, VStack } from '@chakra-ui/react'
 import { HostCpyRepIcon } from './HostCpyRepIcon'
+import ThemeToggler from '../../app/layouts/navigation/ColorToggler'
 
 const Sandbox = () => {
   return (
     <VStack>
+      <ThemeToggler />
       <Button leftIcon={<HostCpyRepIcon />} colorScheme="teal" variant="solid">
         HostCpyRepIcon
       </Button>
