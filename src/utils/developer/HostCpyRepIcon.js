d23b11a074d6db38422ab4927293ad3eb98736dd
🤖 <baradghimire@gmail.com>
2021-06-02 14:58:19 +1000

    Add test icon

---
+++ src/utils/developer/HostCpyRepIcon.js
@@ -0,0 +1,9 @@
+import { createIcon } from '@chakra-ui/react'
+
+export const HostCpyRepIcon = createIcon({
+  displayName: 'HostCpyRepIcon',
+  viewBox: '0 0 200 200',
+  path: (
+    <path fill="currentColor" d="M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0" />
+  ),
+})
--- src/utils/developer/Sandbox.js
+++ src/utils/developer/Sandbox.js
@@ -1,11 +1,13 @@
 import { VStack } from '@chakra-ui/react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCamera } from '@fortawesome/free-solid-svg-icons'
+import { HostCpyRepIcon } from './HostCpyRepIcon'
 
 const Sandbox = () => {
   return (
     <VStack>
       <FontAwesomeIcon icon={faCamera} />
+      <HostCpyRepIcon />
     </VStack>
   )
 }
