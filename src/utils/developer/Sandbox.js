5c84dc05d4deb4639e117c60ecc2ed04dc88e4d2
🤖 <baradghimire@gmail.com>
2021-06-02 14:34:48 +1000

    Initialise sandbox

--- src/utils/developer/Sandbox.js
+++ src/utils/developer/Sandbox.js
@@ -1,14 +1,11 @@
-import { VStack, Image } from '@chakra-ui/react'
-import getYtThumbnail from 'utils/helpers/get-yt-thumbnail'
+import { VStack } from '@chakra-ui/react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCamera } from '@fortawesome/free-solid-svg-icons'
 
 const Sandbox = () => {
   return (
     <VStack>
-      <Image src={getYtThumbnail('https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'default')} />
-      <Image src={getYtThumbnail('https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'mqdefault')} />
-      <Image src={getYtThumbnail('https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'hqdefault')} />
-      <Image src={getYtThumbnail('https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'sddefault')} />
-      <Image src={getYtThumbnail('https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'maxresdefault')} />
+      <FontAwesomeIcon icon={faCamera} />
     </VStack>
   )
 }
