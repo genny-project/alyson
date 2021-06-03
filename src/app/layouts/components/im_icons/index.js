de86d3d98fc3bf6afc86a0cb76e9cbfccca466ef
slevens <sjlevens@gmail.com>
2021-06-03 13:38:09 +1000

    .

--- src/app/layouts/components/im_icons/index.js
+++
@@ -1,5 +0,0 @@
-import IconEduProRep from './IconEduProRep'
-import IconHostCpyRep from './IconHostCpyRep'
-import IconIntern from './IconIntern'
-
-export { IconEduProRep, IconHostCpyRep, IconIntern }
