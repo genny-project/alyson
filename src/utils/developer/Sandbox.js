2cda202ae99e3487fe642b0e62bb90b0e04fd1ba
slevens <sjlevens@gmail.com>
2021-06-03 13:53:57 +1000

    Update Sandbox.js

--- src/utils/developer/Sandbox.js
+++ src/utils/developer/Sandbox.js
@@ -1,16 +1,5 @@
-import { IconButton, VStack } from '@chakra-ui/react'
-import { IconHostCpyRep, IconEduProRep, IconIntern } from 'app/layouts/components/im_icons'
-import ThemeToggler from 'app/layouts/navigation/ColorToggler'
-
 const Sandbox = () => {
-  return (
-    <VStack>
-      <ThemeToggler />
-      <IconButton size="lg" icon={<IconHostCpyRep />} />
-      <IconButton size="lg" icon={<IconEduProRep />} />
-      <IconButton size="lg" icon={<IconIntern />} />
-    </VStack>
-  )
+  return null
 }
 
 export default Sandbox
