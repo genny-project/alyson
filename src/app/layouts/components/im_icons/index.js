2af5141f812da68364c369f4571ff60f03917ceb
🤖 <baradghimire@gmail.com>
2021-06-04 13:56:13 +1000

    Add Dashboard and ProcessView icons

--- src/app/layouts/components/im_icons/index.js
+++ src/app/layouts/components/im_icons/index.js
@@ -1,7 +1,16 @@
 import IconAgent from './IconAgent'
+import IconDashboard from './IconDashboard'
 import IconEduProRep from './IconEduProRep'
 import IconHostCpyRep from './IconHostCpyRep'
 import IconIntern from './IconIntern'
+import IconProcessView from './IconProcessView'
 
-const icons = { IconAgent, IconEduProRep, IconHostCpyRep, IconIntern }
+const icons = {
+  IconAgent,
+  IconDashboard,
+  IconEduProRep,
+  IconHostCpyRep,
+  IconIntern,
+  IconProcessView,
+}
 export default icons
