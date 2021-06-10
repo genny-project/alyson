b893259f894522ae890a961c242c39ca28cc52bc
slevens <sjlevens@gmail.com>
2021-06-10 11:12:32 +1000

    Update index.js

--- src/app/layouts/table/index.js
+++ src/app/layouts/table/index.js
@@ -1,13 +1,12 @@
 import { useSelector } from 'react-redux'
 import { selectTable } from 'redux/app/selectors'
 import Table from 'app/SBE/table/Table'
-import MapSearch from 'app/SBE/display_modes/map_search'
 
-const TableWrapper = ({ mapSearch }) => {
+const TableWrapper = () => {
   const table = useSelector(selectTable)
 
   if (!table) return null
-  if (mapSearch) return <MapSearch parentCode={table} />
+
   return <Table parentCode={table} />
 }
 
