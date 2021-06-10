982e79f0393a16c31d86b79aa60863ebf9a1c83e
slevens <sjlevens@gmail.com>
2021-06-10 11:12:51 +1000

    Update index.js

--- src/app/layouts/table/index.js
+++ src/app/layouts/table/index.js
@@ -1,15 +1,11 @@
 import { useSelector } from 'react-redux'
 import { selectTable } from 'redux/app/selectors'
 import Table from 'app/SBE/table/Table'
-import { includes } from 'ramda'
-import Journals from './journals'
 
-const TableWrapper = ({ mapSearch }) => {
+const TableWrapper = () => {
   const table = useSelector(selectTable)
 
   if (!table) return null
-
-  if (includes('_JNLS_', table) || includes('_LOGBOOK_', table)) return <Journals sbeCode={table} />
 
   return <Table parentCode={table} />
 }
