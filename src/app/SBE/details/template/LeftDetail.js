18806a30977bf549ef464415ad6e3bb4db085661
c7yrus <42728641+c7yrus@users.noreply.github.com>
2021-07-15 01:48:08 +1000

    styling

--- src/app/SBE/details/template/LeftDetail.js
+++ src/app/SBE/details/template/LeftDetail.js
@@ -1,4 +1,4 @@
-import { VStack, HStack, Box, Flex } from '@chakra-ui/layout'
+import { VStack, HStack, Box } from '@chakra-ui/layout'
 import { Avatar } from '@chakra-ui/avatar'
 import DetailActions from 'app/SBE/details/template/layouts/Actions.js'
 import 'app/layouts/components/css/hide-scroll.css'
@@ -14,23 +14,23 @@ const LeftDetail = ({ beCode, sbeCode }) => {
         <Box w="full">
           <Attribute code={beCode} config={{ h: '15rem', w: '15rem' }} attribute="PRI_IMAGE_URL" />
           <Avatar
-            bg="white"
-            mt="10.8rem"
+            bg="#F7FAFC"
+            mt="10.5rem"
             ml="-8rem"
-            p="2px"
+            p="4px"
             src={`https://cdn.pixabay.com/photo/2017/11/09/21/41/cat-2934720_1280.jpg`}
-            w="4rem"
-            h="4rem"
+            w="4.5rem"
+            h="4.5rem"
             zIndex="modal"
           />
           <Avatar
-            bg="white"
-            mt="10.8rem"
-            ml="-0.5rem"
-            p="2px"
+            bg="#F7FAFC"
+            mt="10.5rem"
+            ml="-1rem"
+            p="4px"
             src={`https://cdn.pixabay.com/photo/2014/05/07/06/44/cat-339400_1280.jpg`}
-            w="4rem"
-            h="4rem"
+            w="4.5rem"
+            h="4.5rem"
             zIndex="modal"
           />
         </Box>
