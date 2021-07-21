57d4200a1266ca19817a6f40cf3ba22e6bb30d44
c7yrus <42728641+c7yrus@users.noreply.github.com>
2021-07-22 00:24:14 +1000

    mobile-view-initial

--- src/app/SBE/detail-profile/detail-layout/application/detail-sections/mobile/template/Header.js
+++ src/app/SBE/detail-profile/detail-layout/application/detail-sections/mobile/template/Header.js
@@ -6,10 +6,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import { IconButton } from '@chakra-ui/react'
 
-import DetailActions from 'app/SBE/detail-profile/detail-layout/intern/templates/Actions.js'
+import DetailActions from 'app/SBE/detail-profile/detail-layout/application/templates/Actions.js'
 import 'app/layouts/components/css/hide-scroll.css'
 import Attribute from 'app/BE/attribute'
-import { LeftDetailAttributesIntern } from 'app/SBE/detail-profile/detail-layout/intern/templates/AttributesList.js'
+import { LeftDetailAttributesApplication } from 'app/SBE/detail-profile/detail-layout/application/templates/AttributesList.js'
 import { closeDrawer } from 'redux/app'
 import 'app/layouts/components/css/hide-scroll.css'
 import Card from 'app/layouts/components/card'
@@ -19,7 +19,7 @@ const Header = ({ beCode, sbeCode }) => {
   const cardBg = useColorModeValue('#ffffff', 'gray.600')
   const dispatch = useDispatch()
   const onClose = () => dispatch(closeDrawer())
-  const videoSrc = useSelector(selectCode(beCode, 'PRI_VIDEO_URL'))?.value
+  const videoSrc = useSelector(selectCode(beCode, '_PRI_INTERN_CODE__PRI_VIDEO_URL'))?.value
   const hasVideo = !isEmpty(videoSrc)
 
   return (
@@ -34,13 +34,15 @@ const Header = ({ beCode, sbeCode }) => {
             icon={<FontAwesomeIcon opacity={50} icon={faTimes} />}
           />
         </Box>
-        <Attribute code={beCode} config={{ h: '5rem', w: '5rem' }} attribute="PRI_IMAGE_URL" />
+        <Attribute
+          code={beCode}
+          config={{ h: '5rem', w: '5rem' }}
+          attribute="_PRI_INTERN_CODE__PRI_IMAGE_URL_"
+        />
         <VStack align="start" spacing={2}>
           <HStack spacing={5}>
-            <Attribute config={{ textStyle: 'head.1' }} code={beCode} attribute="PRI_NAME" />
-            <Attribute config={{ color: '#3182CE' }} code={beCode} attribute="PRI_LINKEDIN_URL" />
+            <Attribute config={{ textStyle: 'head.1' }} code={beCode} attribute="PRI_INTERN_NAME" />
           </HStack>
-          <Attribute code={beCode} attribute="PRI_STAR_RATING" />
           <DetailActions beCode={beCode} sbeCode={sbeCode} />
           <VStack align="start" spacing={4} py={4}>
             {map(({ icon, attr, config }) => {
@@ -50,19 +52,23 @@ const Header = ({ beCode, sbeCode }) => {
                   <Attribute code={beCode} attribute={attr} config={config} />
                 </HStack>
               )
-            })(LeftDetailAttributesIntern)}
+            })(LeftDetailAttributesApplication)}
           </VStack>
         </VStack>
         <Center w="full">
           <Card p={0} w="full" bg="#1A365D" overflow="hidden" maxH="15rem">
             <Center>
               {hasVideo ? (
-                <Attribute code={beCode} attribute="PRI_VIDEO_URL" />
+                <Attribute code={beCode} attribute="_PRI_INTERN_CODE__PRI_VIDEO_URL" />
               ) : (
                 <Center minH="7rem" w="100%">
                   <VStack>
-                    <Text color="#ffffff">{`The Intern has not uploaded any video yet.`}</Text>
-                    <Text color="#ffffff">{`Once they do, it will appear here!`}</Text>
+                    <Text textStyle="head.2" color="#ffffff">
+                      {`Intertnship video has not been uploaded`}
+                    </Text>
+                    <Text textStyle="head.2" color="#ffffff">
+                      {`Once it is, it will appear here!`}
+                    </Text>
                   </VStack>
                 </Center>
               )}
