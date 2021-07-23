385bd529fe05be48343366846a1550fb0b999d0d
c7yrus <42728641+c7yrus@users.noreply.github.com>
2021-07-23 16:28:17 +1000

    bug fix

--- src/app/SBE/detail-profile/detail-layout/internship/templates/Actions.js
+++ src/app/SBE/detail-profile/detail-layout/internship/templates/Actions.js
@@ -4,18 +4,25 @@ import { HStack } from '@chakra-ui/layout'
 import { Menu, MenuButton, MenuList } from '@chakra-ui/menu'
 import { faEllipsisH } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPencilAlt } from '@fortawesome/free-solid-svg-icons'
-import { map, tail } from 'ramda'
+import { faPencilAlt, faPlus } from '@fortawesome/free-solid-svg-icons'
+import { map, tail, equals } from 'ramda'
 
 import Action from 'app/BE/context/Action'
 import getActions from 'app/SBE/utils/get-actions'
 import { selectCode } from 'redux/db/selectors'
+import getUserType from 'utils/helpers/get-user-type'
+import sameLength from 'redux/utils/same-length'
 
 const DetailActions = ({ sbeCode, beCode }) => {
   const sbe = useSelector(selectCode(sbeCode))
   const actions = getActions(sbe) || []
 
+  const userCode = useSelector(selectCode('USER'), equals)
+  const userType = getUserType(useSelector(selectCode(userCode), sameLength))
+
   if (!actions.length) return null
+
+  console.warn('actions', actions)
 
   return (
     <HStack spacing={6}>
@@ -24,8 +31,8 @@ const DetailActions = ({ sbeCode, beCode }) => {
         code={actions[0]}
         targetCode={beCode}
         noMenu
-        icon={faPencilAlt}
-        name={`Edit`}
+        icon={userType === 'INTERN' ? faPlus : faPencilAlt}
+        name={userType === 'INTERN' ? `Apply` : `Edit`}
         customAction
       />
       {actions.length > 1 && (
