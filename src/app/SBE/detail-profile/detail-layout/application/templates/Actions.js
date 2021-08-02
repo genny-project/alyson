4f4e77283ea59a21292cfef439d4674403b7c8a6
c7yrus <42728641+c7yrus@users.noreply.github.com>
2021-08-02 12:41:01 +1000

    action

--- src/app/SBE/detail-profile/detail-layout/application/templates/Actions.js
+++ src/app/SBE/detail-profile/detail-layout/application/templates/Actions.js
@@ -1,9 +1,5 @@
 import { useSelector } from 'react-redux'
-import { IconButton } from '@chakra-ui/button'
 import { HStack } from '@chakra-ui/layout'
-import { Menu, MenuButton, MenuList } from '@chakra-ui/menu'
-import { faEllipsisH } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { map } from 'ramda'
 
 import Action from 'app/BE/context/Action'
@@ -17,26 +13,10 @@ const DetailActions = ({ sbeCode, beCode }) => {
   if (!actions.length) return null
 
   return (
-    <HStack spacing={6}>
-      {actions.length > 0 && (
-        <Menu>
-          <MenuButton>
-            <IconButton
-              test-id={'detail-view-actions'}
-              colorScheme="primary"
-              variant="outline"
-              icon={<FontAwesomeIcon icon={faEllipsisH} />}
-              borderRadius="2rem"
-              p="1rem"
-            />
-          </MenuButton>
-          <MenuList>
-            {map(action => (
-              <Action key={action} parentCode={sbeCode} code={action} targetCode={beCode} />
-            ))(actions)}
-          </MenuList>
-        </Menu>
-      )}
+    <HStack>
+      {map(action => (
+        <Action key={action} parentCode={sbeCode} code={action} targetCode={beCode} noMenu />
+      ))(actions)}
     </HStack>
   )
 }
