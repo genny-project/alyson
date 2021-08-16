fd25529e3f0b57a2569ad7b78f8887e5445f52e2
slevens <sjlevens@gmail.com>
2021-08-16 14:00:16 +1000

    add z on to the end of the backend time string to infer 0

--- src/utils/helpers/timezone_magic/get-date.ts
+++ src/utils/helpers/timezone_magic/get-date.ts
@@ -1,8 +1,11 @@
 import { parseISO } from 'date-fns'
+import { includes } from 'ramda'
 
 const getDate = (valueFromBackend: string | undefined) => {
   if (valueFromBackend) {
-    return parseISO(valueFromBackend)
+    return parseISO(
+      includes('Z', valueFromBackend || '') ? valueFromBackend : valueFromBackend + 'Z',
+    )
   }
 
   return null
