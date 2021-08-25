33a1a8ab1e111c96e734e795480de534c55a5963
slevens <sjlevens@gmail.com>
2021-08-25 14:49:18 +1000

    Update index.js

--- src/app/DTT/radio/index.js
+++ src/app/DTT/radio/index.js
@@ -2,14 +2,14 @@ import { Radio as CRadio, RadioGroup, Stack } from '@chakra-ui/react'
 import { useSelector } from 'react-redux'
 import { selectCode } from 'redux/db/selectors'
 import { Read } from 'app/DTT/text'
-import { map, compose } from 'ramda'
+import { map, compose, includes } from 'ramda'
 import safelyParseJson from 'utils/helpers/safely-parse-json'
 
 const Write = ({ questionCode, data, onSendAnswer, groupCode, parentCode }) => {
   const radioData = useSelector(selectCode(`${parentCode}-${questionCode}-options`)) || []
   const options = compose(map(({ code, name }) => ({ label: name, value: code })))(radioData)
 
-  const arrayValue = safelyParseJson(data?.value, [])
+  const arrayValue = includes('[', data?.value || '') ? safelyParseJson(data?.value, []) : []
   const value = arrayValue.length ? arrayValue[0] : data?.value || null
 
   return (
