0ac57986e3ecc2d63369f1413ae2f6bec8f02ba9
c7yrus <42728641+c7yrus@users.noreply.github.com>
2021-09-01 09:54:37 +1000

    regex for phone

--- src/app/DTT/phone/index.js
+++ src/app/DTT/phone/index.js
@@ -28,7 +28,9 @@ const Read = ({ data, config }) => {
 
 export const Write = ({ questionCode, data, onSendAnswer, regexPattern }) => {
   // eslint-disable-next-line no-useless-escape
-  const phoneRegex = RegExp(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im)
+  const phoneRegex = RegExp(
+    /^(?:\+?(61))? ?(?:\((?=.*\)))?(0?[2-57-8])\)? ?(\d\d(?:[- ](?=\d{3})|(?!\d\d[- ]?\d[- ]))\d\d[- ]?\d[- ]?\d{3})$/,
+  )
 
   const inputRef = useRef()
   const value = data?.value
