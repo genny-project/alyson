6fc9386d2852f2c6e0bed34f7eaddf6dbee7c154
c7yrus <42728641+c7yrus@users.noreply.github.com>
2021-09-14 17:29:30 +1000

    video workaround

--- src/app/DTT/video/Player.js
+++ src/app/DTT/video/Player.js
@@ -1,13 +1,23 @@
-const Player = ({ src }) => (
-  <video
-    style={{ maxHeight: '340px', minWidth: '340px' }}
-    controls
-    autoPlay
-    muted
-    poster="/video-intro.png"
-  >
-    <source src={src} />
-  </video>
-)
+import { split } from 'ramda'
+
+const Player = ({ src }) => {
+  const splittedSource = split('/video')(src)
+  const headSrc = splittedSource[0]
+  const tailSrc = splittedSource[1]
+  const finalSrc = `${headSrc}${tailSrc}`
+  console.log('finalsrc', { finalSrc })
+  return (
+    <video
+      style={{ maxHeight: '340px', minWidth: '340px' }}
+      controls
+      autoPlay
+      muted
+      poster="/video-intro.png"
+    >
+      <source src={finalSrc} />
+      {/* <source src="https://mdn.github.io/learning-area/html/multimedia-and-embedding/video-and-audio-content/rabbit320.mp4" /> */}
+    </video>
+  )
+}
 
 export default Player
