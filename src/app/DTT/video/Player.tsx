1ff7dd21a00b6817f8ab05e8ca92ec7b5f284fca
c7yrus <42728641+c7yrus@users.noreply.github.com>
2021-09-14 23:22:48 +1000

    add typescript

--- src/app/DTT/video/Player.tsx
+++ src/app/DTT/video/Player.tsx
@@ -1,7 +1,8 @@
 import { compose, split, reduce } from 'ramda'
+import { ReactElement } from 'react'
 
-const Player = ({ src }) => {
-  const getValue = reduce((acc, value) => acc + value, '')
+const Player = ({ src }: { src: string }): ReactElement => {
+  const getValue = reduce((acc: string, value: string): string => acc + value, '')
   const final = compose(getValue, split('/video'))(src)
 
   return (
