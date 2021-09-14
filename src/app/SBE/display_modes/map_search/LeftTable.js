68d4c3c722c5b772c71010e8549d0befd602b1fb
c7yrus <42728641+c7yrus@users.noreply.github.com>
2021-09-14 11:11:31 +1000

    map view initia

--- src/app/SBE/display_modes/map_search/LeftTable.js
+++ src/app/SBE/display_modes/map_search/LeftTable.js
@@ -1,12 +1,9 @@
 import { useSelector } from 'react-redux'
 import { selectRows } from 'redux/db/selectors'
-import { useColorModeValue, VStack } from '@chakra-ui/react'
+import { useColorModeValue, VStack, Text } from '@chakra-ui/react'
 import getColumns from 'app/SBE/utils/get-columns'
 import getActions from 'app/SBE/utils/get-actions'
 import { selectCode } from 'redux/db/selectors'
-import Search from 'app/SBE/search/Search'
-import Title from 'app/SBE/table/Title'
-import Filters from 'app/SBE/filters'
 import Card from './Card'
 
 const LeftTable = ({ parentCode }) => {
@@ -21,36 +18,9 @@ const LeftTable = ({ parentCode }) => {
   const actions = getActions(tableData)
 
   return (
-    <VStack
-      bg={backgroundColor}
-      position="absolute"
-      top="0"
-      pt="13rem"
-      bottom="0"
-      left="0"
-      w="30vw"
-      alignItems="left"
-      zIndex="overlay"
-      shadow="2xl"
-    >
-      <VStack
-        align="start"
-        w="30vw"
-        h="10rem"
-        position="absolute"
-        top="4rem"
-        pt="1rem"
-        left="0"
-        bg="white"
-        pl="5"
-        boxShadow="md"
-      >
-        <Title sbeCode={parentCode} />
-        <Search sbeCode={parentCode} />
-        <Filters sbeCode={parentCode} />
-      </VStack>
-
-      <VStack pt="1rem" h="80vh" overflowY="scroll">
+    <VStack bg={backgroundColor} w="30vw" alignItems="left" zIndex="overlay" shadow="2xl">
+      <VStack maxH="85vh" overflowY="scroll">
+        <Text textStyle="head.2">{`Internships`}</Text>
         {rows.map(code => (
           <Card
             key={code}
--- src/app/SBE/display_modes/map_search/index.js
+++ src/app/SBE/display_modes/map_search/index.js
@@ -1,12 +1,15 @@
 import { Box } from '@chakra-ui/react'
+
+import { useIsMobile } from 'utils/hooks'
 import Map from './Map'
 import LeftTable from './LeftTable'
 
 const MapSearch = ({ parentCode }) => {
+  const isMobile = useIsMobile()
+
   return (
     <Box>
-      <LeftTable parentCode={parentCode} />
-
+      {!isMobile && <LeftTable parentCode={parentCode} />}
       <Map parentCode={parentCode} />
     </Box>
   )
