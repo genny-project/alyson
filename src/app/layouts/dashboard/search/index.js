14f22ca74c4a6557619e3ab67333ea7207d9211c
c7yrus <42728641+c7yrus@users.noreply.github.com>
2021-09-15 10:38:39 +1000

    intial

--- src/app/layouts/dashboard/search/index.js
+++ src/app/layouts/dashboard/search/index.js
@@ -15,8 +15,9 @@ import { onSendSearch } from 'vertx'
 import { useSelector } from 'react-redux'
 import { selectCode } from 'redux/db/selectors'
 
-const ProcessSearch = ({ sbeCode, process, placeholder }) => {
+const ProcessSearch = ({ sbeCode, process, placeholder = 'Dashboard Search' }) => {
   const [searchValue, setSearchValue] = useState('')
+  const [clear, setClear] = useState(false)
   const inputRef = useRef(null)
   const clearRef = useRef(null)
 
@@ -26,11 +27,13 @@ const ProcessSearch = ({ sbeCode, process, placeholder }) => {
     e.preventDefault()
     onSendSearch({ searchValue, sbeCode, searchType: '!' })
     inputRef.current.blur()
+    setClear(true)
   }
 
   const handleClear = () => {
     onSendSearch({ searchType: '!', searchValue: '', sbeCode })
     setSearchValue('')
+    setClear(false)
   }
 
   useHotkeys('ctrl+k, cmd+k', () => {
@@ -72,15 +75,15 @@ const ProcessSearch = ({ sbeCode, process, placeholder }) => {
         colorScheme="primary"
         test-id={`process-view-search`}
       >
-        Search
+        {`Search`}
       </Button>
-      {search && (
+      {clear && (
         <Button
           onClick={handleClear}
           leftIcon={<FontAwesomeIcon icon={faTimes} />}
           colorScheme="secondary"
         >
-          Clear Search
+          {`Clear Search`}
         </Button>
       )}
     </Stack>
