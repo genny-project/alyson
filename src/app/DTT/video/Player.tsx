cfa61f12eb8fe21884ffb804b824eef48b40246e
c7yrus <42728641+c7yrus@users.noreply.github.com>
2021-09-19 14:03:32 +1000

    change the video to be autoplayed

--- src/app/DTT/video/Player.tsx
+++ src/app/DTT/video/Player.tsx
@@ -1,10 +1,6 @@
-import { compose, split, reduce } from 'ramda'
 import { ReactElement } from 'react'
 
 const Player = ({ src }: { src: string }): ReactElement => {
-  const getValue = reduce((acc: string, value: string): string => acc + value, '')
-  const final = compose(getValue, split('/video'))(src)
-
   return (
     <video
       style={{ maxHeight: '340px', minWidth: '340px' }}
@@ -13,7 +9,7 @@ const Player = ({ src }: { src: string }): ReactElement => {
       muted
       poster="/video-intro.png"
     >
-      <source src={final} />
+      <source src={src} />
     </video>
   )
 }
