9bc04c08c30ae9b569826be2c778ae3c0e0230ea
c7yrus <42728641+c7yrus@users.noreply.github.com>
2021-09-29 11:57:48 +1000

    render the video module component if it is a mentor or mentee training questions

--- src/app/ASKS/form/index.js
+++ src/app/ASKS/form/index.js
@@ -4,13 +4,21 @@ import { useSelector } from 'react-redux'
 import { useIsMobile } from 'utils/hooks'
 import FormMobileView from 'app/ASKS/form/mobile_view'
 import FormDesktopView from 'app/ASKS/form/desktop_view'
+import VideoModules from './variations/video_modules'
 
 const AsksForm = ({ questionCode, onFinish, dialog }) => {
   const childAsks = useSelector(selectCode(questionCode)) || []
   const title = useSelector(selectCode(questionCode, 'title'))
   const config = useSelector(selectCode(questionCode, 'config'))
+  const isMobile = useIsMobile()
 
-  return useIsMobile() || dialog ? (
+  if (
+    questionCode === 'QUE_MENTEE_TRAINING_MODULE_ONE_GRP' ||
+    questionCode === 'QUE_MENTOR_TRAINING_MODULE_ONE_GRP'
+  )
+    return <VideoModules questionCode={questionCode} />
+
+  return isMobile() || dialog ? (
     <FormMobileView
       title={title}
       childAsks={childAsks}
