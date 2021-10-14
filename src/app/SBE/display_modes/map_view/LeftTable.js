c3919f57541d945f8402e6f93bc17ed316b3734e
c7yrus <42728641+c7yrus@users.noreply.github.com>
2021-10-14 12:29:26 +1100

    map view fix

--- src/app/SBE/display_modes/map_view/LeftTable.js
+++ src/app/SBE/display_modes/map_view/LeftTable.js
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux'
+import { length } from 'ramda'
 import { selectRows } from 'redux/db/selectors'
-import { useColorModeValue, VStack, Text } from '@chakra-ui/react'
+import { useColorModeValue, VStack, Text, Box } from '@chakra-ui/react'
 import getColumns from 'app/SBE/utils/get-columns'
 import getActions from 'app/SBE/utils/get-actions'
 import { selectCode } from 'redux/db/selectors'
@@ -16,22 +17,29 @@ const LeftTable = ({ parentCode }) => {
   if (!tableData) return null
 
   const columns = getColumns(tableData)
+  console.log('rows----->', rows)
   const actions = getActions(tableData)
 
   return (
-    <VStack bg={backgroundColor} maxW="30vw" h="inherit" alignItems="left" shadow="2xl">
+    <VStack bg={backgroundColor} minW="20vw" maxW="30vw" h="inherit" alignItems="left" shadow="2xl">
       <Text textStyle="head.2" color="white" textAlign="center" mt="4">{`Internships`}</Text>
       <VStack overflowY="scroll" className="scrollBar">
-        {rows.map(code => (
-          <Card
-            key={code}
-            code={code}
-            parentCode={parentCode}
-            actions={actions}
-            columns={columns}
-            borderRadius="sm"
-          />
-        ))}
+        {!!length(rows) ? (
+          rows.map(code => (
+            <Card
+              key={code}
+              code={code}
+              parentCode={parentCode}
+              actions={actions}
+              columns={columns}
+              borderRadius="sm"
+            />
+          ))
+        ) : (
+          <Box>
+            <Text textStyle="body.2" color="white">{`Sorry, no results found!`}</Text>
+          </Box>
+        )}
       </VStack>
     </VStack>
   )
