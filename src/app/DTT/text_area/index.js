27d951a5cd170f74813f01e323a3bb5f8ab9f278
c7yrus <42728641+c7yrus@users.noreply.github.com>
2021-11-23 09:54:25 +1100

    setting up the initials

---
+++ src/app/DTT/text_area/index.js
@@ -0,0 +1,70 @@
+import { Text, Textarea } from '@chakra-ui/react'
+import { useEffect, useRef, useState } from 'react'
+
+import { ACTIONS } from 'utils/contexts/ErrorReducer'
+import debounce from 'lodash.debounce'
+import { getIsInvalid } from 'utils/functions'
+import { useError } from 'utils/contexts/ErrorContext'
+import { useMobileValue } from 'utils/hooks'
+
+export const Read = ({ data, config = {} }) => {
+  return <Textarea {...config}>{data?.value || config.defaultValue}</Textarea>
+}
+
+export const Write = ({ questionCode, data, onSendAnswer, regexPattern, errorMessage }) => {
+  let regex
+  const { dispatch } = useError()
+  const [errorStatus, setErrorStatus] = useState(false)
+  const [userInput, setuserInput] = useState(data?.value)
+
+  try {
+    regex = RegExp(regexPattern)
+  } catch (err) {
+    console.error('There is an error with the regex', questionCode, err)
+    regex = undefined
+  }
+
+  const inputRef = useRef()
+  const isInvalid = getIsInvalid(userInput)(regex)
+
+  useEffect(() => {
+    isInvalid ? setErrorStatus(true) : setErrorStatus(false)
+  }, [isInvalid])
+
+  useEffect(() => {
+    isInvalid
+      ? dispatch({ type: ACTIONS.SET_TO_TRUE, payload: questionCode })
+      : dispatch({ type: ACTIONS.SET_TO_FALSE, payload: questionCode })
+  }, [dispatch, isInvalid, questionCode])
+
+  const debouncedSendAnswer = debounce(onSendAnswer, 500)
+
+  const maxW = useMobileValue(['', '25vw'])
+
+  return (
+    <>
+      <Textarea
+        test-id={questionCode}
+        ref={inputRef}
+        onBlur={e => !errorStatus && debouncedSendAnswer(e.target.value)}
+        onChange={e => setuserInput(e.target.value)}
+        defaultValue={data?.value}
+        w="full"
+        maxW={maxW}
+        isInvalid={isInvalid}
+      />
+      {errorStatus && (
+        <Text textStyle="tail.error" mt={2}>
+          {errorMessage}
+        </Text>
+      )}
+    </>
+  )
+}
+
+const TextArea = {
+  Write,
+  Read,
+}
+
+export default TextArea
