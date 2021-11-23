7539ca51e4a3d7031e3c1d156ab28ab905f6d063
Digesh Bajracharya <digeshbajracharya@Digeshs-MacBook-Pro.local>
2021-11-23 07:56:59 +0545

    fixed steps complete check for mentee dashboard

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const DAY_IN_MS = 24 * 60 * 60 * 1000
 export const DATE_FORMAT = 'DD MMM YY'
 export const WITH_TIME = DATE_FORMAT + ' h:mm A'
-export const complete = 'COMPLETE'
+export const complete = 'Complete'
