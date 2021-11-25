14993d59e84f16c52b06aa8411194bd2a54d5108
Digesh Bajracharya <digeshbajracharya@Digeshs-MacBook-Pro.local>
2021-11-25 09:39:37 +0545

    hotfix mentor dashboard timeline fix

--- src/app/layouts/dashboard/mentor/helpers/get-mentor-information.js
+++ src/app/layouts/dashboard/mentor/helpers/get-mentor-information.js
@@ -1,6 +1,7 @@
 import { __, equals } from 'ramda'
 
 import { complete } from 'utils/constants'
+import convertToUppercase from 'utils/formatters/uppercase-convert'
 import { selectCode } from 'redux/db/selectors'
 import { useSelector } from 'react-redux'
 
@@ -8,9 +9,12 @@ const isComplete = equals(__, complete)
 
 const useGetMentorInformation = () => {
   const userCode = useSelector(selectCode('USER'))
-  const profileStatus = useSelector(selectCode(userCode, 'PRI_PROFILE'))?.valueString
-  const trainingStatus = useSelector(selectCode(userCode, 'PRI_TRAINING_STATUS'))?.valueString
-  const isProfileCompleted = isComplete(profileStatus)
+  let profileStatus = useSelector(selectCode(userCode, 'PRI_PROFILE'))?.valueString
+  let trainingStatus = useSelector(selectCode(userCode, 'PRI_TRAINING_STATUS'))?.valueString
+  let isProfileCompleted = isComplete(profileStatus)
+
+  profileStatus = convertToUppercase(profileStatus)
+  trainingStatus = convertToUppercase(profileStatus)
 
   return {
     profileStatus,
