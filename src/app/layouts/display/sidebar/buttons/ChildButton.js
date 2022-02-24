be40837ad9ee684056e32dc36914f492585a8c6e
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-02-24 20:21:44 +0545

    styling menuitems

--- src/app/layouts/display/sidebar/buttons/ChildButton.js
+++ src/app/layouts/display/sidebar/buttons/ChildButton.js
@@ -1,4 +1,4 @@
-import { Box, Menu, MenuButton, MenuItem, MenuList, Text, VStack } from '@chakra-ui/react'
+import { Box, HStack, Menu, MenuButton, MenuItem, MenuList, Text, VStack } from '@chakra-ui/react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
@@ -30,22 +30,22 @@ const ChildButton = ({ questionCode, childCode, onClick, sideBarButtons }) => {
       </VStack>
     )
   return (
-    <Menu>
+    <Menu placement="right-start">
       <MenuButton test-id={childCode}>
         <VStack spacing="4" role="group" test-id={childCode} onClick={onClick}>
           <Box display="flex" alignItems="center" justifyContent="center">
             <FontAwesomeIcon icon={icons[childCode]} size="2x" color="#AAE3E2" />
           </Box>
-
-          <Text color="#BDC5CD" fontSize="12px" fontWeight="400">
-            {name}
-          </Text>
-
-          {!sideBarButtons && <FontAwesomeIcon icon={faAngleDown} />}
+          <HStack>
+            <Text color="#BDC5CD" fontSize="12px" fontWeight="400">
+              {name}
+            </Text>
+            <FontAwesomeIcon icon={faAngleDown} color="#BDC5CD" />
+          </HStack>
         </VStack>
       </MenuButton>
 
-      <MenuList>
+      <MenuList minW="350px" margin="40px 0px 0px 12px">
         {childAsks.map(childAsk => (
           <MenuItem
             onClick={() => {
@@ -53,6 +53,9 @@ const ChildButton = ({ questionCode, childCode, onClick, sideBarButtons }) => {
             }}
             test-id={childAsk.questionCode}
             key={childAsk.questionCode}
+            _hover={{ bg: '#3AB8B5', color: '#ffffff' }}
+            fontSize="14px"
+            fontWeight="400"
           >
             {childAsk.name}
           </MenuItem>
