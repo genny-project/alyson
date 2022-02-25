3bdc0c7019b3b53bc7fadf8be5d296209e6cf1ef
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-02-25 13:21:04 +0545

    show the template of sidebar depending on the pcm

--- src/app/layouts/display/sidebar/index.js
+++ src/app/layouts/display/sidebar/index.js
@@ -2,7 +2,7 @@ import { Center, VStack } from '@chakra-ui/react'
 import { find, includes, reduce } from 'ramda'
 import { useSelector } from 'react-redux'
 import { selectCode } from 'redux/db/selectors'
-import ChildButton from 'app/layouts/display/sidebar/buttons/SidebarButtons'
+import SidebarButtons from 'app/layouts/display/sidebar/buttons/SidebarButtons'
 
 import { SIDEBAR_WIDTH } from 'utils/constants'
 import { NAV_Q_CODE } from 'utils/constants'
@@ -29,22 +29,75 @@ const SideBar = () => {
     PRI_TEMPLATE_CODE: code,
   } = mappedPcm
 
-  if (!data) return null
-
-  return (
-    <Center w={SIDEBAR_WIDTH} bg="#224371" h="100vh">
-      <VStack test-id={questionCode} justifyContent="center">
-        {data.map(childCode => (
-          <ChildButton
-            key={childCode}
+  const TemplateOne = () => {
+    return (
+      <Center w={SIDEBAR_WIDTH} bg="#224371" h="100vh">
+        <VStack test-id={questionCode} justifyContent="center">
+          <SidebarButtons
+            key={PRI_LOC1}
             questionCode={questionCode}
-            childCode={childCode}
+            childCode={PRI_LOC1}
             sideBarButtons={true}
           />
-        ))}
-      </VStack>
-    </Center>
-  )
+          <SidebarButtons
+            key={PRI_LOC2}
+            questionCode={questionCode}
+            childCode={PRI_LOC2}
+            sideBarButtons={true}
+          />
+          <SidebarButtons
+            key={PRI_LOC3}
+            questionCode={questionCode}
+            childCode={PRI_LOC3}
+            sideBarButtons={true}
+          />
+          <SidebarButtons
+            key={PRI_LOC4}
+            questionCode={questionCode}
+            childCode={PRI_LOC4}
+            sideBarButtons={true}
+          />
+          <SidebarButtons
+            key={PRI_LOC5}
+            questionCode={questionCode}
+            childCode={PRI_LOC5}
+            sideBarButtons={true}
+          />
+          <SidebarButtons
+            key={PRI_LOC6}
+            questionCode={questionCode}
+            childCode={PRI_LOC6}
+            sideBarButtons={true}
+          />
+        </VStack>
+      </Center>
+    )
+  }
+
+  const DefaultTemplate = () => {
+    return (
+      <Center w={SIDEBAR_WIDTH} bg="#224371" h="100vh">
+        <VStack test-id={questionCode} justifyContent="center">
+          {data.map(childCode => (
+            <SidebarButtons
+              key={childCode}
+              questionCode={questionCode}
+              childCode={childCode}
+              sideBarButtons={true}
+            />
+          ))}
+        </VStack>
+      </Center>
+    )
+  }
+
+  if (!data) return null
+
+  if (sidebarPcm) {
+    if (code === 'TPL_WEST') return <TemplateOne />
+  }
+
+  return <DefaultTemplate />
 }
 
 export default SideBar
