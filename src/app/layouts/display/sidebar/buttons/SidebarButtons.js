09f6771af536bbd85552daaca229e38be0c17ec7
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-02-28 14:43:45 +0545

    getting the icons from the question

--- src/app/layouts/display/sidebar/buttons/SidebarButtons.js
+++ src/app/layouts/display/sidebar/buttons/SidebarButtons.js
@@ -1,4 +1,14 @@
-import { Box, HStack, Menu, MenuButton, MenuItem, MenuList, Text, VStack } from '@chakra-ui/react'
+import {
+  Box,
+  HStack,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+  VStack,
+  Avatar,
+} from '@chakra-ui/react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
@@ -6,10 +16,13 @@ import icons from 'utils/icons'
 import { selectCode } from 'redux/db/selectors'
 import sendAskClick from 'app/ASKS/utils/send-ask-click'
 import { useSelector } from 'react-redux'
+import useApi from 'api'
 
 const SidebarButtons = ({ questionCode, childCode, onClick, iconId }) => {
   const data = useSelector(selectCode(questionCode, childCode))
-  console.log('---->ICON ID<-------', iconId)
+
+  const { getImageSrc } = useApi()
+  const src = getImageSrc(iconId, { height: '500', width: '500' })
 
   if (!data) return null
 
@@ -23,7 +36,11 @@ const SidebarButtons = ({ questionCode, childCode, onClick, iconId }) => {
     return (
       <VStack spacing="4" role="group" p="2" test-id={childCode} onClick={handleClick} as="button">
         <Box display="flex" alignItems="center" justifyContent="center">
-          <FontAwesomeIcon icon={icons[childCode]} size="2x" color="#AAE3E2" />
+          {iconId ? (
+            <Avatar cursor="pointer" src={src} />
+          ) : (
+            <FontAwesomeIcon icon={icons[childCode]} size="2x" color="#AAE3E2" />
+          )}
         </Box>
         <Text color="#BDC5CD" fontSize="12px" fontWeight="400">
           {name}
@@ -35,7 +52,11 @@ const SidebarButtons = ({ questionCode, childCode, onClick, iconId }) => {
       <MenuButton test-id={childCode}>
         <VStack spacing="4" role="group" test-id={childCode} onClick={onClick}>
           <Box display="flex" alignItems="center" justifyContent="center">
-            <FontAwesomeIcon icon={icons[childCode]} size="2x" color="#AAE3E2" />
+            {iconId ? (
+              <Avatar cursor="pointer" src={src} />
+            ) : (
+              <FontAwesomeIcon icon={icons[childCode]} size="2x" color="#AAE3E2" />
+            )}
           </Box>
           <HStack>
             <Text color="#BDC5CD" fontSize="12px" fontWeight="400">
