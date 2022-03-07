1eb2af85c05f6e2ff0147f717cae851cdb96c302
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-03-07 14:48:38 +0545

    refactor

--- src/utils/contexts/IsFieldNotEmptyReducer.js
+++ src/utils/contexts/IsFieldNotEmptyReducer.js
@@ -1,4 +1,4 @@
-import { isEmpty } from 'ramda'
+import { isEmpty, equals } from 'ramda'
 
 export const ACTIONS = {
   SET_TO_TRUE: 'setTrue',
@@ -8,14 +8,14 @@ export const ACTIONS = {
 export const initialState = {}
 
 export const isFiledNotEmptyReducer = (fieldState, { payload }) => {
-  let isValueEmpty
   const fieldValue = document.getElementById(payload)
   const value = fieldValue?.value
-  isValueEmpty = value ? isEmpty(value) : true
+  const isValueEmpty = isEmpty(value)
+  const isValueUndefined = equals(undefined)(value)
 
-  if (isValueEmpty) {
+  if (isValueEmpty || isValueUndefined) {
     return { ...fieldState, [payload]: false }
-  } else if (!isValueEmpty) return { ...fieldState, [payload]: true }
+  }
 
   return fieldState
 }
