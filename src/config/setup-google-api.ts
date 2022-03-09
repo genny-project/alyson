c138af73b6261633d9b07f8625855c68f44ac31d
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-03-09 07:09:38 +0545

    proper implementation for setting up google api

--- src/config/setup-google-api.ts
+++ src/config/setup-google-api.ts
@@ -1,15 +1,15 @@
+import axios from 'axios'
 import { HOST } from 'config/genny'
 
-// import axios from 'axios'
 const setupGoogleApi = async () => {
   const maps = document.createElement('script')
-  // const res = await axios.get(`${apiConfig.api_url}/googleapi/v1/map`, {
-  //   headers: { Authorization: `bearer ${token}` },
-  // })
-  // const src = URL.createObjectURL(new Blob([res.data], { type: 'text/javascript' }))
-  // maps.setAttribute('src', src)
-  maps.setAttribute('src', `${HOST}/googleapi/v1/map`)
+  const res = await axios.get(`${HOST}/googleapi/v1/map`, {})
+  const src = URL.createObjectURL(new Blob([res.data], { type: 'text/javascript' }))
+  maps.setAttribute('src', src)
+  // maps.setAttribute(
+  //   'src',
+  //   `https://maps.googleapis.com/maps/api/js?key=${apiConfig.ENV_GOOGLE_MAPS_APIKEY}&libraries=places,drawing`,
+  // )
   document.head.appendChild(maps)
 }
-
 export default setupGoogleApi
