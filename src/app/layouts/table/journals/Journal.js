c898be5c57885c11287395f2d32755946bd4881f
Thabo Gumede <76052777+Thabo-hub@users.noreply.github.com>
2022-03-15 09:09:48 +0200

    GEN-845
    'IM - Logbook - Approve Logbook -  button (Approve Logbook / Unapprove Logbook)   doesn’t change the colour, fixed now color is changing according to the status if approved color is green if not approved color is red'

--- src/app/layouts/table/journals/Journal.js
+++ src/app/layouts/table/journals/Journal.js
@@ -28,16 +28,11 @@ const Journal = ({ code, actions, parentCode }) => {
         <HStack>
           <Text textStyle="body.1">{`${date?.value}`}</Text>
           <Text color="teal" textStyle="body.1">{`${hours?.value} hrs`}</Text>
-          {journalStatus === 'APPROVED' ? (
-            <Text color="success.500">
-              <Attribute code={code} attribute={'PRI_STATUS'} />
-            </Text>
+           {journalStatus === 'APPROVED' ? (
+              <Attribute code={code} attribute={'PRI_STATUS'} config={{color:'success.500'}}/>
           ) : (
-            <Text color="error.500">
-              <Attribute code={code} attribute={'PRI_STATUS'} />
-            </Text>
+              <Attribute code={code} attribute={'PRI_STATUS'} config={{color:'error.500'}}/>
           )}
-          ;
           {userType === 'INTERN' && equals('APPROVED')(journalStatus)
             ? !isEmpty(internsAction) &&
               map(action => (
