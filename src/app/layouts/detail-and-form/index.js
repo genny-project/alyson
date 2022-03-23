1b0a2175f15399a9c3fabb2bc7ee7e9312afac40
Digesh Bajracharya <digesh.bajracharya@gada.io>
2022-03-23 10:02:21 +0545

    MM-195: PCM setup for mentor agree detail view

--- src/app/layouts/detail-and-form/index.js
+++ src/app/layouts/detail-and-form/index.js
@@ -1,20 +1,22 @@
-import { useSelector } from 'react-redux'
-import { Flex, Box } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react'
 
-import Form from 'app/layouts/form'
 import DetailView from 'app/layouts/detail-and-form/detail-view'
+import Form from 'app/layouts/form'
+import { selectCode } from '../../../redux/db/selectors'
 import { selectMentee } from 'redux/db/selectors'
+import { useSelector } from 'react-redux'
 
 const DetailForm = () => {
   const menteeCodes = useSelector(selectMentee)
   const mentee = menteeCodes?.[0]
+  const menteeCode = useSelector(selectCode(mentee, 'PRI_CODE'))?.value
 
   return (
     <Flex mb={5} px={10} alignItems="flex-start">
       <Box maxW="50vw">
         <Form adjacent />
       </Box>
-      <DetailView beCode={mentee} />
+      <DetailView beCode={menteeCode} />
     </Flex>
   )
 }
