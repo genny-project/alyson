595124e50417a1315295e23db0d49a6cf3fb0068
Digesh Bajracharya <digesh.bajracharya@gada.io>
2022-03-23 14:00:00 +0545

    MM-195: mapped pcm information

--- src/app/layouts/detail-and-form/detail-view/index.js
+++ src/app/layouts/detail-and-form/detail-view/index.js
@@ -7,101 +7,18 @@ import { selectCode } from 'redux/db/selectors'
 import { useSelector } from 'react-redux'
 
 const DefaultTemplate = ({ beCode, name, video, bg, mappedPcm, miniCard, cardsbg }) => {
-  const { PRI_LOC1, PRI_LOC2, PRI_LOC3, PRI_LOC4, PRI_LOC5, PRI_LOC6 } = mappedPcm
-
-  return (
-    <Grid w="60vw" bg={bg} spacing={4} p="5" overflowY="scroll" top="10vh" gap={'1em'}>
-      {video ? (
-        <Grid
-          justifyContent="space-between"
-          bg="gradient.900"
-          mb={5}
-          templateColumns={'200px 2fr'}
-          gap={'1rem'}
-          borderRadius={3}
-        >
-          <VStack justifyContent="center" spacing={5} m="auto" paddingX={'0.5rem'}>
-            <Attribute config={{ size: 'xl', name: name }} code={beCode} attribute={PRI_LOC1} />
-            <Attribute config={{ textStyle: 'head.3' }} code={beCode} attribute={PRI_LOC3} />
-          </VStack>
-          <VStack>
-            <Attribute code={beCode} attribute={PRI_LOC2} />
-          </VStack>
-        </Grid>
-      ) : (
-        <VStack mb={5}>
-          <Attribute config={{ size: 'xl', name: name }} code={beCode} attribute={PRI_LOC1} />
-          <Spacer />
-          <Attribute config={{ textStyle: 'head.3' }} code={beCode} attribute={PRI_LOC3} />
-        </VStack>
-      )}
-      <Grid
-        w={'100%'}
-        alignItems="flex-start"
-        templateColumns={'repeat(auto-fit, minmax(10rem, 1fr))'}
-        gap={'1em'}
-      >
-        <VStack
-          boxShadow="base"
-          rounded="md"
-          p="5"
-          w={'100%'}
-          alignItems="flex-start"
-          bg={cardsbg}
-          minH={miniCard ? '17rem' : 'inherit'}
-          flex="1 1 10rem"
-          height={'100%'}
-        >
-          <Text textStyle="head.2" mb={5}>
-            {'Professional Details'}
-          </Text>
-
-          <Details label="LinkedIn" locationCode={''} currentMentor={beCode} miniCard />
-          <Details label="Hobbies" locationCode={''} currentMentor={beCode} miniCard />
-        </VStack>
-        <VStack
-          boxShadow="base"
-          rounded="md"
-          p="5"
-          w={'100%'}
-          alignItems="flex-start"
-          bg={cardsbg}
-          minH={miniCard ? '17rem' : 'inherit'}
-          flex="1 1 10rem"
-          height={'100%'}
-        >
-          <Text textStyle="head.2" mb={5}>
-            {'Personal Details'}
-          </Text>
-          <Details label="First Name" locationCode={PRI_LOC3} currentMentor={beCode} miniCard />
-          <Details label="Last Name" locationCode={PRI_LOC4} currentMentor={beCode} miniCard />
-          <Details label="Email" locationCode={PRI_LOC5} currentMentor={beCode} miniCard />
-          <Details label="Mobile" locationCode={PRI_LOC6} currentMentor={beCode} miniCard />
-        </VStack>
-      </Grid>
-      <VStack
-        boxShadow="base"
-        rounded="md"
-        p="5"
-        w={'100%'}
-        alignItems="flex-start"
-        bg={cardsbg}
-        minH={miniCard ? '17rem' : 'inherit'}
-        flex="1 1 10rem"
-        height={'100%'}
-      >
-        <Text textStyle="head.2" mb={5}>
-          {'What I am interested from the mentorship'}
-        </Text>
-
-        <Details label="" locationCode={''} currentMentor={beCode} miniCard />
-      </VStack>
-    </Grid>
-  )
-}
-
-const TemplateOne = ({ beCode, name, video, bg, mappedPcm, miniCard, cardsbg }) => {
-  const { PRI_LOC1, PRI_LOC2, PRI_LOC3, PRI_LOC4, PRI_LOC5, PRI_LOC6 } = mappedPcm
+  const {
+    PRI_LOC1,
+    PRI_LOC2,
+    PRI_LOC3,
+    PRI_LOC4,
+    PRI_LOC5,
+    PRI_LOC6,
+    PRI_LOC7,
+    PRI_LOC8,
+    PRI_LOC9,
+    PRI_LOC10,
+  } = mappedPcm
 
   return (
     <Grid w="60vw" bg={bg} spacing={4} p="5" overflowY="scroll" top="10vh" gap={'1em'}>
@@ -152,6 +69,7 @@ const TemplateOne = ({ beCode, name, video, bg, mappedPcm, miniCard, cardsbg }) 
           </Text>
           <Details label="First Name" locationCode={PRI_LOC3} currentMentor={beCode} miniCard />
           <Details label="Last Name" locationCode={PRI_LOC4} currentMentor={beCode} miniCard />
+          <Details label="Gender" locationCode={PRI_LOC9} currentMentor={beCode} miniCard />
           <Details label="Email" locationCode={PRI_LOC5} currentMentor={beCode} miniCard />
           <Details label="Mobile" locationCode={PRI_LOC6} currentMentor={beCode} miniCard />
         </VStack>
@@ -171,8 +89,8 @@ const TemplateOne = ({ beCode, name, video, bg, mappedPcm, miniCard, cardsbg }) 
             {'Professional Details'}
           </Text>
 
-          <Details label="LinkedIn" locationCode={''} currentMentor={beCode} miniCard />
-          <Details label="Hobbies" locationCode={''} currentMentor={beCode} miniCard />
+          <Details label="LinkedIn" locationCode={PRI_LOC7} currentMentor={beCode} miniCard />
+          <Details label="Hobbies" locationCode={PRI_LOC10} currentMentor={beCode} miniCard />
         </VStack>
       </Grid>
       <VStack
@@ -190,7 +108,115 @@ const TemplateOne = ({ beCode, name, video, bg, mappedPcm, miniCard, cardsbg }) 
           {'What I am interested from the mentorship'}
         </Text>
 
-        <Details label="" locationCode={''} currentMentor={beCode} miniCard />
+        <Details label="" locationCode={PRI_LOC8} currentMentor={beCode} miniCard />
+      </VStack>
+    </Grid>
+  )
+}
+
+const TemplateOne = ({ beCode, name, video, bg, mappedPcm, miniCard, cardsbg }) => {
+  const {
+    PRI_LOC1,
+    PRI_LOC2,
+    PRI_LOC3,
+    PRI_LOC4,
+    PRI_LOC5,
+    PRI_LOC6,
+    PRI_LOC7,
+    PRI_LOC8,
+    PRI_LOC9,
+    PRI_LOC10,
+  } = mappedPcm
+
+  return (
+    <Grid w="60vw" bg={bg} spacing={4} p="5" overflowY="scroll" top="10vh" gap={'1em'}>
+      {video ? (
+        <Grid
+          justifyContent="space-between"
+          bg="gradient.900"
+          mb={2}
+          templateColumns={'200px 2fr'}
+          gap={'1rem'}
+          rounded={'md'}
+          overflow={'hidden'}
+        >
+          <VStack justifyContent="center" spacing={5} m="auto" paddingX={'0.5rem'}>
+            <Attribute config={{ size: 'xl', name: name }} code={beCode} attribute={PRI_LOC1} />
+            <Attribute config={{ textStyle: 'head.3' }} code={beCode} attribute={PRI_LOC3} />
+          </VStack>
+          <VStack>
+            <Attribute code={beCode} attribute={PRI_LOC2} />
+          </VStack>
+        </Grid>
+      ) : (
+        <VStack mb={5}>
+          <Attribute config={{ size: 'xl', name: name }} code={beCode} attribute={PRI_LOC1} />
+          <Spacer />
+          <Attribute config={{ textStyle: 'head.3' }} code={beCode} attribute={PRI_LOC3} />
+        </VStack>
+      )}
+      <Grid
+        w={'100%'}
+        alignItems="flex-start"
+        templateColumns={'repeat(auto-fit, minmax(10rem, 1fr))'}
+        gap={'1em'}
+      >
+        <VStack
+          boxShadow="base"
+          rounded="md"
+          p="5"
+          w={'100%'}
+          alignItems="flex-start"
+          bg={cardsbg}
+          minH={miniCard ? '17rem' : 'inherit'}
+          flex="1 1 10rem"
+          height={'100%'}
+        >
+          <Text textStyle="head.2" mb={5}>
+            {'Personal Details'}
+          </Text>
+          <Details label="First Name" locationCode={PRI_LOC3} currentMentor={beCode} />
+          <Details label="Last Name" locationCode={PRI_LOC4} currentMentor={beCode} />
+          <Details label="Gender" locationCode={PRI_LOC9} currentMentor={beCode} />
+          <Details label="Email" locationCode={PRI_LOC5} currentMentor={beCode} />
+          <Details label="Mobile" locationCode={PRI_LOC6} currentMentor={beCode} />
+        </VStack>
+
+        <VStack
+          boxShadow="base"
+          rounded="md"
+          p="5"
+          w={'100%'}
+          alignItems="flex-start"
+          bg={cardsbg}
+          minH={miniCard ? '17rem' : 'inherit'}
+          flex="1 1 10rem"
+          height={'100%'}
+        >
+          <Text textStyle="head.2" mb={5}>
+            {'Professional Details'}
+          </Text>
+
+          <Details label="LinkedIn" locationCode={PRI_LOC7} currentMentor={beCode} />
+          <Details label="Hobbies" locationCode={PRI_LOC10} currentMentor={beCode} />
+        </VStack>
+      </Grid>
+      <VStack
+        boxShadow="base"
+        rounded="md"
+        p="5"
+        w={'100%'}
+        alignItems="flex-start"
+        bg={cardsbg}
+        minH={miniCard ? '17rem' : 'inherit'}
+        flex="1 1 10rem"
+        height={'100%'}
+      >
+        <Text textStyle="head.2" mb={5}>
+          {'What I am interested from the mentorship'}
+        </Text>
+
+        <Details label="" locationCode={PRI_LOC8} currentMentor={beCode} />
       </VStack>
     </Grid>
   )
@@ -198,7 +224,7 @@ const TemplateOne = ({ beCode, name, video, bg, mappedPcm, miniCard, cardsbg }) 
 
 const DetailView = ({ beCode }) => {
   const allPcmCode = useSelector(selectCode(`PCMINFORMATION`)) || []
-  const detailViewPcmCode = find(includes('_DETAIL_VIEW'))(allPcmCode)
+  const detailViewPcmCode = find(includes('_MENTEE_DETAIL'))(allPcmCode)
 
   const detailViewPcm = useSelector(selectCode(detailViewPcmCode, 'allAttributes'))
   const mappedPcm = reduce((acc, { attributeCode, valueString }) => {
