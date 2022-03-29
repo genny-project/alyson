bd69de0e377df41363865bd0ed6e4874d980198c
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-03-29 13:11:59 +1100

    initials to get the interndetail view going

--- src/app/SBE/detail-profile/detail-layout/intern/detail-sections/desktop/index.js
+++ src/app/SBE/detail-profile/detail-layout/intern/detail-sections/desktop/index.js
@@ -1,10 +1,24 @@
 import { Box } from '@chakra-ui/layout'
+import { find, includes, reduce } from 'ramda'
 
 import 'app/layouts/components/css/hide-scroll.css'
 import LeftDetailSection from 'app/SBE/detail-profile/detail-layout/intern/detail-sections/desktop/LeftDetailSection.js'
 import RightDetailSection from 'app/SBE/detail-profile/detail-layout/intern/detail-sections/desktop/RightDetailSection.js'
+import { selectCode } from 'redux/db/selectors'
+import { useSelector } from 'react-redux'
 
 const DesktopView = ({ beCode, sbeCode, onScroll, onWheel }) => {
+  const allPcmCode = useSelector(selectCode(`PCMINFORMATION`)) || []
+  const internDetailViewPcmCode = find(includes('_INTERN_PROFILE'))(allPcmCode)
+  const internDetailViewPcm = useSelector(selectCode(internDetailViewPcmCode, 'allAttributes'))
+
+  const mappedPcm = reduce((acc, { attributeCode, valueString }) => {
+    acc = { ...acc, [attributeCode]: valueString }
+    return acc
+  }, {})(internDetailViewPcm || [])
+
+  console.log('%c 🙀', 'background: tomato; color: silver; padding: 0.5rem', mappedPcm)
+
   return (
     <Box className="nobar" overflowY="scroll" onScroll={onScroll} onWheel={onWheel}>
       <Box w="100%" alignItems="start" display="flex">
