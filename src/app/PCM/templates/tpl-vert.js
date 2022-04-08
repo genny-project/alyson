cbbc33bcce3c34681bb3c4354a40719e41b585e8
Dan M <daniel@thewindmills.com.au>
2022-04-08 12:07:13 +1000

    TPL_VERT is now set up

--- src/app/PCM/templates/tpl-vert.js
+++ src/app/PCM/templates/tpl-vert.js
@@ -1,5 +1,16 @@
+import { VStack } from '@chakra-ui/react'
+import getPcmField from 'app/PCM/helpers/get-pcm-field'
+import getSpillLocs from 'app/PCM/helpers/get-spill-locs'
+import mapSpillLocs from 'app/PCM/helpers/map-spill-locs'
+
 const TemplateVert = ({ mappedPcm }) => {
-  return <div>Vert</div>
+  return (
+    <VStack test-id={mappedPcm.PRI_QUESTION_CODE} justifyContent="center">
+      {mapSpillLocs(getSpillLocs(mappedPcm)())(loc => {
+        return getPcmField(loc, mappedPcm)()
+      })}
+    </VStack>
+  )
 }
 
 export default TemplateVert
