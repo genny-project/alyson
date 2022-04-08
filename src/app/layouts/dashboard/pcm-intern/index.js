e448e982985951fac3a6a57dc51c2286d6305911
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-04-08 11:26:09 +1000

    get rid of the unused vars

--- src/app/layouts/dashboard/pcm-intern/index.js
+++ src/app/layouts/dashboard/pcm-intern/index.js
@@ -1,22 +1,15 @@
-import { Box, HStack, Text, VStack, Stack } from '@chakra-ui/layout'
+import { HStack, Text, VStack } from '@chakra-ui/layout'
 import { Flex, Progress, Spacer, Divider, Image } from '@chakra-ui/react'
-import { Button } from '@chakra-ui/button'
 import { useSelector } from 'react-redux'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight, faSearch } from '@fortawesome/free-solid-svg-icons'
-import { find, includes, toUpper } from 'ramda'
+import { toUpper } from 'ramda'
 
 import { selectAttributes } from 'redux/db/selectors'
-import { selectDashboard } from 'redux/app/selectors'
-import { callBucketView, onSendMessage } from 'vertx'
+import { onSendMessage } from 'vertx'
 import Recommendations from 'app/layouts/dashboard/intern/recommendations'
-import Attribute from 'app/BE/attribute'
-import { useEffect } from 'react'
-import Card from 'app/layouts/components/card'
 
 const Intern = ({ userCode }) => {
   const profileCompletedPercentage = 30
-  const [firstnameInfo, associatedAgentInfo, status] = useSelector(
+  const [firstnameInfo, associatedAgentInfo] = useSelector(
     selectAttributes(userCode, ['PRI_FIRSTNAME', 'PRI_AGENT_NAME', 'PRI_STATUS']),
   )
 
@@ -107,82 +100,10 @@ const Intern = ({ userCode }) => {
       </VStack>
 
       <Divider w="calc(100% - 150px)" alignSelf="center" borderColor="#808080" />
+
+      <Recommendations />
     </VStack>
   )
-
-  //   const dashboardSbes = useSelector(selectDashboard)
-
-  //   const internSbe = find(includes('_INTERN_'), dashboardSbes || [])
-
-  //   useEffect(() => {
-  //     callBucketView()
-  //   }, [])
-
-  //   return (
-  //     <VStack>
-  //       <Stack maxW="90vw" direction={['column', 'row']} align="stretch">
-  //         <Card>
-  //           <HStack spacing="5">
-  //             <Box
-  //               onClick={() =>
-  //                 onSendMessage({
-  //                   code: 'QUE_AVATAR_PROFILE_GRP',
-  //                   parentCode: 'QUE_AVATAR_GRP',
-  //                   rootCode: userCode,
-  //                   targetCode: userCode,
-  //                 })
-  //               }
-  //             >
-  //               <Attribute code={userCode} attribute="PRI_IMAGE_URL" config={{ size: '2xl' }} />
-  //             </Box>
-  //             <VStack align="start">
-  //               <Text textStyle="tail.3">{`Welcome back,`}</Text>
-  //               <Text textStyle="head.1">{name?.value}</Text>
-  //               <Text textStyle="body.3" maxW="20rem">
-  //                 {occ?.value}
-  //               </Text>
-  //             </VStack>
-  //           </HStack>
-  //         </Card>
-  //         <Card>
-  //           <VStack align="stretch" height="100%">
-  //             <Text textStyle="body.1">{`Actions`}</Text>
-  //             <VStack align="stretch" height="inherit" justifyContent="space-evenly">
-  //               <Button
-  //                 hidden={status?.value === 'PROGRESS'}
-  //                 onClick={() =>
-  //                   onSendMessage({
-  //                     code: 'QUE_TREE_ITEM_INTERNSHIPS',
-  //                     parentCode: 'QUE_TREE_ITEM_INTERNSHIPS',
-  //                   })
-  //                 }
-  //                 colorScheme="primary"
-  //                 leftIcon={<FontAwesomeIcon icon={faSearch} />}
-  //                 rightIcon={<FontAwesomeIcon icon={faArrowRight} />}
-  //               >
-  //                 {`Find an Internship`}
-  //               </Button>
-  //               <Button
-  //                 onClick={() =>
-  //                   onSendMessage({
-  //                     code: 'ACT_PRI_EVENT_EDIT',
-  //                     parentCode: internSbe,
-  //                     targetCode: userCode,
-  //                   })
-  //                 }
-  //                 colorScheme="primary"
-  //                 variant="outline"
-  //               >
-  //                 {`Edit Your Profile`}
-  //               </Button>
-  //             </VStack>
-  //           </VStack>
-  //         </Card>
-  //       </Stack>
-  //       <Progress />
-  //       <Recommendations />
-  //     </VStack>
-  //   )
 }
 
 export default Intern
