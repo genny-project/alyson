bc4005b294d5bca9a3b023cc61c2c6fccac1626b
Expressionless <rhysmchaen@gmail.com>
2022-04-19 16:10:44 +1000

    Patched logos

--- src/app/layouts/navigation/index.js
+++ src/app/layouts/navigation/index.js
@@ -2,20 +2,20 @@ import DesktopNav from './desktop'
 import MobileNav from './mobile'
 import { apiConfig } from 'config/get-api-config'
 import { useIsMobile } from 'utils/hooks'
+import { equals } from 'ramda'
 
 const Navigation = () => {
   const { clientId, realm } = apiConfig
   const value = clientId || realm
-  const logoSrc =
-    value === 'internmatch'
-      ? '/internmatch_new.png'
-      : value === 'mentormatch'
-      ? '/MM_Primary_Fullcolour-1.png'
-      : value === 'lojing'
-      ? '/lojing-logo.png'
-      : value === 'credmatch'
-      ? '/credmatch_logo.jpg'
-      : '/credmatch_logo.jpg'
+  const logoSrc = equals(value, 'alyson')
+    ? '/internmatch_new.png'
+    : equals(value, 'mentormatch')
+    ? '/MM_Primary_Fullcolour-1.png'
+    : equals(value, 'lojing')
+    ? '/lojing-logo.png'
+    : equals(value, 'credmatch')
+    ? '/credmatch_logo.jpg'
+    : '/credmatch_logo.jpg'
   const isMobile = useIsMobile()
 
   return isMobile ? <MobileNav logoSrc={logoSrc} /> : <DesktopNav logoSrc={logoSrc} />
