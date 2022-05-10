61014aca5a8885db85edc1806cc9dca2ef9e7ee3
Digesh Bajracharya <digesh.bajracharya@gada.io>
2022-05-10 08:09:59 +0545

    fixed header UI

--- src/app/PCM/templates/navigation/tpl-north.js
+++ src/app/PCM/templates/navigation/tpl-north.js
@@ -1,37 +1,33 @@
-import { Box, Button, Image, Flex, HStack, Spacer } from '@chakra-ui/react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { Box, Button, Flex, HStack, Image, Spacer } from '@chakra-ui/react'
+import { LOGO_WIDTH, dashboardViewQuestion } from 'utils/constants'
 
 import AskMenu from 'app/ASKS/menu'
 import Avatar from 'app/layouts/navigation/Avatar.js'
 import Drafts from 'app/layouts/navigation/drafts/Drafts.js'
-import { onSendMessage } from 'vertx'
-import { dashboardViewQuestion, LOGO_WIDTH } from 'utils/constants'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { apiConfig } from 'config/get-api-config'
+import icons from 'utils/icons'
+import { onSendMessage } from 'vertx'
 
-const TemplateNorth = ({ bg, color, mappedPcm, logoSrc }) => {
+const TemplateNorth = ({ bg, color, mappedPcm, logoSrc, textColor }) => {
   const { PRI_LOC2, PRI_LOC3, PRI_LOC4 } = mappedPcm
 
   return (
-    <>
-      <header
-        style={{
-          color,
-          position: 'fixed',
-          top: 0,
-          zIndex: 9999,
-          maxWidth: '100vw',
-          left: 0,
-          right: 0,
-          backgroundColor: bg,
-          h: 25,
-          boxShadow: 'rgb(0 0 0 / 10%) 0px 2px 0px 0px',
-        }}
-      >
-        <nav>
-          <Flex align="center" p="3">
-            {/* These are hiiden as for now as they dont have actions. */}
-            {/* <Box mx={5} alignItems="center" m="auto">
+    <header
+      style={{
+        color,
+        maxWidth: '100vw',
+        h: 25,
+        backgroundColor: bg,
+        boxShadow: `0 4px 24px rgba(0,0,0,0.08)`,
+        position: 'relative',
+        zIndex: '1',
+      }}
+    >
+      <nav>
+        <Flex align="center" p="3">
+          {/* These are hiiden as for now as they dont have actions. */}
+          {/* <Box mx={5} alignItems="center" m="auto">
                 <HStack marginLeft="8" spacing="5">
                   <Box
                     onClick={() =>
@@ -46,34 +42,34 @@ const TemplateNorth = ({ bg, color, mappedPcm, logoSrc }) => {
                   <FontAwesomeIcon size="lg" icon={faSearch} cursor="pointer" color="#234371" />
                 </HStack>
               </Box> */}
-            <Box mx={5} alignItems="center" m="auto">
-              {apiConfig && (
-                <Image
-                  onClick={() =>
-                    onSendMessage({
-                      code: dashboardViewQuestion,
-                      parentCode: dashboardViewQuestion,
-                    })
-                  }
-                  src={logoSrc}
-                  htmlWidth={LOGO_WIDTH}
-                />
-              )}
-            </Box>
-            <Spacer />
-            <HStack spacing={8} marginRight="5">
-              <AskMenu
-                questionCode={PRI_LOC2}
-                icon={<Button leftIcon={<FontAwesomeIcon icon={faPlus} />}>{`Add`}</Button>}
-                hideLabel={true}
+          <Box mx={5} alignItems="center" m="auto">
+            {apiConfig && (
+              <Image
+                onClick={() =>
+                  onSendMessage({
+                    code: dashboardViewQuestion,
+                    parentCode: dashboardViewQuestion,
+                  })
+                }
+                src={logoSrc}
+                htmlWidth={LOGO_WIDTH}
               />
-              <Drafts code={PRI_LOC3} />
-              <Avatar code={PRI_LOC4} />
-            </HStack>
-          </Flex>
-        </nav>
-      </header>
-    </>
+            )}
+          </Box>
+          <Spacer />
+
+          <HStack spacing={8} marginRight="5">
+            <AskMenu
+              questionCode={PRI_LOC2}
+              icon={<Button leftIcon={<FontAwesomeIcon icon={icons[PRI_LOC2]} />}>{`Add`}</Button>}
+              hideLabel={true}
+            />
+            <Drafts code={PRI_LOC3} textColor={textColor} />
+            <Avatar code={PRI_LOC4} />
+          </HStack>
+        </Flex>
+      </nav>
+    </header>
   )
 }
 
