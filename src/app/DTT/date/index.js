67b24be103e61f0d9476d41e4ff057358e495793
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-05-16 16:00:00 +1000

    make the time required to be mandatory

--- src/app/DTT/date/index.js
+++ src/app/DTT/date/index.js
@@ -219,7 +219,7 @@ const Write = ({ questionCode, data, onSendAnswer, typeName, regexPattern, quest
               borderColor: 'gray.300',
               background: 'gray.100',
             }}
-            required
+            required={true}
           />
         )}
       </Grid>
