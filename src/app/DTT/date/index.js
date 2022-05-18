7bfe947afdefc6afed52637522a68af47136fb5b
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-05-18 13:14:51 +1000

    refactoring

--- src/app/DTT/date/index.js
+++ src/app/DTT/date/index.js
@@ -1,11 +1,10 @@
 import { Input, Text } from '@chakra-ui/react'
 import { dateOfBirthQuestionCode, eligibleAge, journalDateQuestionCode } from 'utils/constants'
-import { differenceInYears, format, isBefore, parseISO, startOfTomorrow } from 'date-fns'
+import { differenceInYears, format, parseISO } from 'date-fns'
 import { useEffect, useState } from 'react'
 
 import { ACTIONS } from 'utils/contexts/ErrorReducer'
 import DateChip from './DateChip'
-import { Grid } from '@chakra-ui/layout'
 import Year from './Year'
 import getDate from 'utils/helpers/timezone_magic/get-date'
 import { getIsInvalid } from 'utils/functions'
@@ -39,76 +38,27 @@ const Write = ({ questionCode, data, onSendAnswer, typeName, regexPattern, quest
   const { dispatch } = useError()
   const { dispatchFieldMessage } = useIsFieldNotEmpty()
   const [errorStatus, setErrorStatus] = useState(false)
-
-  const [isPreviousDate, setIsPreviousDate] = useState(true)
+  const [userInput, setuserInput] = useState(data?.value)
   const [errorMsg, setErrorMsg] = useState(initialErrorMsg)
-  const [chosenDate, setChosenDate] = useState()
-  const [chosenTime, setChosenTime] = useState()
 
   const includeTime = includes('LocalDateTime', typeName)
-
-  const current = new Date()
-  const today = format(current, 'yyyy-MM-dd')
-  const hrs = ('0' + current.getHours()).slice(-2)
-  const mins = ('0' + current.getMinutes()).slice(-2)
-  const currentTime = hrs + ':' + mins
-  const timeOutDuration = !chosenTime ? 2500 : 1000
-
-  const chosenDateAndTime =
-    (chosenDate && chosenTime) || (includeTime && chosenTime)
-      ? format(new Date(chosenDate), 'yyyy/MM/dd') + ' ' + chosenTime
-      : includeTime && chosenDate
-      ? format(new Date(chosenDate), 'yyyy/MM/dd') + ' ' + currentTime
-      : chosenDate
-      ? format(new Date(chosenDate), 'yyyy/MM/dd')
-      : undefined
-
-  const chosenDateAndTimeCopy =
-    chosenDate && chosenTime
-      ? format(new Date(chosenDate), 'yyyy/MM/dd') + ' ' + chosenTime
-      : undefined
-
-  if (questionCode === 'QUE_INTERNSHIP_START_DATE') {
-    console.log('%c 🙀', 'background: tomato; color: silver; padding: 0.5rem', {
-      data,
-      chosenDate,
-      chosenTime,
-      chosenDateAndTime,
-    })
-  }
-
   const onlyYear = typeName === 'year'
 
-  const handleChange = () => {
-    console.log('triggered handleChange ======>')
-    !errorStatus && onSendAnswer(safelyParseDate(chosenDate).toISOString())
-
-    setTimeout(() => {
-      if ((chosenDate && chosenTime) || (includeTime && chosenDate) || chosenTime) {
-        !errorStatus && onSendAnswer(safelyParseDate(chosenDateAndTime).toISOString())
-      }
-    }, timeOutDuration)
-
-    dispatchFieldMessage({ payload: questionCode })
+  const handleChange = e => {
+    if (e.target.value) {
+      !errorStatus && onSendAnswer(safelyParseDate(e.target.value).toISOString())
+      dispatchFieldMessage({ payload: questionCode })
+    }
   }
 
   const maxW = useMobileValue(['', '25vw'])
 
-  const isInvalid = getIsInvalid(chosenDateAndTime)(RegExp(regexPattern))
+  const isInvalid = getIsInvalid(userInput)(RegExp(regexPattern))
 
-  const tomorrowsDateInISOFormat = startOfTomorrow(today)
-  const inputDate = new Date(chosenDateAndTime)
-  const formatInputDate = chosenDateAndTime
-    ? format(new Date(chosenDateAndTime), 'yyyy-MM-dd')
-    : today
+  const today = format(new Date(), 'yyyy-MM-dd')
+
+  const formatInputDate = userInput ? format(new Date(userInput), 'yyyy-MM-dd') : today
   const diffInYears = differenceInYears(parseISO(today), parseISO(formatInputDate))
-
-  useEffect(() => {
-    if (includeTime && !!chosenDateAndTime) {
-      handleChange()
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [chosenDateAndTime, includeTime])
 
   useEffect(() => {
     isInvalid ? setErrorStatus(true) : setErrorStatus(false)
@@ -121,22 +71,6 @@ const Write = ({ questionCode, data, onSendAnswer, typeName, regexPattern, quest
   }, [dispatch, isInvalid, questionCode])
 
   useEffect(() => {
-    if (questionCode === 'QUE_JOURNAL_DATE' && chosenDateAndTime) {
-      const isDateBefore = isBefore(inputDate, tomorrowsDateInISOFormat)
-
-      isDateBefore ? setIsPreviousDate(true) : setIsPreviousDate(false)
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [questionCode, chosenDateAndTime, today])
-
-  useEffect(() => {
-    if (!isPreviousDate) {
-      setErrorStatus(true)
-      setErrorMsg('You cannot choose future date.')
-    }
-  }, [isPreviousDate])
-
-  useEffect(() => {
     if (questionCode === dateOfBirthQuestionCode) {
       if (diffInYears < eligibleAge) {
         setErrorStatus(true)
@@ -147,95 +81,34 @@ const Write = ({ questionCode, data, onSendAnswer, typeName, regexPattern, quest
     }
   }, [diffInYears, questionCode])
 
-  return isPreviousDate && data?.value ? (
+  return data?.value ? (
     <DateChip
       onlyYear={onlyYear}
       includeTime={includeTime}
-      onClick={() => {
-        onSendAnswer(chosenDateAndTime)
-        setChosenDate(chosenDate)
-        setChosenTime(chosenTime)
-      }}
+      onClick={() => onSendAnswer('')}
       date={getDate(data?.value)}
     />
   ) : onlyYear ? (
     <Year questionCode={questionCode} handleChange={handleChange} />
   ) : (
     <>
-      <Grid gridTemplateColumns={'1fr 1fr'} gap={'0.75rem'}>
-        <Input
-          id={questionCode}
-          onKeyDown={e => e.preventDefault()}
-          test-id={questionCode}
-          type={'date'}
-          onBlur={includeTime && handleChange}
-          onChange={e => setChosenDate(e.target.value)}
-          defaultValue={chosenDate}
-          max={
-            questionCode === journalDateQuestionCode
-              ? today
-              : questionCode === dateOfBirthQuestionCode
-              ? today
-              : ''
-          }
-          w="full"
-          maxW={maxW}
-          paddingBlock={3}
-          paddingInline={5}
-          fontWeight={'medium'}
-          borderColor={'gray.700'}
-          _hover={{
-            borderColor: 'green.500',
-            boxShadow: 'lg',
-          }}
-          _focusVisible={{
-            borderColor: 'green.500',
-            boxShadow: 'initial',
-          }}
-          _invalid={{
-            background: 'error.50',
-            borderColor: 'error.500',
-            color: 'error.500',
-          }}
-          _disabled={{
-            borderColor: 'gray.300',
-            background: 'gray.100',
-          }}
-        />
-        {
-          <Input
-            type={'time'}
-            id={questionCode}
-            test-id={questionCode}
-            onBlur={() => handleChange}
-            onChange={e => setChosenTime(e.target.value)}
-            defaultValue={chosenTime}
-            w="full"
-            maxW={maxW}
-            paddingBlock={3}
-            paddingInline={5}
-            fontWeight={'medium'}
-            borderColor={'gray.700'}
-            _hover={{
-              borderColor: 'green.500',
-              boxShadow: 'lg',
-            }}
-            _focusVisible={{
-              borderColor: 'green.500',
-              boxShadow: 'initial',
-            }}
-            _invalid={{
-              background: 'error.50',
-              borderColor: 'error.500',
-              color: 'error.500',
-            }}
-            _disabled={{
-              borderColor: 'gray.300',
-              background: 'gray.100',
-            }}
-          />
+      <Input
+        id={questionCode}
+        onKeyDown={e => e.preventDefault()}
+        test-id={questionCode}
+        type={includeTime ? 'datetime-local' : 'date'}
+        onBlur={handleChange}
+        onChange={e => setuserInput(e.target.value)}
+        w="full"
+        maxW={maxW}
+        max={
+          questionCode === journalDateQuestionCode
+            ? today
+            : questionCode === dateOfBirthQuestionCode
+            ? today
+            : ''
         }
-      </Grid>
+      />
       {errorStatus && (
         <Text textStyle="tail.error" mt={2}>
           {errorMsg}
