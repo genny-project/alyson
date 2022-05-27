26419c2b568a9f03f715349f77e57ae6e7842a45
Expressionless <rhysmchaen@gmail.com>
2022-05-27 11:31:39 +1000

    logo update

--- src/app/layouts/navigation/index.js
+++ src/app/layouts/navigation/index.js
@@ -6,17 +6,20 @@ import { useIsMobile } from 'utils/hooks'
 
 const Navigation = () => {
   const { clientId, realm } = apiConfig
+
+  /* I hate this but alyson client id is still a thing so until we deal
+   with that properly */
+  const isInternmatch = equals(clientId, 'alyson') || equals(clientId, 'internmatch')
   const value = clientId
-  const logoSrc =
-    clientId || equals(realm, 'internmatch')
-      ? '/internmatch_new.png'
-      : equals(value, 'mentormatch')
-      ? '/MM_Primary_Fullcolour-1.png'
-      : equals(value, 'lojing')
-      ? '/lojing-logo.png'
-      : equals(value, 'credmatch')
-      ? '/credmatch_logo.jpg'
-      : '/credmatch_logo.jpg'
+  const logoSrc = isInternmatch
+    ? '/internmatch_new.png'
+    : equals(value, 'mentormatch')
+    ? '/MM_Primary_Fullcolour-1.png'
+    : equals(value, 'lojing')
+    ? '/lojing-logo.png'
+    : equals(value, 'credmatch')
+    ? '/credmatch_logo.jpg'
+    : '/credmatch_logo.jpg'
   const isMobile = useIsMobile()
 
   return isMobile ? <MobileNav logoSrc={logoSrc} /> : <DesktopNav value={value} logoSrc={logoSrc} />
