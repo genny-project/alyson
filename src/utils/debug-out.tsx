c2683798ef83f12f3faf94ab70283c61a3bcdd97
Dan M <daniel@thewindmills.com.au>
2022-06-05 15:57:14 +1000

    Fixed the debugOut file

--- src/utils/debug-out.tsx
+++ src/utils/debug-out.tsx
@@ -9,22 +9,22 @@ const out = (fn: (message?: any) => void) => (message?: any) => {
 /**
  * Preforms console.log as long as `showLogs()` is `true`
  */
-const log = (message?: any) => out(console.log)
+const log = (message?: any) => out(console.log)(message)
 
 /**
  * Preforms console.info as long as `showLogs()` is `true`
  */
-const info = (message?: any) => out(console.info)
+const info = (message?: any) => out(console.info)(message)
 
 /**
  * Preforms console.warn as long as `showLogs()` is `true`
  */
-const warn = (message?: any) => out(console.warn)
+const warn = (message?: any) => out(console.warn)(message)
 
 /**
  * Preforms console.error as long as `showLogs()` is `true`
  */
-const error = (message?: any) => out(console.error)
+const error = (message?: any) => out(console.error)(message)
 
 /**
  * Provides logging functions from `console`, but only if `showLogs()` returns true
