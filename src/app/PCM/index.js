edf79013f52fe35e2570601d47f6392a8f4211a8
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-06-05 11:55:52 +1000

    refactor the logic to select templates based on templates code and template

--- src/app/PCM/index.js
+++ src/app/PCM/index.js
@@ -15,33 +15,26 @@ const MappedPcm = ({ code, mappedPcm, properties }) => {
   const { PRI_TEMPLATE_CODE: templateCode } = mappedPcm
 
   if (isNotEmpty(mappedPcm)) {
+    const template = templateHandlerMachine(mappedPcm)(templateCode)(properties)
     if (templateCode) {
-      const template = templateHandlerMachine(mappedPcm)(templateCode)(properties)
-
-      if (!template) {
-        console.error(
-          'Falling back on default template for PCM: ' +
-            code +
-            ' as template: ' +
-            templateCode +
-            ' could not be found!',
-        )
-        return <TemplateDefault {...properties} />
-      }
-
       return template
+    } else if (templateCode && !template) {
+      console.error(
+        `Falling back on default template for PCM: ${code} as template: ${templateCode} could not be found!`,
+      )
+      return <TemplateDefault {...properties} />
     } else {
-      console.error('PCM ' + code + " doesn't have a template code!")
+      console.error(`PCM ${code} doesn't have a template code!`)
       return (
         <Center>
           <CircularProgress mt="5" isIndeterminate />
         </Center>
       )
     }
+  } else {
+    console.error(`PCM with code ${code} is empty! Rendering default template!`)
+    return <TemplateDefault {...properties} />
   }
-
-  console.error('PCM with code ' + code + ' is empty! Rendering default template!')
-  return <TemplateDefault {...properties} />
 }
 
 export default Pcm
