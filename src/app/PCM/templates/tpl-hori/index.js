ce6338c613d2f6c208df7ade3b2e58834526a367
Dan M <daniel@thewindmills.com.au>
2022-06-24 16:34:22 +1000

    Removed any align items or justify from TPL_HORI until further notice

--- src/app/PCM/templates/tpl-hori/index.js
+++ src/app/PCM/templates/tpl-hori/index.js
@@ -9,7 +9,7 @@ import PcmField from 'app/PCM/components/pcm-field'
  */
 const TemplateHori = ({ mappedPcm }) => {
   return (
-    <HStack alignItems="center">
+    <HStack>
       {mapSpillLocs(getSpillLocs(mappedPcm)())(loc => (
         <PcmField key={loc} code={loc} mappedPcm={mappedPcm} />
       ))}
