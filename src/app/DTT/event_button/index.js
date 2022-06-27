8bacb9e1e250a8e062a2907b539b6999aa2ed0de
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-06-27 12:50:35 +1000

    refactor the event button

---
+++ src/app/DTT/event_button/index.js
@@ -0,0 +1,39 @@
+import { Box, Button } from '@chakra-ui/react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import { onSendMessage } from 'vertx'
+import Submit from './Submit'
+import isSubmitButton from 'app/DTT/event_button/helpers/is-submit.js'
+
+const EventButton = ({ askData, onFinish, parentCode }) => {
+  const { questionCode, targetCode, name, disabled } = askData
+
+  const onClick = () =>
+    onSendMessage({
+      code: questionCode,
+      rootCode: parentCode,
+      parentCode,
+      targetCode,
+      value: true,
+    })
+
+  if (isSubmitButton(name))
+    return <Submit askData={askData} onFinish={onFinish} parentCode={parentCode} />
+
+  return (
+    <Box>
+      <Button
+        test-id={questionCode}
+        isDisabled={disabled}
+        onClick={onClick}
+        leftIcon={<FontAwesomeIcon icon={faCheck} />}
+        colorScheme="primary"
+        variant="solid"
+      >
+        {name}
+      </Button>
+    </Box>
+  )
+}
+
+export default EventButton
