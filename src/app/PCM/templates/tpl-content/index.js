42e8ac46ba38c09c3ffb0570ac4bc38860113713
Dan M <daniel@thewindmills.com.au>
2022-06-28 14:42:40 +1000

    Added a quick comment to tpl_content

--- src/app/PCM/templates/tpl-content/index.js
+++ src/app/PCM/templates/tpl-content/index.js
@@ -1,5 +1,8 @@
 import PcmField from 'app/PCM/components/pcm-field'
 
+/**
+ * A straight passthrough template, can use this to prevent re-rendering large templates with lots of children
+ */
 const TemplateContent = ({ mappedPcm }) => {
   return <PcmField code={mappedPcm.PRI_LOC1} mappedPcm={mappedPcm} />
 }
