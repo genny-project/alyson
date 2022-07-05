0543b4dd1de1a861d0183e80253fd855989e6b2e
Xiang <xiang.chen@gada.io>
2022-07-05 11:17:01 +1000

    PIM-65 change the subtitles for intership details

--- src/app/SBE/detail-profile/detail-layout/application/detail-sections/desktop/template/ProfileSections.js
+++ src/app/SBE/detail-profile/detail-layout/application/detail-sections/desktop/template/ProfileSections.js
@@ -92,7 +92,7 @@ export const InternshipDetailsSection = ({ beCode }) => {
             </GridItem>
             <GridItem rowSpan={2} colSpan={2}>
               <VStack align="start">
-                <Text textStyle="tail.2">{`Expected Interpersonal Learning Outcomes`}</Text>
+                <Text textStyle="tail.2">{`Expected Technical & Industry Specific Learning Outcomes`}</Text>
                 <Attribute
                   code={beCode}
                   attribute={`_LNK_INTERNSHIP__PRI_BASE_LEARNING_OUTCOMES`}
@@ -114,7 +114,7 @@ export const InternshipDetailsSection = ({ beCode }) => {
             </GridItem>
             <GridItem rowSpan={2} colSpan={2}>
               <VStack align="start">
-                <Text textStyle="tail.2">{`Expected Technical & Industry Specefic Learning Outcomes`}</Text>
+                <Text textStyle="tail.2">{`Expected Interpersonal Learning Outcomes`}</Text>
                 <Attribute
                   code={beCode}
                   attribute={`_LNK_INTERNSHIP__PRI_SPECIFIC_LEARNING_OUTCOMES`}
