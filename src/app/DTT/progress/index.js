834f60ae0609196027e008039cc3b643738527b0
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-07-19 15:28:18 +1000

    write of progress behaving as readonly, upon request from BE

--- src/app/DTT/progress/index.js
+++ src/app/DTT/progress/index.js
@@ -1,11 +1,21 @@
 import ProgressBar from './Progress_Bar'
-import { Write } from 'app/DTT/text'
+import { Progress, Text, VStack } from '@chakra-ui/react'
 
 const Read = ({ data }) => <ProgressBar value={data?.value} />
+const Write = ({ data, placeholderName: label }) => {
+  return (
+    <VStack alignItems="start">
+      <Text color="gray.700" alignSelf="start">
+        {label}
+      </Text>
+      <Progress w="full" colorScheme="green" size="sm" value="20" />
+    </VStack>
+  )
+}
 
-const Progress = {
+const ProgressComponent = {
   Read,
   Write,
 }
 
-export default Progress
+export default ProgressComponent
