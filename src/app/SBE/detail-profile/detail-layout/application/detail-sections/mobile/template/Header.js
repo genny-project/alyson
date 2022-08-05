75f0f046a186038b5b92bdfd3ab050c57afa2412
Digesh Bajracharya <digesh.bajracharya@gada.io>
2022-08-05 11:02:16 +0545

    PIM-172: Internship spelling mistake fix

--- src/app/SBE/detail-profile/detail-layout/application/detail-sections/mobile/template/Header.js
+++ src/app/SBE/detail-profile/detail-layout/application/detail-sections/mobile/template/Header.js
@@ -80,7 +80,7 @@ const Header = ({ beCode, sbeCode }) => {
                 <Center minH="7rem" w="100%">
                   <VStack>
                     <Text textStyle="head.2" color="#ffffff">
-                      {`Intertnship video has not been uploaded`}
+                      {`Internship video has not been uploaded`}
                     </Text>
                     <Text textStyle="head.2" color="#ffffff">
                       {`Once it is, it will appear here!`}
