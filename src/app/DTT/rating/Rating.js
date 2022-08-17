997e57c52ba5501df9761252feb1151eb57959f8
Xiang <xiang.chen@gada.io>
2022-08-17 17:06:51 +1000

    Add keys to Ring Stars

--- src/app/DTT/rating/Rating.js
+++ src/app/DTT/rating/Rating.js
@@ -5,8 +5,8 @@ const Rating = ({ value, hover, setHover, onChange, questionCode }) => {
   if (!setHover)
     return (
       <HStack spacing="0">
-        {[1, 2, 3, 4, 5].map(idx => (
-          <div key={idx} style={{ width: '2rem', height: '2rem', padding: '0.5rem' }}>
+        {[1, 2, 3, 4, 5].map((idx, index) => (
+          <div key={index} style={{ width: '2rem', height: '2rem', padding: '0.5rem' }}>
             <Star fill={value >= idx ? 'gold' : 'white'} />
           </div>
         ))}
