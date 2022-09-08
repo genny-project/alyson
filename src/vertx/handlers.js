8eea1a619782d9a0942b54a5487479b5ed4366c0
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-09-08 12:54:46 +1000

    remove the unnecessary log

--- src/vertx/handlers.js
+++ src/vertx/handlers.js
@@ -2,7 +2,6 @@ import { forEach } from 'ramda'
 import log from 'utils/log'
 
 export const messageHandler = onNewMsg => body => {
-  console.log('testing=======>', { body, onNewMsg })
   if (body) {
     const { data_type, messages, aliasCode } = body
     log(`${aliasCode || data_type}`, body)
