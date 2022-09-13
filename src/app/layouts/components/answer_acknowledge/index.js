e8406da9efd1fa0690d6bae9bebf5166f6a93c60
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-09-13 12:16:40 +1000

    refactor ack logic so that it can be re used across multiple components

---
+++ src/app/layouts/components/answer_acknowledge/index.js
@@ -0,0 +1,29 @@
+import { useSelector } from 'react-redux'
+import { faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { maxNumberOfRetries, ACKMESSAGEKEY } from 'utils/constants'
+import { isNotStringifiedEmptyArray } from 'utils/functionals'
+import { selectCode } from 'redux/db/selectors'
+
+const AnswerAcknowledge = ({
+  failedValidation,
+  userInput,
+  retrySendingAnswerRef,
+  questionCode,
+}) => {
+  const ackMessageObject = useSelector(selectCode(ACKMESSAGEKEY))
+  const ackMessageValue = ackMessageObject?.[questionCode] || ''
+
+  return !failedValidation && userInput && isNotStringifiedEmptyArray(userInput) ? (
+    !!ackMessageValue ? (
+      <FontAwesomeIcon opacity="0.5" color="green" icon={faCheckCircle} />
+    ) : retrySendingAnswerRef.current < maxNumberOfRetries ? (
+      <FontAwesomeIcon opacity="0.5" color="orange" icon={faCheckCircle} />
+    ) : (
+      <FontAwesomeIcon opacity="0.5" color="red" icon={faTimesCircle} />
+    )
+  ) : null
+}
+
+export default AnswerAcknowledge
