351fa9a25cc6c8772685820c31e375467f2fa160
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-09-16 14:32:59 +1000

    autoplay the video sound

--- src/app/DTT/video/Player.tsx
+++ src/app/DTT/video/Player.tsx
@@ -6,7 +6,6 @@ const Player = ({ src }: { src: string }): ReactElement => {
       style={{ maxHeight: '340px', minWidth: '240px' }}
       controls
       autoPlay
-      muted
       poster="/video-intro.png"
     >
       <source src={src} />
