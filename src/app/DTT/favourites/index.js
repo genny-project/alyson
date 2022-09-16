31c59c4449c36fbde2624b2c461ff6982848b38e
Kriti Shrestha <kshrestha@macbook.localdomain>
2022-09-16 15:23:04 +1000

    changed the ui of the favourites component

--- src/app/DTT/favourites/index.js
+++ src/app/DTT/favourites/index.js
@@ -1,33 +1,38 @@
-import { faStar, faStarHalf } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 import debounce from 'lodash.debounce'
 import { useIsFieldNotEmpty } from 'utils/contexts/IsFieldNotEmptyContext'
 
-export const Write = ({ questionCode, data, onSendAnswer }) => {
+export const Write = ({ questionCode, onSendAnswer }) => {
   const [starred, setStarred] = useState(false)
   const debouncedSendAnswer = debounce(onSendAnswer, 500)
   const { dispatchFieldMessage } = useIsFieldNotEmpty()
-  const handleClick = currentState => {
+
+  const handleChange = currentState => {
     setStarred(currentState => !currentState)
-    debouncedSendAnswer(currentState)
+    debouncedSendAnswer(!currentState)
     dispatchFieldMessage({ payload: questionCode })
   }
 
   return (
     <FontAwesomeIcon
-      onClick={() => handleClick(starred)}
+      onClick={() => handleChange(starred)}
       size="lg"
-      icon={!!starred ? faStarHalf : faStar}
+      icon={faStar}
+      color={starred ? '#ffd700' : '#C0C0C0'}
     />
   )
 }
+
 export const Read = ({ data }) => {
-  const isStarred = data?.value
-  return <FontAwesomeIcon size="lg" icon={isStarred ? faStarHalf : faStar} />
+  const isStarred = data.value
+  return <FontAwesomeIcon size="lg" icon={faStar} color={isStarred ? '#ffd700' : '#C0C0C0'} />
 }
+
 const Favourites = {
   Write,
   Read,
 }
+
 export default Favourites
