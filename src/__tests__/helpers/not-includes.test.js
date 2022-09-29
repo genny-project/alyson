acdec1636a48dc4739e0f369caf9e18aea5eb02e
Kriti Shrestha <88418372+kriti-stha@users.noreply.github.com>
2022-09-30 09:53:47 +1000

    Update not-includes.test.js

--- src/__tests__/helpers/not-includes.test.js
+++ src/__tests__/helpers/not-includes.test.js
@@ -1,6 +1,6 @@
 import notIncludes from 'utils/helpers/not-includes'
 
-describe('Returns a boolean on whether a value inlcudes the agrument or not.', () => {
+describe('Returns a boolean on whether a value includes the argument or not.', () => {
   it('Should return true if the argument is not included and false otherside.', () => {
     expect(notIncludes('abc')(['abc', 'def'])).toEqual(false)
     expect(notIncludes('')(['abc', 'def', ''])).toEqual(false)
