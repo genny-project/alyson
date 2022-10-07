091ba3773f0df998b9ad3be35002b7062b8ca962
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-10-07 15:52:28 +1100

    cleaning up

--- src/app/BE/project-be/index.js
+++ src/app/BE/project-be/index.js
@@ -1,21 +1,17 @@
 import { selectCode } from 'redux/db/selectors'
 import { useSelector } from 'react-redux'
 import { compose } from 'ramda'
+import { projectCodeString } from 'utils/constants'
 
 /**
  * Returns the value stored in redux with the key `PROJECT`.
  * This should be the name of the main project base entity.
  */
 
-const projectCodeString = 'PROJECT'
-
 const useGetProjectInformation = () => {
   const projectCode = compose(useSelector, selectCode)(projectCodeString)
-
   return { projectCode }
 }
-
-const useGetProjectCode = () => useSelector(selectCode('PROJECT'))
 
 /**
  * Returns the attribute from PRJ_{clientId}@{attributeCode}, or undefined if it does not exist
@@ -25,7 +21,8 @@ const useGetProjectCode = () => useSelector(selectCode('PROJECT'))
  * Returns the attribute object, or undefined
  */
 const useGetAttributeFromProjectBaseEntity = attributeCode => {
-  return useSelector(selectCode(useGetProjectCode(), attributeCode))
+  const { projectCode } = useGetProjectInformation()
+  return useSelector(selectCode(projectCode, attributeCode))
 }
 
-export { useGetAttributeFromProjectBaseEntity, useGetProjectCode, useGetProjectInformation }
+export { useGetAttributeFromProjectBaseEntity, useGetProjectInformation }
