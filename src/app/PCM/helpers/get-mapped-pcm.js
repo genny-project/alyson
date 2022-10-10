51517c117caa6fbee29acb0f97a6183300caa775
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-10-10 12:49:26 +1100

    makes more sense

--- src/app/PCM/helpers/get-mapped-pcm.js
+++ src/app/PCM/helpers/get-mapped-pcm.js
@@ -5,9 +5,9 @@ import { selectCode } from 'redux/db/selectors'
 import { pcmKeyDefault } from 'utils/constants'
 import getMappedPcm from 'app/PCM/helpers/get-mapped-pcm-from-pcm-object'
 
-const useGetMappedPcm = (identifier, pcmKey = pcmKeyDefault, getterFn = getMappedPcm) => {
+const useGetMappedPcm = (code, pcmKey = pcmKeyDefault, getterFn = getMappedPcm) => {
   const allPcmCode = compose(useSelector, selectCode)(pcmKey) || []
-  const selectedPcmCode = find(equals(identifier))(allPcmCode)
+  const selectedPcmCode = find(equals(code))(allPcmCode)
   const selectedPcm = useSelector(selectCode(selectedPcmCode, 'allAttributes'), (left, right) =>
     equals(getterFn(left))(getterFn(right)),
   )
