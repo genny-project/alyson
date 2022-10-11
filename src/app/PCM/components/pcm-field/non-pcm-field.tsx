3a34ef06c89c78cf9c3e7ad4cb8f2fc2e30b8242
Dan M <daniel@thewindmills.com.au>
2022-10-11 12:55:41 +1100

    Added readonly flag

--- src/app/PCM/components/pcm-field/non-pcm-field.tsx
+++ src/app/PCM/components/pcm-field/non-pcm-field.tsx
@@ -6,12 +6,15 @@ import React from 'react'
 import debugOut from 'utils/debug-out'
 import getAskFromAttribute from 'app/PCM/helpers/get-ask-from-attribute'
 import { NonPcmPcmFieldProps } from 'app/PCM/components/pcm-field/types'
+import Ask from 'app/ASKS/ask'
 
 const NonPcmPcmField: React.FC<NonPcmPcmFieldProps> = (props): JSX.Element => {
   const { prefix, child, code, mappedPcm, config } = props
 
   const questionGroupCode = mappedPcm.PRI_QUESTION_CODE || ''
   const ask = getAskFromAttribute(questionGroupCode)(code)
+
+  const readonly = config?.readonly ?? ask?.readonly ?? true
 
   if (isEmpty(ask)) {
     debugOut.error(`NonPcmPcmField got an empty ask for ${props.code}! Returning a blank div`)
@@ -38,20 +41,34 @@ const NonPcmPcmField: React.FC<NonPcmPcmFieldProps> = (props): JSX.Element => {
           question: ask.question,
           props: config,
         })}
-      {!isEvt && !child && (
-        <Attribute
-          key={code}
-          attribute={code}
-          code={config?.parentCode || ask?.targetCode}
-          size={config?.size}
-          mini={config?.mini}
-          parentCode={config?.parentCode || ask?.targetCode}
-          variant={config?.variant}
-          config={config?.config}
-          styles={config?.styles}
-          hasIndicatorIcon={config?.hasIndicatorIcon}
-        />
-      )}
+      {!isEvt &&
+        !child &&
+        (readonly ? (
+          <Attribute
+            key={code}
+            attribute={code}
+            code={config?.parentCode || ask?.targetCode}
+            size={config?.size}
+            mini={config?.mini}
+            parentCode={config?.parentCode || ask?.targetCode}
+            variant={config?.variant}
+            config={config?.config}
+            styles={config?.styles}
+            hasIndicatorIcon={config?.hasIndicatorIcon}
+          />
+        ) : (
+          <Ask
+            key={code}
+            parentCode={config?.parentCode || ask?.parentCode}
+            questionCode={ask?.code}
+            config={config?.config}
+            noLabel={undefined}
+            secondaryColor={undefined}
+            onFinish={undefined}
+            passedAskData={undefined}
+            passedTargetCode={config?.parentCode || ask?.targetCode}
+          />
+        ))}
     </>
   )
 }
