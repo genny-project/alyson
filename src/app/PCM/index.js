c57eca16d89a3f0ccdf7acdae1ba025ef7a4db5b
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-10-17 12:08:03 +1100

    get rid of the unnecessary log

--- src/app/PCM/index.js
+++ src/app/PCM/index.js
@@ -22,8 +22,6 @@ const Pcm = ({ code, properties, depth }) => {
     properties,
   )
 
-  console.log({ mappedPcm, templateCode })
-
   if (!template) {
     debugOut.warn(
       `Falling back on default template for PCM: ${code} as template: ${templateCode} could not be found!`,
