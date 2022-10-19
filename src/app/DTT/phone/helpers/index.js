9929f519db0356011904e8ac0a90e5d9520f903d
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-10-19 11:20:40 +1100

    feature to show country flag with or without trailing + sign

--- src/app/DTT/phone/helpers/index.js
+++ src/app/DTT/phone/helpers/index.js
@@ -6,6 +6,7 @@ export const prepareAnswer = input => (equals('+')(input) ? '' : input)
 const getCountryInfo = countryList => countryCode => requiredInfo => {
   let countryObject = find(propEq('code', countryCode))(countryList || [])
   let requiredCountryInfo = prop(requiredInfo)(countryObject)
+
   return requiredCountryInfo
 }
 
@@ -13,9 +14,14 @@ export const getCountryInfoFromCountryList = getCountryInfo(countryList)
 
 export const getCountryObjectFromExistingUserInput = countryList => userInput => {
   const initialInput = compose(head, splitAt(5))(userInput)
+  let firstCharacterOfInitialInput = initialInput.charAt(0)
+  let initialInputWithPlusPrefix = equals(firstCharacterOfInitialInput)('+')
+    ? initialInput
+    : `+${initialInput}`
+
   return reduce((acc, countryObject) => {
     let { code } = countryObject
-    acc = includes(`${code}`, initialInput) ? acc.concat(countryObject) : acc
+    acc = includes(`${code}`, initialInputWithPlusPrefix) ? acc.concat(countryObject) : acc
     return acc
   }, [])(countryList)
 }
