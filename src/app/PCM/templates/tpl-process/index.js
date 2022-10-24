0660d65b781cf775112b2f023c8ec011bd884c03
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-10-24 14:48:22 +1100

    function to get the sbe code

--- src/app/PCM/templates/tpl-process/index.js
+++ src/app/PCM/templates/tpl-process/index.js
@@ -1,5 +1,5 @@
 import Process from 'app/layouts/process'
-import { compose, find, pathOr, propEq } from 'ramda'
+import { compose, find, pathOr, propEq, __ } from 'ramda'
 import { useSelector } from 'react-redux'
 import { selectCodeUnary } from 'redux/db/selectors'
 
@@ -11,27 +11,19 @@ const useGetSbesFromPcm = code => {
   return associatedSbeValue
 }
 
-const TemplateProcess = ({ mappedPcm, depth }) => {
-  const availableInterns = pathOr('', ['PRI_LOC1'])(mappedPcm)
-  const availableInternsSbeCode = useGetSbesFromPcm(availableInterns)
+const getBeObjectFromMappedPcm = pcmObject => location => pathOr('', [location])(pcmObject)
 
-  const appliedApplications = pathOr('', ['PRI_LOC2'])(mappedPcm)
-  const appliedApplicationsSbeCode = useGetSbesFromPcm(appliedApplications)
+const TemplateProcess = ({ mappedPcm }) => {
+  const getBeObject = getBeObjectFromMappedPcm(mappedPcm)
+  const getBeObjectBasedOnCode = compose(useGetSbesFromPcm, getBeObject)
 
-  const shortlistedApplications = pathOr('', ['PRI_LOC3'])(mappedPcm)
-  const shortlistedApplicationsSbeCode = useGetSbesFromPcm(shortlistedApplications)
-
-  const interviewedApplications = pathOr('', ['PRI_LOC4'])(mappedPcm)
-  const interviewedApplicationsSbeCode = useGetSbesFromPcm(interviewedApplications)
-
-  const offeredApplications = pathOr('', ['PRI_LOC5'])(mappedPcm)
-  const offeredApplicationsSbeCode = useGetSbesFromPcm(offeredApplications)
-
-  const placedApplications = pathOr('', ['PRI_LOC6'])(mappedPcm)
-  const placedApplicationsSbeCode = useGetSbesFromPcm(placedApplications)
-
-  const inProgressApplications = pathOr('', ['PRI_LOC7'])(mappedPcm)
-  const inProgressApplicationsSbeCode = useGetSbesFromPcm(inProgressApplications)
+  const availableInternsSbeCode = getBeObjectBasedOnCode('PRI_LOC1')
+  const appliedApplicationsSbeCode = getBeObjectBasedOnCode('PRI_LOC2')
+  const shortlistedApplicationsSbeCode = getBeObjectBasedOnCode('PRI_LOC3')
+  const interviewedApplicationsSbeCode = getBeObjectBasedOnCode('PRI_LOC4')
+  const offeredApplicationsSbeCode = getBeObjectBasedOnCode('PRI_LOC5')
+  const placedApplicationsSbeCode = getBeObjectBasedOnCode('PRI_LOC6')
+  const inProgressApplicationsSbeCode = getBeObjectBasedOnCode('PRI_LOC7')
 
   const processCodes = [
     availableInternsSbeCode,
