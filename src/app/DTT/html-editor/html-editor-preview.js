b5f5a2864456e7e155501db659f5eb86c39418db
Dan M <daniel@thewindmills.com.au>
2022-10-25 14:23:57 +1100

    Added an inModal flag to the config to deterimine if we want a popup or not for html output

--- src/app/DTT/html-editor/html-editor-preview.js
+++ src/app/DTT/html-editor/html-editor-preview.js
@@ -18,7 +18,7 @@ import purify from './purify'
 import { faExpandArrowsAlt } from '@fortawesome/free-solid-svg-icons'
 import { useRef, useState } from 'react'
 
-const HtmlEditorPreview = ({ html }) => {
+const HtmlEditorPreview = ({ html, inModal }) => {
   const ref = useRef(null)
   const { isOpen, onOpen, onClose } = useDisclosure()
 
@@ -34,46 +34,59 @@ const HtmlEditorPreview = ({ html }) => {
   }
   const clean = purify(html)
 
-  return (
-    <Box>
-      <VStack width={'100%'} align="flex-start">
-        <HStack justify="space-between" w="100%">
-          <ChakraText>Output</ChakraText>
-          <IconButton onClick={onOpen} icon={<FontAwesomeIcon icon={faExpandArrowsAlt} />} />
-        </HStack>
-        <Box borderTop="1px" width={'100%'}>
-          <Box maxHeight={'100%'} padding={1}>
-            <iframe
-              ref={ref}
-              srcDoc={clean}
-              title={'iframe-preview'}
-              width={'100%'}
-              height={height}
-              onLoad={onLoad}
-            />
+  if (inModal) {
+    return (
+      <Box>
+        <VStack width={'100%'} align="flex-start">
+          <HStack justify="space-between" w="100%">
+            <ChakraText>Output</ChakraText>
+            <IconButton onClick={onOpen} icon={<FontAwesomeIcon icon={faExpandArrowsAlt} />} />
+          </HStack>
+          <Box borderTop="1px" width={'100%'}>
+            <Box maxHeight={'100%'} padding={1}>
+              <iframe
+                ref={ref}
+                srcDoc={clean}
+                title={'iframe-preview'}
+                width={'100%'}
+                height={height}
+                onLoad={onLoad}
+              />
+            </Box>
           </Box>
-        </Box>
-      </VStack>
+        </VStack>
 
-      <Modal isOpen={isOpen} onClose={onClose} size={'4xl'}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Html Preview</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <iframe
-              ref={ref}
-              title={'iframe-modal'}
-              height={height}
-              width="100%"
-              srcDoc={clean}
-              onLoad={onLoad}
-            />
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-    </Box>
-  )
+        <Modal isOpen={isOpen} onClose={onClose} size={'4xl'}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Html Preview</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <iframe
+                ref={ref}
+                title={'iframe-modal'}
+                height={height}
+                width="100%"
+                srcDoc={clean}
+                onLoad={onLoad}
+              />
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      </Box>
+    )
+  } else {
+    return (
+      <iframe
+        ref={ref}
+        title={'iframe-modal'}
+        height={height}
+        width="100%"
+        srcDoc={clean}
+        onLoad={onLoad}
+      />
+    )
+  }
 }
 
 export default HtmlEditorPreview
