006739d145a69c18dbec1c339634be75ed7be1e0
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-10-25 13:07:24 +1100

    setting up the initials to get question events working with pcm

---
+++ src/app/ASKS/question-events/index.js
@@ -0,0 +1,5 @@
+const QuestionEvents = () => {
+  return <div>{`This should work`}</div>
+}
+
+export default QuestionEvents
--- src/app/PCM/components/pcm-field/non-pcm-field.tsx
+++ src/app/PCM/components/pcm-field/non-pcm-field.tsx
@@ -8,12 +8,14 @@ import getAskFromAttribute from 'app/PCM/helpers/get-ask-from-attribute'
 import { NonPcmPcmFieldProps } from 'app/PCM/components/pcm-field/types'
 import showTemplateNames from 'utils/helpers/show-template-names'
 import Ask from 'app/ASKS/ask'
+import QuestionEvents from 'app/ASKS/question-events'
 
 const NonPcmPcmField: React.FC<NonPcmPcmFieldProps> = (props): JSX.Element => {
   const { prefix, child, code, mappedPcm, config } = props
   const questionGroupCode = mappedPcm?.PRI_QUESTION_CODE || ''
   const ask = getAskFromAttribute(questionGroupCode)(code)
   const isEvt = equals(prefix, 'EVT')
+  const isQueEvent = equals(code, 'QUE_EVENTS')
   const renderEventButton = isEvt && !child
   const renderChild = not(renderEventButton) && !!child
   const isReadOnly = config?.readonly ?? ask?.readonly ?? true
@@ -23,6 +25,10 @@ const NonPcmPcmField: React.FC<NonPcmPcmFieldProps> = (props): JSX.Element => {
   if (isEmpty(ask)) {
     debugOut.error(`NonPcmPcmField got an empty ask for ${props.code}! Returning a blank div`)
     return showTemplateNames ? <div>Empty ask for ${props.code}</div> : <div />
+  }
+
+  if (isQueEvent) {
+    return <QuestionEvents />
   }
 
   return renderEventButton ? (
