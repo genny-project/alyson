0745b90358a3e6dd40f8a753bc1b899b2df2fe12
Dan M <daniel@thewindmills.com.au>
2022-10-27 13:15:28 +1100

    Started SBE detail view

--- src/app/PCM/templates/tpl-detail-view/tpl-sbe-detail-view/index.js
+++ src/app/PCM/templates/tpl-detail-view/tpl-sbe-detail-view/index.js
@@ -1,5 +1,24 @@
+import { Box, VStack } from '@chakra-ui/react'
+import useGetDetailData from '../get-detail-data'
+import FieldRow from './field-row'
+
 const TemplateSBEDetailView = ({ mappedPcm }) => {
-  return <div></div>
+  const { baseEntityCode, fields } = useGetDetailData(mappedPcm)
+
+  return (
+    <Box>
+      <VStack>
+        {fields.map((field, index) => (
+          <FieldRow
+            key={`detailview-${baseEntityCode}-${index}`}
+            baseEntityCode={baseEntityCode}
+            attributeCode={field}
+            index={index}
+          />
+        ))}
+      </VStack>
+    </Box>
+  )
 }
 
 export default TemplateSBEDetailView
