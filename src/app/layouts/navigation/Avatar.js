ea6ff749cb0330ee7ec43a5b3ba9761a14b85c45
TengkuBaltazar <muhammad.baltazar@onebyone.co>
2022-11-15 08:10:42 +0800

    Configurable keycloak in Avatar.js

--- src/app/layouts/navigation/Avatar.js
+++ src/app/layouts/navigation/Avatar.js
@@ -126,7 +126,7 @@ const AvatarMenu = ({ code: QUE_AVATAR_GRP }) => {
               ))}
 
             <a
-              href={`https://keycloak.gada.io/auth/realms/${apiConfig?.realm}/account/`}
+              href={`${apiConfig?.ENV_KEYCLOAK_REDIRECTURI}/realms/${apiConfig?.realm}/account/`}
               target="_blank"
               rel="noreferrer"
             >
