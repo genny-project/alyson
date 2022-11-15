0b404d9ae9e904de1e85d1b793accf8bb335476a
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-11-16 09:47:47 +1100

    refactor

--- src/app/PCM/helpers/get-spill-locs.js
+++ src/app/PCM/helpers/get-spill-locs.js
@@ -3,7 +3,7 @@ import { includes, keys, reduce } from 'ramda'
 /**
  * Returns all PRI_LOCs not in definedFields
  */
-const getSpillLocs = mappedPcm => (...definedFields) =>
+const getSpillLocs = (mappedPcm, ...definedFields) =>
   reduce((acc, attributeCode) => {
     if (
       includes('PRI_LOC')(attributeCode) ||
--- src/app/PCM/templates/vert-and-hori/map-standard.js
+++ src/app/PCM/templates/vert-and-hori/map-standard.js
@@ -4,7 +4,7 @@ import mapSpillLocs from 'app/PCM/helpers/map-spill-locs'
 
 const mapStandard = (mappedPcm, depth) =>
   mapSpillLocs(loc => <PcmField key={loc} code={loc} mappedPcm={mappedPcm} depth={depth} />)(
-    getSpillLocs(mappedPcm)(),
+    getSpillLocs(mappedPcm),
   )
 
 export default mapStandard
