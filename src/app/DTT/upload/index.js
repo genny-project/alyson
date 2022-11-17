ec15138708b36337dec54ca08d8d43d2eaedfb43
c7yrus <42728641+c7yrus@users.noreply.github.com>
2022-11-17 15:22:09 +1100

    fixes

--- src/app/DTT/upload/index.js
+++ src/app/DTT/upload/index.js
@@ -160,7 +160,7 @@ const Write = ({
           />
         ) : data?.value ? (
           <VStack>
-            {!!src && <DocViewer documents={docs} pluginRenderers={[DocViewerRenderers]} />}
+            {!!src && <DocViewer documents={docs} pluginRenderers={DocViewerRenderers} />}
             <HStack>
               <Button leftIcon={<FontAwesomeIcon icon={faCheck} />} colorScheme="green">{`${
                 fileName || 'File'
