a248049681bc4e1693901f8c46d13a6cf4248964
Kriti Shrestha <kritishrestha@Kritis-MBP.localdomain>
2022-11-29 15:08:19 +1100

    refactoring

--- src/app/layouts/table/index.js
+++ src/app/layouts/table/index.js
@@ -2,14 +2,14 @@ import InternInternshipSearch from './intern_internship'
 import Journals from './journals'
 import Table from 'app/SBE/table/Table'
 import getUserType from 'utils/helpers/get-user-type'
-import { includes } from 'ramda'
+import { compose, includes } from 'ramda'
 import { selectCode } from 'redux/db/selectors'
 import { useSelector } from 'react-redux'
 import debugOut from 'utils/debug-out'
 
 const TableWrapper = ({ mapSearch, passedComponents = [], tableCode: table }) => {
-  const userCode = useSelector(selectCode('USER'))
-  const userType = getUserType(useSelector(selectCode(userCode)))
+  const userCode = compose(useSelector, selectCode)('USER')
+  const userType = compose(getUserType, useSelector, selectCode)(userCode)
 
   if (!table) {
     debugOut.error('Got null table!')
