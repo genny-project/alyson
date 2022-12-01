1d966526a861cd66d56ab542664c7259bb23a0ad
Dan M <daniel@thewindmills.com.au>
2022-12-01 16:03:56 +1100

    Aignment fix

--- src/app/PCM/templates/vert-and-hori/map-standard.js
+++ src/app/PCM/templates/vert-and-hori/map-standard.js
@@ -3,8 +3,14 @@ import getSpillLocs from 'app/PCM/helpers/get-spill-locs'
 import mapSpillLocs from 'app/PCM/helpers/map-spill-locs'
 
 const mapStandard = (mappedPcm, depth) =>
-  mapSpillLocs(loc => <PcmField key={loc} code={loc} mappedPcm={mappedPcm} depth={depth} />)(
-    getSpillLocs(mappedPcm),
-  )
+  mapSpillLocs(loc => (
+    <PcmField
+      key={loc}
+      code={loc}
+      mappedPcm={mappedPcm}
+      depth={depth}
+      config={{ config: { mt: 0 } }}
+    />
+  ))(getSpillLocs(mappedPcm))
 
 export default mapStandard
