87c6beca3c26dd72cf899501e474f13ece2b76c9
Dan M <daniel@thewindmills.com.au>
2022-12-13 10:11:35 +1100

    Fixed issue with process view columns being cut off

--- src/app/PCM/templates/tpl-process/index.js
+++ src/app/PCM/templates/tpl-process/index.js
@@ -5,23 +5,25 @@ import PcmField from 'app/PCM/components/pcm-field'
 
 const TemplateProcess = ({ mappedPcm }) => {
   const spillLocs = getSpillLocs(mappedPcm, 'PRI_LOC1')
-
+  console.log(spillLocs)
   return (
-    <VStack justifyContent="flex-start" spacing={0} px="5">
-      <Box alignSelf={'flex-start'}>
+    <VStack justifyContent="flex-start" spacing={0}>
+      <Box alignSelf={'flex-start'} px="5">
         <PcmField code={mappedPcm.PRI_LOC1} />
       </Box>
 
-      <HStack
-        spacing={5}
-        paddingBlock="5"
-        align="flex-start"
-        justify="space-between"
-        height={'auto'}
-        overflow={'auto'}
-      >
-        {mapSpillLocs(loc => <PcmField key={loc} code={loc} />)(spillLocs)}
-      </HStack>
+      <Box width={'100%'}>
+        <HStack
+          spacing={5}
+          paddingBlock="5"
+          align="flex-start"
+          justify="space-between"
+          height={'auto'}
+          overflow={'auto'}
+        >
+          {mapSpillLocs(loc => <PcmField key={loc} code={loc} />)(spillLocs)}
+        </HStack>
+      </Box>
     </VStack>
   )
 }
