0c458d4765654da9f8340790388b3f39c3819702
Dan M <daniel@thewindmills.com.au>
2022-12-20 14:59:32 +1100

    Reverted process view

--- src/app/PCM/templates/tpl-process/index.js
+++ src/app/PCM/templates/tpl-process/index.js
@@ -1,30 +1,15 @@
 import getSpillLocs from 'app/PCM/helpers/get-spill-locs'
-import { HStack, VStack, Box } from '@chakra-ui/react'
-import mapSpillLocs from 'app/PCM/helpers/map-spill-locs'
-import PcmField from 'app/PCM/components/pcm-field'
+import useGetSbeCodeFromPcm from '../helpers/get-sbe-code-from-pcm'
+import getBeObjectFromMappedPcm from 'app/PCM/templates/helpers/get-be-object-from-mappedPcm'
+import Process from 'app/layouts/process'
+import { map, compose, keys } from 'ramda'
 
 const TemplateProcess = ({ mappedPcm }) => {
   const spillLocs = getSpillLocs(mappedPcm, 'PRI_LOC1')
-  return (
-    <VStack justifyContent="flex-start" spacing={0}>
-      <Box alignSelf={'flex-start'} px="5">
-        <PcmField code={mappedPcm.PRI_LOC1} />
-      </Box>
+  const getBeObject = getBeObjectFromMappedPcm(mappedPcm)
+  const processCodes = map(compose(useGetSbeCodeFromPcm, getBeObject))(keys(spillLocs))
 
-      <Box width={'100%'}>
-        <HStack
-          spacing={5}
-          paddingBlock="5"
-          align="flex-start"
-          justify="space-between"
-          height={'auto'}
-          overflow={'auto'}
-        >
-          {mapSpillLocs(loc => <PcmField key={loc} code={loc} />)(spillLocs)}
-        </HStack>
-      </Box>
-    </VStack>
-  )
+  return <Process processCodes={processCodes} />
 }
 
 export default TemplateProcess
