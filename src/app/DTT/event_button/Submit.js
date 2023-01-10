372c55ea31690f107fa13b9013d08007210ac358
c7yrus <42728641+c7yrus@users.noreply.github.com>
2023-01-10 13:32:44 +1100

    cleaning up submit button

--- src/app/DTT/event_button/Submit.js
+++ src/app/DTT/event_button/Submit.js
@@ -1,11 +1,9 @@
 import { Button, useTheme } from '@chakra-ui/react'
-import { compose, filter, identity, includes, map, prop } from 'ramda'
-import { selectAttributes, selectCode, selectWholeQuestionData } from 'redux/db/selectors'
-import { useDispatch, useSelector } from 'react-redux'
+import { includes } from 'ramda'
+import { selectCode } from 'redux/db/selectors'
+import { useSelector } from 'react-redux'
 
-import { highlightQuestion } from 'redux/app'
 import { onSendMessage } from 'vertx'
-import { selectCurrentFormQuestions } from 'redux/app/selectors'
 import { useError } from 'utils/contexts/ErrorContext'
 import useProductColors from 'utils/productColors'
 import { useState } from 'react'
@@ -13,32 +11,14 @@ import { useState } from 'react'
 const Submit = ({ askData, onFinish, parentCode, clientId }) => {
   const { questionCode, targetCode, name, disabled: disabledFromBackEnd } = askData
   const { errorState } = useError()
-  const dispatch = useDispatch()
-  const onHighlightQuestion = compose(dispatch, highlightQuestion)
   const theme = useTheme()
 
   const errorStateValues = Object.values(errorState)
   const hasError = includes(true)(errorStateValues)
   const isDisabled = hasError || disabledFromBackEnd
   const attrCode = useSelector(selectCode(questionCode, 'attributeCode'))
-  const questions = useSelector(selectCurrentFormQuestions)
-  const questionDatas = useSelector(selectWholeQuestionData(questions))
-  const mandatoryQuestions = filter(prop('mandatory'), questionDatas)
-  const mandatoryAttributes = map(prop('attributeCode'))(mandatoryQuestions)
-  const attributeData = filter(
-    identity,
-    useSelector(selectAttributes(targetCode, mandatoryAttributes)),
-  )
-  const mandatoryAttributesNoValue = compose(
-    map(prop('attributeCode')),
-    filter(attr => !attr.value),
-  )(attributeData)
 
   const { buttonBackgroundColor } = useProductColors()
-
-  const mandatoryQuestionsNoValue = filter(
-    q => q.questionCode !== 'QUE_SUBMIT' && includes(q.attributeCode, mandatoryAttributesNoValue),
-  )(mandatoryQuestions)
 
   const [loading, setLoading] = useState(false)
 
