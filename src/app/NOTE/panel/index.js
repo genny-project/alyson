a4f879752bd863909eb77aa14771b6e786e219d1
Joseph Maher <mr.joseph.maher@gmail.com>
2023-01-24 10:19:52 +1100

    Note panel close button

--- src/app/NOTE/panel/index.js
+++ src/app/NOTE/panel/index.js
@@ -1,19 +1,26 @@
-import { HStack, Text, VStack } from '@chakra-ui/layout'
+import { Box, HStack, Text, VStack } from '@chakra-ui/layout'
+import { IconButton } from '@chakra-ui/react'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Attribute from 'app/BE/attribute'
 import Card from 'app/layouts/components/card'
-import { useSelector } from 'react-redux'
+import 'app/layouts/components/css/hide-scroll.css'
+import { reverse } from 'ramda'
+import { useDispatch, useSelector } from 'react-redux'
+import { closeDrawer } from 'redux/app'
 import { selectCode } from 'redux/db/selectors'
 import bestTitleAttribute from 'utils/helpers/best-title-attribute'
+import { useIsMobile } from 'utils/hooks'
 import AddNote from '../add_note'
 import nameOfColumn from '../helpers/name-of-column'
 import NoteCard from '../note_card'
-import 'app/layouts/components/css/hide-scroll.css'
-import { useIsMobile } from 'utils/hooks'
-import { reverse } from 'ramda'
+
 const NotePanel = ({ code, idx, length }) => {
   const notes = useSelector(selectCode(code, 'NOTES')) || []
 
   const isMobile = useIsMobile()
+  const dispatch = useDispatch()
+  const onClose = () => dispatch(closeDrawer())
 
   return (
     <Card
@@ -32,6 +39,16 @@ const NotePanel = ({ code, idx, length }) => {
           <VStack align="start">
             <Attribute code={code} attribute={bestTitleAttribute(code)} />
           </VStack>
+
+          <Box overflow="hidden" borderRadius="50%" position="absolute" zIndex="modal" right="6">
+            <IconButton
+              onClick={onClose}
+              size="sm"
+              color="white"
+              bg="black"
+              icon={<FontAwesomeIcon opacity={50} icon={faTimes} />}
+            />
+          </Box>
         </HStack>
 
         <AddNote code={code} />
