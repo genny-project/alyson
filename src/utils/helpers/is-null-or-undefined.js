3beb7d82ebbd50a8e2efdcc5c90e44ea58964356
Kriti Shrestha <kritishrestha@Kritis-MacBook-Pro.local>
2023-01-30 10:44:42 +1100

    refactor

--- src/utils/helpers/is-null-or-undefined.js
+++ src/utils/helpers/is-null-or-undefined.js
@@ -3,7 +3,7 @@ const isNullOrUndefined = value => equals(value, undefined) || equals(value, nul
 
 export const isNullOrUndefinedOrEmpty = value => isNullOrUndefined(value) || isEmpty(value)
 
-export const isNotNullOrUndefinedOrEmpty = value => not(isNullOrUndefined(value) || isEmpty(value))
+export const isNotNullOrUndefinedOrEmpty = value => not(isNullOrUndefinedOrEmpty(value))
 
 export const isNullOrUndefinedOrFalse = value => isNullOrUndefined(value) || equals(value, false)
 
