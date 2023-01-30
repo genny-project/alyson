be32199dac6eecc04ca1d127690b3180caf18590
Dan M <daniel@thewindmills.com.au>
2023-01-30 11:39:49 +1100

    Updated TPL_PROCESS_VIEW to fix order and some styling

--- src/app/PCM/templates/tpl-process/index.js
+++ src/app/PCM/templates/tpl-process/index.js
@@ -2,12 +2,14 @@ import getSpillLocs from 'app/PCM/helpers/get-spill-locs'
 import useGetSbeCodeFromPcm from 'app/PCM/templates/helpers/get-sbe-code-from-pcm.js'
 import getBeObjectFromMappedPcm from 'app/PCM/templates/helpers/get-be-object-from-mappedPcm'
 import Process from 'app/layouts/process'
-import { map, compose, keys } from 'ramda'
+import { map, compose, keys, sort } from 'ramda'
 
 const TemplateProcess = ({ mappedPcm }) => {
   const spillLocs = getSpillLocs(mappedPcm, 'PRI_LOC1')
   const getBeObject = getBeObjectFromMappedPcm(mappedPcm)
-  const processCodes = map(compose(useGetSbeCodeFromPcm, getBeObject))(keys(spillLocs))
+  const processCodes = map(compose(useGetSbeCodeFromPcm, getBeObject))(
+    sort((a, b) => a.localeCompare(b), keys(spillLocs)),
+  )
 
   return <Process processCodes={processCodes} />
 }
