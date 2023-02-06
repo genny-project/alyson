c8f72c6c4821ca4ecd3ac1bcb41d9270db2adc14
Dan M <daniel@thewindmills.com.au>
2023-02-06 14:03:58 +1100

    Fixed date handling on saved search and seperator

---
+++ src/app/PCM/templates/tpl-sbe-row-add/sbe-add-boolean.js
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/react'
+
+const SBEAddBoolean = ({ askData, value, onChange, disabled }) => {
+  const questionCode = askData?.questionCode || ''
+
+  return <Box />
+}
+
+export default SBEAddBoolean
