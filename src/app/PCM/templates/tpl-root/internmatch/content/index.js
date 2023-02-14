19a12d495d1916c184274ff558c3b3d88d83cbda
c7yrus <42728641+c7yrus@users.noreply.github.com>
2023-02-14 16:32:14 +1100

    refactoring everything to its own folder

---
+++ src/app/PCM/templates/tpl-root/internmatch/content/index.js
@@ -0,0 +1,26 @@
+import {
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerOverlay,
+  Grid,
+  IconButton,
+} from '@chakra-ui/react'
+import DeveloperConsole, { isDev } from 'utils/developer'
+
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import LogrocketIdentifier from 'app/layouts/components/logrocket_identifier'
+import Dialog from 'app/layouts/display/dialog'
+import DisplayDrawer from 'app/layouts/display/drawer'
+import Toast from 'app/layouts/display/toast'
+import PcmField from 'app/PCM/components/pcm-field'
+import { SIDEBAR_WIDTH } from 'utils/constants'
+
+const Content = () => {
+  return <div>Content</div>
+}
+
+export default Content
--- src/app/PCM/templates/tpl-root/internmatch/index.js
+++ src/app/PCM/templates/tpl-root/internmatch/index.js
@@ -18,6 +18,9 @@ import DisplayDrawer from 'app/layouts/display/drawer'
 import Toast from 'app/layouts/display/toast'
 import PcmField from 'app/PCM/components/pcm-field'
 import { SIDEBAR_WIDTH } from 'utils/constants'
+import Header from 'app/PCM/templates/tpl-root/internmatch/header'
+import Sidebar from 'app/PCM/templates/tpl-root/internmatch/sidebar'
+import Content from 'app/PCM/templates/tpl-root/internmatch/content'
 
 const InternmatchRoot = ({
   isMobile,
@@ -46,38 +49,15 @@ const InternmatchRoot = ({
       fontFamily={'product.bodyFont'}
     >
       {/* HEADER WRAPPER */}
-      <header
-        style={{
-          gridArea: isMobile ? 'header/nav' : 'header',
-          color,
-          backgroundColor: lightColor,
-          boxShadow: '0px 4px 32px -16px rgba(0, 0, 0, 0.25)',
-          position: 'relative',
-        }}
-      >
-        {/* Header PCM*/}
-        <PcmField
-          code={PRI_LOC1}
-          mappedPcm={mappedPcm}
-          depth={depth}
-          properties={{ bg: 'lightColor', color: color }}
-        />
-
-        {isMobile && (
-          <IconButton
-            ref={btnRef}
-            aria-label="Toggle Side Navigation Bar"
-            aria-controls="sideNav"
-            icon={<FontAwesomeIcon icon={faBars} />}
-            position="absolute"
-            top="1.25rem"
-            left="0.75rem"
-            color="product.secondary"
-            bg="transparent"
-            onClick={onOpen}
-          />
-        )}
-      </header>
+      <Header
+        isMobile={isMobile}
+        color={color}
+        PRI_LOC1={PRI_LOC1}
+        mappedPcm={mappedPcm}
+        depth={depth}
+        btnRef={btnRef}
+        onOpen={onOpen}
+      />
 
       {/* SIDEBAR WRAPPER */}
 
