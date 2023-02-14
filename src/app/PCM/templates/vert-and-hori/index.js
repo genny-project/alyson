bf7024ab9f4187da996620f3bed6f7b4bf32ae2a
Dan M <daniel@thewindmills.com.au>
2023-02-14 14:04:15 +1100

    First commit

--- src/app/PCM/templates/vert-and-hori/index.js
+++ src/app/PCM/templates/vert-and-hori/index.js
@@ -7,8 +7,7 @@ import vert from './vert'
  * Returns all mapped attributes in a vertical list.
  * Currently uses a more expensive mapping of attributes as this may take in locs
  */
-export const TemplateVert = ({ mappedPcm, depth, config }) =>
-  vert(mapStandard)(mappedPcm, depth, config)
+export const TemplateVert = ({ mappedPcm, depth, config }) => vert(mapAll)(mappedPcm, depth, config)
 
 export const TemplateVertAll = ({ mappedPcm, depth, config }) =>
   vert(mapAll)(mappedPcm, depth, config)
@@ -17,8 +16,7 @@ export const TemplateVertAll = ({ mappedPcm, depth, config }) =>
  * Returns all mapped attributes in a horizontal list.
  * Currently uses a more expensive mapping of attributes as this may take in locs
  */
-export const TemplateHori = ({ mappedPcm, depth, config }) =>
-  hori(mapStandard)(mappedPcm, depth, config)
+export const TemplateHori = ({ mappedPcm, depth, config }) => hori(mapAll)(mappedPcm, depth, config)
 
 export const TemplateHoriAll = ({ mappedPcm, depth, config }) =>
   hori(mapAll)(mappedPcm, depth, config)
