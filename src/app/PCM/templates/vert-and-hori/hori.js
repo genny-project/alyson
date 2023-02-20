420d18562f6224f38d68eb79008b688a18a5fb3a
Dan M <daniel@thewindmills.com.au>
2023-02-20 16:10:40 +1100

    Added styling fields for justify

--- src/app/PCM/templates/vert-and-hori/hori.js
+++ src/app/PCM/templates/vert-and-hori/hori.js
@@ -1,11 +1,14 @@
 import { HStack } from '@chakra-ui/react'
 import { useIsMobile } from 'utils/hooks'
+import useProductColors from 'utils/productColors'
 
 const hori = mappingFunction => (mappedPcm, depth, config) => {
   const isMobile = useIsMobile()
 
+  const { tplHoriJustify } = useProductColors()
+
   return (
-    <HStack spacing={5} direction={isMobile ? 'column' : 'row'} w={'full'}>
+    <HStack spacing={5} direction={isMobile ? 'column' : 'row'} w={'full'} justify={tplHoriJustify}>
       {mappingFunction(mappedPcm, depth, config ?? { config: { mt: 0 } })}
     </HStack>
   )
