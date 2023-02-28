7e677aab6d93bb93487494c6a7d7afa8ff08d839
Kriti Shrestha <kritishrestha@Kritis-MBP.localdomain>
2023-03-01 10:43:39 +1100

    separated each type of toast message into a component of its own

---
+++ src/app/layouts/display/toast/error/index.js
@@ -0,0 +1,58 @@
+import { useToast, HStack, Text } from '@chakra-ui/react'
+import { Iconly } from 'react-iconly'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { selectToast } from 'redux/app/selectors'
+
+const status = 'error'
+const description = 'Error alert'
+const bgColor = '#F3E6EE'
+const borderColor = '#EA5024'
+const icon = 'Danger'
+const iconColor = '#D8201E'
+
+const ErrorToast = props => {
+  const { onClick } = props
+
+  const toast = useToast()
+  const newToast = useSelector(selectToast)
+
+  useEffect(() => {
+    if (newToast)
+      toast({
+        duration: 5000,
+        status,
+        render: () => (
+          <HStack
+            paddingBlock={'5'}
+            paddingInline={'10'}
+            borderWidth={'1px'}
+            borderColor={borderColor}
+            borderRadius={'2.5rem'}
+            color={'black'}
+            bg={bgColor}
+            justifyContent="space-between"
+          >
+            <HStack>
+              <Iconly
+                name={icon}
+                size={'large'}
+                set={'bulk'}
+                primaryColor={'white'}
+                secondaryColor={iconColor}
+              />
+              <Text fontSize={'16px'}>{description}</Text>
+            </HStack>
+
+            <Text onClick={onClick} color={iconColor} cursor={'pointer'}>
+              {' '}
+              x{' '}
+            </Text>
+          </HStack>
+        ),
+      })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [newToast, toast])
+  return null
+}
+export default ErrorToast
