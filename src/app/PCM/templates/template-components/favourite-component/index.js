fb3399a9ac63d93ca9cdeb6db60a9aac057f7781
Digesh Bajracharya <digesh.bajracharya@gada.io>
2023-03-01 13:08:49 +0545

    property ui fixes for missing details

--- src/app/PCM/templates/template-components/favourite-component/index.js
+++ src/app/PCM/templates/template-components/favourite-component/index.js
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button, Text } from '@chakra-ui/react'
+
 import { faStar } from '@fortawesome/free-solid-svg-icons'
-
-import debounce from 'lodash.debounce'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import sendEvtClick from 'app/ASKS/utils/send-evt-click'
-import { Button } from '@chakra-ui/react'
+import debounce from 'lodash.debounce'
+import { useState } from 'react'
 
-const FavouriteComponent = ({ starred, sourceCode, targetCode }) => {
+const FavouriteComponent = ({ starred, sourceCode, targetCode, showLabel }) => {
   const [isStarred, setIsStarred] = useState(starred)
 
   const sendEvt = value => {
@@ -27,13 +27,28 @@ const FavouriteComponent = ({ starred, sourceCode, targetCode }) => {
   }
 
   return (
-    <Button onClick={onClick} m={0} p={0} bg="#00000000">
+    <Button
+      onClick={onClick}
+      m={0}
+      p={0}
+      bg="#ffffff"
+      padding={1}
+      h={'auto'}
+      w={'auto'}
+      borderRadius={0}
+      _focus={{ outline: 'none' }}
+    >
       <FontAwesomeIcon
         position="absolute"
         icon={faStar}
-        size="lg"
-        color={isStarred ? '#ffd700' : '#C0C0C0'}
+        size="md"
+        color={isStarred ? 'product.primary' : '#C0C0C0'}
       />
+      {!!showLabel && (
+        <Text as="span" ml={2}>
+          {isStarred ? 'Remove from Favourite' : 'Add to Favourite'}
+        </Text>
+      )}
     </Button>
   )
 }
