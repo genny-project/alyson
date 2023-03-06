a7c9c146a40bcafbc9c67d6dbd5e65c5fa889a98
Dan M <daniel@thewindmills.com.au>
2023-03-06 15:36:31 +1100

    added index and count to mapquestiongrp

--- src/app/PCM/templates/vert-and-hori/map-all.js
+++ src/app/PCM/templates/vert-and-hori/map-all.js
@@ -9,8 +9,7 @@ import notIncludes from 'utils/helpers/not-includes'
 
 const mapAll = (mappedPcm, depth, isInternmatch = false, config = {}) => {
   const spillLocs = getSpillLocs(mappedPcm)
-  let i = 0
-  const questionGrp = mapQuestionGroup((ask, question) => {
+  const questionGrp = mapQuestionGroup((ask, question, index, count) => {
     const attributeCode =
       ask?.attributeCode ?? ask?.question?.attributeCode ?? question?.attributeCode ?? ''
 
@@ -18,13 +17,17 @@ const mapAll = (mappedPcm, depth, isInternmatch = false, config = {}) => {
       const evtAttrCode = includes('EVT_', attributeCode)
       const isEvtNext = equals(attributeCode, 'EVT_NEXT')
       const isEvtSubmit = equals(attributeCode, 'EVT_SUBMIT')
-      i++
+      console.log(index, count)
       return (
         <Box
           key={attributeCode}
           w={'full'}
           textAlign={
-            (evtAttrCode || isEvtNext || isEvtSubmit) && isInternmatch && i !== 1 ? 'end' : 'start'
+            (evtAttrCode || isEvtNext || isEvtSubmit) &&
+            isInternmatch &&
+            (index !== 0 || count === 1)
+              ? 'end'
+              : 'start'
           }
         >
           <PcmField code={attributeCode} mappedPcm={mappedPcm} depth={depth} />
@@ -34,14 +37,12 @@ const mapAll = (mappedPcm, depth, isInternmatch = false, config = {}) => {
   })(mappedPcm.PRI_QUESTION_CODE)
 
   const filteredUndefinedQuestionGroup = filter(item => !!item)(questionGrp)
-  i = 0
   const mappedDefinedLocs = mapSpillLocs(loc => {
-    i++
     return (
       <Box
         key={loc}
         w={'full'}
-        textAlign={includes('EVENTS', loc) && isInternmatch && i !== 1 ? 'end' : 'start'}
+        textAlign={includes('EVENTS', loc) && isInternmatch ? 'end' : 'start'}
       >
         <PcmField code={loc} mappedPcm={mappedPcm} depth={depth} config={config} />
       </Box>
