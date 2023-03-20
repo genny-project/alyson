9d8bf632298ea3353b1fd2ae7029232abb98094f
Digesh Bajracharya <digesh.bajracharya@gada.io>
2023-03-20 11:44:05 +0545

    LOJ-607: added pagination in lojing property table

--- src/app/PCM/templates/tpl_vertical_cards/index.js
+++ src/app/PCM/templates/tpl_vertical_cards/index.js
@@ -1,14 +1,15 @@
-import { Box, Grid } from '@chakra-ui/react'
-import { filter, includes, map } from 'ramda'
-import { getColumnDefs, getFields } from '../../helpers/sbe-utils'
+import { Box, Grid, Stack } from '@chakra-ui/react'
+import { equals, filter, includes, map } from 'ramda'
 import { selectCode, selectKeys } from 'redux/db/selectors'
+import { getColumnDefs, getFields } from '../../helpers/sbe-utils'
 
-import CardItem from './cardItem'
+import useGetMappedBaseEntity from 'app/PCM/helpers/use-get-mapped-base-entity'
+import Pagination from 'app/SBE/table/Pagination.js'
 import Title from 'app/SBE/table/Title'
 import { useGetActionsFromCode } from 'app/SBE/utils/get-actions'
-import useGetMappedBaseEntity from 'app/PCM/helpers/use-get-mapped-base-entity'
-import useProductColors from 'utils/productColors'
 import { useSelector } from 'react-redux'
+import useProductColors from 'utils/productColors'
+import CardItem from './cardItem'
 
 const TemplateVerticalCards = ({ mappedPcm, depth }) => {
   const sbeCodePrefix = mappedPcm?.PRI_LOC1 || ''
@@ -23,14 +24,21 @@ const TemplateVerticalCards = ({ mappedPcm, depth }) => {
 
   const rows = useSelector(selectCode(sbeCode, 'rows')) || []
   const { fieldTextColor } = useProductColors()
+  const showPagination = equals(sbeCode, 'SBE_TABLE_PROPERTIES')
 
   return (
     <>
       {!!rows.length && (
         <Box padding={'10px'}>
-          <Box paddingBottom={3}>
+          <Stack
+            direction={'row'}
+            paddingBottom={3}
+            alignItems={'center'}
+            justifyContent={'space-between'}
+          >
             <Title sbeCode={sbeCode} />
-          </Box>
+            {!!showPagination && <Pagination sbeCode={sbeCode} />}
+          </Stack>
 
           <Grid gap={4}>
             {map(item => (
