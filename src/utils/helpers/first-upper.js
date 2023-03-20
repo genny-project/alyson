56d0be29642e2718ef5302bc80cdab04f582cc90
Dan M <daniel@thewindmills.com.au>
2023-03-21 09:38:29 +1100

    changed firstUpper to point-free

--- src/utils/helpers/first-upper.js
+++ src/utils/helpers/first-upper.js
@@ -1,6 +1,8 @@
-import { toUpper, slice, toLower } from 'ramda'
+import { toUpper, compose, toLower, join, adjust, split } from 'ramda'
 
-const firstUpper = value =>
-  `${toUpper(slice(0, 1, value))}${toLower(slice(1, value.length, value))}`
+/**
+ * Takes an input string and returns that string with the first letter uppercase.
+ */
+const firstUpper = compose(join(''), adjust(0, toUpper), split(''), toLower)
 
 export default firstUpper
