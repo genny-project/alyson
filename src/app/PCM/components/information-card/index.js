8c4ad8e537c2df348a94d2dde1fd88a208934ac7
kritistha <kritistha020@gmail.com>
2023-04-12 13:33:18 +1000

    create reusable info cards

--- src/app/PCM/components/information-card/index.js
+++ src/app/PCM/components/information-card/index.js
@@ -1,7 +1,8 @@
-import { Box, Text, VStack, Divider, HStack } from '@chakra-ui/react'
+import { Box, Text, Divider, HStack } from '@chakra-ui/react'
 import { lt } from 'ramda'
 
 const InformationCard = () => {
+  const cardTitle = 'Interns / Candidates'
   const data = [
     { title: 'Available', value: '115' },
     { title: 'Applied', value: '12' },
@@ -9,37 +10,120 @@ const InformationCard = () => {
     { title: 'Interviewing', value: '19' },
   ]
 
+  const moreData = [
+    { title: 'Offered', value: '11' },
+    { title: 'Placed', value: '5' },
+  ]
+
   return (
     <>
       <Box
         bg={'#FFFFFF'}
         width={'33rem'}
-        height={'20rem'}
+        h={'20rem'}
         borderRadius={'2.5rem'}
-        paddingInline={'2rem'}
+        paddingBlock={'2.5rem'}
+        marginLeft={'2rem'}
       >
-        <VStack alignItems={'flex-start'}>
-          <Text fontSize={'20px'} fontFamily={'almarai'} paddingBlock={'1.5rem'}>
-            Interns / Candidates
-          </Text>
-          <Box
-            bg={'#FFFFFF'}
-            width={'29rem'}
-            height={'6rem'}
-            borderRadius={'1.8rem'}
-            border={'1px'}
-            borderColor={'#06323117'}
-          ></Box>
-          <Box
-            bg={'#FFFFFF'}
-            width={'29rem'}
-            height={'6rem'}
-            borderRadius={'1.8rem'}
-            border={'1px'}
-            borderColor={'#06323117'}
-            margin={'1rem'}
-          ></Box>
-        </VStack>
+        <Text
+          justifyContent={'flex-start'}
+          marginLeft={'2rem'}
+          marginTop={'-1rem'}
+          fontSize={'20px'}
+        >
+          {cardTitle}
+        </Text>
+        <Box
+          w={'30rem'}
+          h={'6rem'}
+          marginTop={'1rem'}
+          marginLeft={'2rem'}
+          borderRadius={'1.8rem'}
+          display={'inline-flex'}
+          border={'1px'}
+          borderColor={'#06323117'}
+        >
+          {data.map(({ title, value }, index) => (
+            <HStack
+              key={`${index}-${title}`}
+              w={'25rem'}
+              h={'3rem'}
+              marginTop={'1.5rem'}
+              justifyContent={'center'}
+              paddingInline={'1rem'}
+            >
+              <Box marginRight={'1.25rem'}>
+                <Text
+                  fontSize={'1.6rem'}
+                  textAlign={'center'}
+                  color={'#EA5024'}
+                  marginBottom={'-5px'}
+                >
+                  {value}
+                </Text>
+                <Text fontSize={'0.75rem'} maxW={'6.5rem'} textAlign={'center'}>
+                  {title}
+                </Text>
+              </Box>
+              {lt(index, data.length - 1) ? (
+                <Divider
+                  orientation={'vertical'}
+                  h={'3.1rem'}
+                  borderColor={'#06323161'}
+                  marginInlineStart={'1.5rem'}
+                />
+              ) : null}
+            </HStack>
+          ))}
+        </Box>
+        <Box
+          w={'30rem'}
+          h={'6rem'}
+          marginTop={'1rem'}
+          marginLeft={'2rem'}
+          borderRadius={'1.8rem'}
+          display={'flex'}
+          border={'1px'}
+          borderColor={'#06323117'}
+        >
+          {moreData.map(({ title, value }, index) => (
+            <HStack
+              key={`${title}-${index}`}
+              justifyContent={'flex-start'}
+              h={'3rem'}
+              marginTop={'1.5rem'}
+              paddingInline={'1rem'}
+            >
+              <Box marginRight={'1.5rem'}>
+                <Text
+                  fontSize={'1.6rem'}
+                  textAlign={'center'}
+                  color={'#EA5024'}
+                  marginBottom={'-5px'}
+                  marginLeft={'0.5rem'}
+                >
+                  {value}
+                </Text>
+                <Text
+                  fontSize={'0.75rem'}
+                  maxW={'6.5rem'}
+                  textAlign={'center'}
+                  marginLeft={'0.5rem'}
+                >
+                  {title}
+                </Text>
+              </Box>
+              {lt(index, moreData.length - 1) ? (
+                <Divider
+                  orientation={'vertical'}
+                  h={'3.1rem'}
+                  borderColor={'#06323161'}
+                  marginInlineStart={'1.5rem'}
+                />
+              ) : null}
+            </HStack>
+          ))}
+        </Box>
       </Box>
     </>
   )
