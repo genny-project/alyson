4222a2a9e80715529fbdb36a356911dcd1dadd77
Dan M <daniel@thewindmills.com.au>
2023-04-13 09:43:39 +1000

    Added mapping function to checklist

--- src/app/PCM/templates/tpl-detail-view/tpl-application-detail-view/checkLists.js
+++ src/app/PCM/templates/tpl-detail-view/tpl-application-detail-view/checkLists.js
@@ -7,30 +7,44 @@ import {
 } from 'utils/constants'
 
 import Ask from 'app/ASKS/ask'
+import { mapObject } from 'utils/helpers/map-object'
+import { compose } from 'ramda'
+import { rejectUndefined } from 'utils/helpers/reject-undefined'
 
 const CheckLists = ({ code: passedQuestionCode, isStudent }) => {
   const CheckListItem = ({ parentCode, questionCode }) => {
     return <Ask overrideComponent={'approve'} parentCode={parentCode} questionCode={questionCode} />
   }
 
+  /// Would be better to have the capabilities or backend handle this in the future, however
+  /// currently all asks are sent
+  const checklistItemsDisplayForStudent = {
+    [matchesVisaQuestionCode]: true,
+    [matchesEmpContractQuestionCode]: false,
+    [websiteInvestigatedQuestionCode]: false,
+    [adminConfirmedQuestionCode]: true,
+  }
+
+  const mapItems = compose(
+    rejectUndefined,
+    mapObject((questionCode, forStudent) => {
+      if (forStudent || !isStudent) {
+        return (
+          <CheckListItem
+            key={`CHECKLIST-ITEM-${questionCode}`}
+            parentCode={passedQuestionCode}
+            questionCode={questionCode}
+          />
+        )
+      } else {
+        return undefined
+      }
+    }),
+  )
+
   return (
     <Box>
-      <Grid>
-        <CheckListItem parentCode={passedQuestionCode} questionCode={matchesVisaQuestionCode} />
-        {!isStudent && (
-          <CheckListItem
-            parentCode={passedQuestionCode}
-            questionCode={matchesEmpContractQuestionCode}
-          />
-        )}
-        {!isStudent && (
-          <CheckListItem
-            parentCode={passedQuestionCode}
-            questionCode={websiteInvestigatedQuestionCode}
-          />
-        )}
-        <CheckListItem parentCode={passedQuestionCode} questionCode={adminConfirmedQuestionCode} />
-      </Grid>
+      <Grid>{mapItems(checklistItemsDisplayForStudent)}</Grid>
     </Box>
   )
 }
