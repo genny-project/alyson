6982f9956fc9ebfa971cfec5bc6061577298bfa6
kritistha <kritistha020@gmail.com>
2023-04-17 12:38:51 +1000

    create contat details card

---
+++ src/app/PCM/components/contact-details-card/index.js
@@ -0,0 +1,31 @@
+import { Box, HStack, Text, VStack } from '@chakra-ui/react'
+import { Iconly } from 'react-iconly'
+
+const ContactDetailsCard = () => {
+  const title = 'Contact Details'
+  const phoneNumber = '+61 0410 604 021'
+  const email = 'gerard@outcome.life'
+
+  return (
+    <Box width={'33rem'} height={'10rem'} bg={'#FFFFFF'} borderRadius={'2.5rem'}>
+      <HStack paddingInline={'3rem'} paddingBlock={'2rem'} justifyContent={'space-between'}>
+        <Text fontFamily={'Almarai'} fontSize={'20px'}>
+          {' '}
+          {title}
+        </Text>
+        <VStack alignItems={'flex-start'}>
+          <HStack>
+            <Iconly name={'Call'} set={'light'} />
+            <Text fontSize={'14px'}>{phoneNumber} </Text>
+          </HStack>
+          <HStack>
+            <Iconly name={'Message'} set={'two-tone'} />
+            <Text fontSize={'14px'}>{email} </Text>
+          </HStack>
+        </VStack>
+      </HStack>
+    </Box>
+  )
+}
+
+export default ContactDetailsCard
