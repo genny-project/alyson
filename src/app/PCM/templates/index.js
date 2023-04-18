d86e5503e956ee58fa87965d50962b183345c436
kritistha <kritistha020@gmail.com>
2023-04-18 15:40:00 +1000

    revert some changes

--- src/app/PCM/templates/index.js
+++ src/app/PCM/templates/index.js
@@ -1,39 +1,55 @@
 import { Box, Text } from '@chakra-ui/react'
 import { TemplateHori, TemplateHoriAll, TemplateVert, TemplateVertAll } from './vert-and-hori'
 
-import TemplateText from 'app/PCM/templates/text-templates'
 import TemplateAddItems from 'app/PCM/templates/tpl-add-items'
+import TemplateApplicationDetailView from 'app/PCM/templates/tpl-detail-view/tpl-application-detail-view'
 import TemplateAvatar from 'app/PCM/templates/tpl-avatar'
 import TemplateBell from 'app/PCM/templates/tpl-bell'
 import TemplateCard from 'app/PCM/templates/tpl-card'
 import TemplateContent from 'app/PCM/templates/tpl-content'
 import TemplateDefault from 'app/PCM/templates/tpl-default'
 import TemplateDetailView from 'app/PCM/templates/tpl-detail-view'
-import TemplateApplicationDetailView from 'app/PCM/templates/tpl-detail-view/tpl-application-detail-view'
-import TemplatePropertyDetailView from 'app/PCM/templates/tpl-detail-view/tpl-property-detail-view'
-import TemplateSBEDetailView from 'app/PCM/templates/tpl-detail-view/tpl-sbe-detail-view'
 import TemplateDisplay from 'app/PCM/templates/tpl-display'
 import TemplateForm from 'app/PCM/templates/tpl-form'
 import TemplateHeader from 'app/PCM/templates/tpl-header'
+import TemplateHorizontalCards from 'app/PCM/templates/tpl_horizontal_cards'
 import TemplateLogo from 'app/PCM/templates/tpl-logo'
+import TemplatePopup from 'app/PCM/templates/tpl_popup'
+import TemplateProcess from 'app/PCM/templates/tpl-process'
 import TemplateProgressBar from 'app/PCM/templates/tpl-progress-bar'
+import TemplatePropertyDetailView from 'app/PCM/templates/tpl-detail-view/tpl-property-detail-view'
 import TemplateRoot from 'app/PCM/templates/tpl-root'
+import TemplateSBEDetailView from 'app/PCM/templates/tpl-detail-view/tpl-sbe-detail-view'
 import TemplateSBERowAdd from 'app/PCM/templates/tpl-sbe-row-add'
 import TemplateSBESelect from 'app/PCM/templates/tpl-sbe-select'
 import TemplateSearchBar from 'app/PCM/templates/tpl-search-bar'
 import TemplateSidebarOne from 'app/PCM/templates/tpl-sidebar-one'
 import TemplateSubmitApplication from 'app/PCM/templates/tpl-submit-application/index.js'
 import TemplateTable from 'app/PCM/templates/tpl-table'
-import TemplateHorizontalCards from 'app/PCM/templates/tpl_horizontal_cards'
-import TemplatePopup from 'app/PCM/templates/tpl_popup'
+import TemplateText from 'app/PCM/templates/text-templates'
 import TemplateVerticalCards from 'app/PCM/templates/tpl_vertical_cards'
 import debugOut from 'utils/debug-out'
 import hasNot from 'utils/helpers/has-not'
 import showTemplateNames from 'utils/helpers/show-template-names'
-import TemplateHostCompanyProfile from './tpl-host-company-profile'
+
 /**
  * Takes in a mappedPcm, a templateCode and some misc properties and returns a template component.
  *
+
+    
+          
+            
+    
+
+          
+          
+            
+    
+
+          
+    
+    @@ -73,7 +72,7 @@ const templateHandlerMachine = depth => mappedPcm => parentCode => rest => {
+  
  * If the template does not exist, it will return the default template
  *
  */
@@ -44,7 +60,6 @@ const templateHandlerMachine = depth => mappedPcm => parentCode => rest => {
     parentCode,
     ...rest,
   }
-
   const listOfTemplates = {
     TPL_DEFAULT: <TemplateDefault />,
     TPL_ROOT: <TemplateRoot showTemplateNames={showTemplateNames} {...mandatoryProps} />,
@@ -72,10 +87,11 @@ const templateHandlerMachine = depth => mappedPcm => parentCode => rest => {
     TPL_CARD: <TemplateCard {...mandatoryProps} />,
     TPL_DETAIL_VIEW: <TemplateDetailView {...mandatoryProps} />,
     TPL_HORIZONTAL_CARDS: <TemplateHorizontalCards {...mandatoryProps} />,
-    TPL_PROCESS: <TemplateHostCompanyProfile />,
+    TPL_PROCESS: <TemplateProcess {...mandatoryProps} />,
     TPL_ADD_ITEMS: <TemplateAddItems {...mandatoryProps} />,
     TPL_BELL: <TemplateBell {...mandatoryProps} />,
     TPL_AVATAR: <TemplateAvatar {...mandatoryProps} />,
+
     TPL_PROPERTY_DETAIL_VIEW: <TemplatePropertyDetailView {...mandatoryProps} />,
     TPL_APPLICATION_DETAIL_VIEW: <TemplateApplicationDetailView {...mandatoryProps} />,
     TPL_SBE_DETAIL_VIEW: <TemplateSBEDetailView {...mandatoryProps} />,
@@ -86,22 +102,17 @@ const templateHandlerMachine = depth => mappedPcm => parentCode => rest => {
     TPL_SEARCH_BAR: <TemplateSearchBar {...mandatoryProps} />,
     TPL_SUBMIT_APPLICATION: <TemplateSubmitApplication {...mandatoryProps} />,
   }
-
   const { PRI_TEMPLATE_CODE: templateCode } = mappedPcm
   let noMatchingTemplates = hasNot(templateCode)(listOfTemplates)
   const defaultTemplate = <TemplateDefault />
   let matchingTemplate = listOfTemplates[templateCode]
   let template = noMatchingTemplates ? defaultTemplate : matchingTemplate
-
   if (noMatchingTemplates) {
     debugOut.warn(`No template exists for code: ${templateCode}! Falling back on default tempalte!`)
     return undefined
   }
-
   const code = mappedPcm?.code || mappedPcm?.PRI_NAME || 'No code or name'
-
   const color = Math.random().toString(16).substr(-6)
-
   return showTemplateNames ? (
     <Box position={'relative'}>
       <Text
@@ -126,5 +137,4 @@ const templateHandlerMachine = depth => mappedPcm => parentCode => rest => {
     template
   )
 }
-
 export default templateHandlerMachine
